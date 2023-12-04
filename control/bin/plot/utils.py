Implemented REINFORCE, fixed underflow issues in ReactorEnv, and made small changes to allow plotting in envs other than PID

control/bin/plot/utils.py
@@ -39,6 +39,7 @@ def sensitivity_curve(ax, data):
 
 def param_sweep(ax, data):
     params = list(data.keys())
+    print(params)
     params.sort()
     compare_auc = []
     compare_2nd_half = []
@@ -52,7 +53,7 @@ def param_sweep(ax, data):
     best_auc_idx = np.array(compare_auc).argmax()
     print("Sweeping AUC:", params[best_auc_idx], compare_auc[best_auc_idx])
     best_2ndhalf_idx = np.array(compare_2nd_half).argmax()
-    print("Sweeping the 2nd half:", params[best_2ndhalf_idx], compare_final[best_2ndhalf_idx])
+    print("Sweeping the 2nd half:", params[best_2ndhalf_idx], compare_2nd_half[best_2ndhalf_idx])
     best_final_idx = np.array(compare_final).argmax()
     print("Sweeping Final:", params[best_final_idx], compare_final[best_final_idx])
     print("")
@@ -76,7 +77,10 @@ def load_param(pth, xlim=[], pick_seed=None):
             with open(p+"/info_logs.pkl", "rb") as f:
                 info = pickle.load(f)
             for step in info:
-                cons.append(step["env_info/constrain"])
+                try:
+                    cons.append(step["env_info/constrain"])
+                except:
+                    pass
             cons = np.array(cons)
             
         if xlim != []:

control/src/agent/simple_ac.py
@@ -12,7 +12,7 @@ class SimpleAC(BaseAC):
         self.v_baseline = FC(self.device, self.state_dim, cfg.hidden_critic, 1)
         self.v_optimizer = torch.optim.RMSprop(list(self.v_baseline.parameters()), cfg.lr_v)
 
-    def inner_update(self):
+    def inner_update(self, trunc=False):
         batch = self.get_data()
         
         log_prob, _ = self.actor.log_prob(batch['obs'], batch['act'])

control/src/environment/smpl/envs/reactorenv.py
@@ -31,7 +31,7 @@ MAX_ACTIONS = [35.0, 0.2]  # Tc (coolant temperature), qout (volumetric flow rat
 MIN_ACTIONS = [15.0, 0.05]
 STEADY_OBSERVATIONS = [0.8778252, 51.34660837, 0.659]
 STEADY_ACTIONS = [26.85, 0.1]
-ERROR_REWARD = -1000.0
+ERROR_REWARD = -10000.0
 
 
 class ReactorPID:
@@ -195,23 +195,18 @@ class ReactorModel:
 
         rate_exp = -self.E_divided_by_R / (T + 273.15)
 
-        if rate_exp > 700.0:
-            print("Exponent Above Threshold")
-            return [None] * 3
-
         try:
             rate = self.k0 * c * np.exp(rate_exp)  # kmol/m^3/min
+            dxdt = [
+                self.q_in * (self.cAf - c) / (np.pi * self.r ** 2 * h + 1e-8) - rate,  # kmol/m^3/min
+                self.q_in * (self.Tf - T) / (np.pi * self.r ** 2 * h + 1e-8)
+                - self.dH / (self.rho * self.Cp) * rate
+                + 2 * self.U / (self.r * self.rho * self.Cp) * (Tc - T),  # degree C/min
+                (self.q_in - q) / (np.pi * self.r ** 2)  # m/min
+                ]
+            return dxdt
         except:
-            print("Failing Rate Exp:", rate_exp)
-
-        dxdt = [
-            self.q_in * (self.cAf - c) / (np.pi * self.r ** 2 * h + 1e-8) - rate,  # kmol/m^3/min
-            self.q_in * (self.Tf - T) / (np.pi * self.r ** 2 * h + 1e-8)
-            - self.dH / (self.rho * self.Cp) * rate
-            + 2 * self.U / (self.r * self.rho * self.Cp) * (Tc - T),  # degree C/min
-            (self.q_in - q) / (np.pi * self.r ** 2)  # m/min
-        ]
-        return dxdt
+            return [np.NAN] * 3
 
     # integrates one sampling time or time step and returns the next state
     def step(self, x, u):
@@ -290,7 +285,7 @@ class ReactorEnvGym(smplEnvBase):
         if reward is not None:
             return reward
         elif self.observation_beyond_box(current_observation) or self.action_beyond_box(action):
-            return self.error_reward
+            return self.error_reward * (self.max_steps - self.step_count)
         # /---- standard ----
         current_observation_evaluated = self.evaluate_observation(current_observation)
         assert isinstance(current_observation_evaluated, float)
@@ -301,7 +296,7 @@ class ReactorEnvGym(smplEnvBase):
         else:
             reward = current_observation_evaluated
         # ---- standard ----
-        reward = max(self.error_reward, reward)  # reward cannot be smaller than the error_reward
+        reward = max(self.error_reward * (self.max_steps - self.step_count), reward)  # reward cannot be smaller than the error_reward
         if self.debug_mode:
             print("reward:", reward)
         return reward
@@ -358,7 +353,7 @@ class ReactorEnvGym(smplEnvBase):
             observation = self.reactor.step(self.previous_observation, action)
             if np.isnan(observation).any():
                 observation = self.previous_observation
-                reward = self.error_reward
+                reward = self.error_reward * (self.max_steps - self.step_count)
                 done = True
                 done_info["terminal"] = True
         # /---- to capture numpy warnings ---- 