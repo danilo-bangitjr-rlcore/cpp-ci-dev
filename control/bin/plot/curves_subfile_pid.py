d16bee0936e3e290cd78ded3f18a3ab2e680dd42
han-wang-rlcore <hanwang@rlcore.ai>
2023-12-04 15:42:30 -0700

    plot each dimension on separate subplots

--- control/bin/plot/curves_subfile_pid.py
+++ control/bin/plot/curves_subfile_pid.py
@@ -1,3 +1,5 @@
+import copy
+
 from curves import DATAROOT, sweep_offline, reproduce_demo, best_offline
 from curves import visualize_training_info
 
@@ -56,9 +58,27 @@ def constant_pid_target0_replay0():
         }
         best_offline(pths, "best_changeAction_replay0_const_pid_discrete", ylim=[-50, 2])
 
-    direct_action()
+    def clip_change_action():
+        SHAREPATH = "output/test_v0/TTChangeAction/ClipConstPID/learning_rate/target0/replay0/env_scale_1/action_-0.1_0.1/"
+        pths = {
+            "SAC": [DATAROOT + SHAREPATH + "SAC/param_4/", "C0", 5],
+            "SimpleAC": [DATAROOT + SHAREPATH + "SimpleAC/param_6/", "limegreen", 3],
+            "GAC": [DATAROOT + SHAREPATH + "GAC/param_2", "C1", 1],
+        }
+        best_offline(pths, "best_changeActionClip_replay0_const_pid_e1_a0.1", ylim=[-50, 2])
+
+        SHAREPATH = "output/test_v0/TTChangeAction/ClipDiscreteConstPID/learning_rate/target0/replay0/env_scale_1/change_0.01/"
+        pths = {
+            "SAC": [DATAROOT + SHAREPATH + "SAC/param_3/", "C0", 1],
+            "SimpleAC": [DATAROOT + SHAREPATH + "SimpleAC/param_6/", "limegreen", 3],
+            "GAC": [DATAROOT + SHAREPATH + "GAC/param_0", "C1", 5],
+        }
+        best_offline(pths, "best_changeAction_clip_replay0_const_pid_discrete", ylim=[-50, 2])
+
+    # direct_action()
     change_action_continuous()
-    change_action_discrete()
+    # change_action_discrete()
+    # clip_change_action()
 
 def constant_pid_target0_replay100():
     def direct_action():
@@ -110,23 +130,32 @@ def visualize():
         # "env_info/constrain",
         "env_info/lambda",
     ]
-    file = DATAROOT + "output/test_v0/TTChangeAction/ConstPID/visualize/GAC/param_0/seed_0"
-    visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=None)
-    visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=[2950, 3050])
-    visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=[3600, 3700])
-    visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=[1850, 1950])
-    visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=[1000, 1500])
-    visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=[500, 1000])
-    visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=[0, 500])
+    def change_action_continuous_replay0(target_key):
+        file = DATAROOT + "output/test_v0/TTChangeAction/ConstPID/visualize/GAC/param_0/seed_0"
+        visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=None)
+        # visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=[2950, 3050])
+        # visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=[3600, 3700])
+        # visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=[1850, 1950])
+        # visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=[1000, 1500])
+        # visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=[500, 1000])
+        # visualize_training_info(file, target_key, title="vis_ChangeAction_GAC", threshold=0.99, xlim=[0, 500])
 
-    file = DATAROOT + "output/test_v0/TTChangeAction/DiscreteConstPID/visualize/GAC/param_0/seed_0"
-    visualize_training_info(file, target_key, title="vis_ChangeAction_discrete_GAC", threshold=0.99, xlim=None)
+    def change_action_discrete_replay0(target_key):
+        target_key.remove("actor_info/param2")
+        file = DATAROOT + "output/test_v0/TTChangeAction/DiscreteConstPID/visualize/GAC/param_0/seed_0"
+        visualize_training_info(file, target_key, title="vis_ChangeAction_discrete_GAC", threshold=0.99, xlim=None, ylim=[-2, 2])
+    def direct_action_replay0(target_key):
+        file = DATAROOT + "output/test_v0/TTAction/ConstPID/visualize/replay0/GAC/param_0/seed_0"
+        visualize_training_info(file, target_key, title="vis_DirectAction_GAC_replay0", threshold=0.99, xlim=None)
 
-    file = DATAROOT + "output/test_v0/TTAction/ConstPID/visualize/replay0/GAC/param_0/seed_0"
-    visualize_training_info(file, target_key, title="vis_DirectAction_GAC_replay0", threshold=0.99, xlim=None)
+    def direct_action_replay100(target_key):
+        file = DATAROOT + "output/test_v0/TTAction/ConstPID/visualize/replay100/GAC/param_0/seed_0"
+        visualize_training_info(file, target_key, title="vis_DirectAction_GAC_replay100", threshold=0.99, xlim=None)
 
-    file = DATAROOT + "output/test_v0/TTAction/ConstPID/visualize/replay100/GAC/param_0/seed_0"
-    visualize_training_info(file, target_key, title="vis_DirectAction_GAC_replay100", threshold=0.99, xlim=None)
+    # change_action_continuous_replay0(copy.deepcopy(target_key))
+    change_action_discrete_replay0(copy.deepcopy(target_key))
+    direct_action_replay0(copy.deepcopy(target_key))
+    direct_action_replay100(copy.deepcopy(target_key))
 
 
 if __name__ == '__main__':
@@ -136,10 +165,12 @@ if __name__ == '__main__':
     # SHAREPATH = "output/test_v0/TTAction/ConstPID/learning_rate/target0/replay100_batch32/env_scale_10/"
     # SHAREPATH = "output/test_v0/TTChangeAction/ConstPID/learning_rate/target0/replay100_batch32/env_scale_1/action_-0.1_0.1/"
     # SHAREPATH = "output/test_v0/TTChangeAction/DiscreteConstPID/learning_rate/target0/replay100_batch32/env_scale_1/change_0.01/"
+    # SHAREPATH = "output/test_v0/TTChangeAction/ClipConstPID/learning_rate/target0/replay0/env_scale_1/action_-0.1_0.1/"
+    # SHAREPATH = "output/test_v0/TTChangeAction/ClipDiscreteConstPID/learning_rate/target0/replay0/env_scale_1/change_0.01/"
     # pth_base = DATAROOT + SHAREPATH
     # sweep_parameter(pth_base)
 
     # demo()
-    constant_pid_target0_replay0()
-    constant_pid_target0_replay100()
-    # visualize()
+    # constant_pid_target0_replay0()
+    # constant_pid_target0_replay100()
+    visualize()
