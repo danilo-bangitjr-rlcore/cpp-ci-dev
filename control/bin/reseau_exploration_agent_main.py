b801237f0882d6d64034108a5c8319172a00478a
Revan Macqueen <revanmacqueen@Revans-MBP.amii.ca>
2024-01-26 14:11:33 -0700

    merged

--- control/bin/reseau_exploration_agent_main.py
+++ control/bin/reseau_exploration_agent_main.py
@@ -12,16 +12,28 @@ print("Change dir to", os.getcwd())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="run_file")
+    # parser.add_argument('--init_fpm', default=200, type=int)
+    # parser.add_argument('--reset', default=True, type=bool)
+    # parser.add_argument('--reset_fpm', default=50, type=int) # Should it be 15?
+    # parser.add_argument('--reset_duration', default=1200, type=int) # 20 minutes
+    # parser.add_argument('--fpm_min', default=10, type=int)
+    # parser.add_argument('--fpm_max', default=200, type=int)
+    # parser.add_argument('--num_fpms', default=100, type=int)
+    # parser.add_argument('--orp_delay_start_times', default=['09:10:00', '13:10:00'], type=str, nargs='+')
+    # parser.add_argument('--orp_delay_duration', default=9600, type=int) # Duration of ORP Delay Experiment in seconds (9600 = 2 hours 40 minutes)
+    # parser.add_argument('--orp_threshold_factor', default=2.0, type=float)
+
     parser.add_argument('--init_fpm', default=200, type=int)
     parser.add_argument('--reset', default=True, type=bool)
     parser.add_argument('--reset_fpm', default=50, type=int) # Should it be 15?
-    parser.add_argument('--reset_duration', default=1200, type=int) # 20 minutes
+    parser.add_argument('--reset_duration', default=20, type=int) # 20 minutes
     parser.add_argument('--fpm_min', default=10, type=int)
     parser.add_argument('--fpm_max', default=200, type=int)
     parser.add_argument('--num_fpms', default=100, type=int)
-    parser.add_argument('--orp_delay_start_times', default=['09:10:00', '13:10:00'], type=str, nargs='+')
-    parser.add_argument('--orp_delay_duration', default=9600, type=int) # Duration of ORP Delay Experiment in seconds (9600 = 2 hours 40 minutes)
+    parser.add_argument('--orp_delay_start_times', default=['02:20:00', '13:10:00'], type=str, nargs='+')
+    parser.add_argument('--orp_delay_duration', default=30, type=int) # Duration of ORP Delay Experiment in seconds (9600 = 2 hours 40 minutes)
     parser.add_argument('--orp_threshold_factor', default=2.0, type=float)
+
 
     cfg = parser.parse_args()
 
