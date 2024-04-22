aee39b1fc8cfd1aa0cb26a0d28d7a2f496a71355
Revan MacQueen <revan.macqueen@rlcore.ai>
2024-04-22 15:04:49 -0600

    merged

--- root/environment/wrapper/d4rl.py
+++ root/environment/wrapper/d4rl.py
@@ -1,32 +1,9 @@
-import gymnasium as gym
-from gymnasium.wrappers import FlattenObservation
-from gymnasium.spaces import Box
+import gym
+from gym.spaces import Box
 import numpy as np
 
-class DiscreteControlWrapper(gym.Env):
-    def __init__(self, name: str, timeout: int):
-        self.env = gym.make(name, max_episode_steps=timeout)
-        self.observation_space = self.env.observation_space
-        self.action_space = self.env.action_space
-    
-    def step(self, action: np.ndarray) -> tuple:
-        action_ = action[0]
-        return self.env.step(action_)
-        
-    def reset(self, seed: int=0) -> (np.ndarray, dict):
-        return self.env.reset(seed=seed)
-
-    def get_action_samples(self) -> (np.ndarray, tuple):
-        samples = np.arange(self.action_space.n).reshape(-1, 1)
-        shape = samples.shape
-        return samples, shape
-
-
 class D4RLWrapper:
-
-    def __init__(self, name: str, seed: int):
-        import d4rl
-        import gym
+    def __init__(self, name, seed):
         self.env = gym.make(name)
         self.env.unwrapped.seed(seed)
         self.observation_space = Box(low=self.env.observation_space.low, high=self.env.observation_space.high)
