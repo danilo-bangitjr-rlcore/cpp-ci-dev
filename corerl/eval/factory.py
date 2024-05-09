b0aee29d4d79e066b760a2b8c2a7c207eaf7363e
Samuel Neumann <samuel@rlcore.ai>
2024-05-09 13:20:11 -0600

    Add previously added evaluators to factory

--- corerl/eval/factory.py
+++ corerl/eval/factory.py
@@ -1,6 +1,8 @@
 from omegaconf import DictConfig
 
 from corerl.eval.reward import RewardEval
+from corerl.eval.state import StateEval
+from corerl.eval.envfield import EnvFieldEval
 from corerl.eval.ibe import IBE
 from corerl.eval.action_gap import ActionGapEval
 from corerl.eval.base_eval import BaseEval
@@ -12,6 +14,10 @@ def init_single_evaluator(cfg: DictConfig, eval_args: dict) -> BaseEval:
         return IBE(cfg, **eval_args)
     elif name == 'reward':
         return RewardEval(cfg, **eval_args)
+    elif name == 'state':
+        return StateEval(cfg, **eval_args)
+    elif name == 'envfield':
+        return EnvFieldEval(cfg, **eval_args)
     elif name == 'action_gap':
         return ActionGapEval(cfg, **eval_args)
     else:
