Fix bug with IBE calculation
Fixed a bug with the IBE calculation. See algorithm 1 in
https://arxiv.org/pdf/2312.02355.

corerl/eval/ibe.py
@@ -102,7 +102,7 @@ class IBE(BaseEval):
         predictions = self.model(sa)
         delta = self.get_delta(batch)
 
-        be_batch = 2 * predictions * delta - torch.square(delta)
+        be_batch = 2 * predictions * delta - torch.square(predictions)
         mean_be = torch.squeeze(torch.mean(be_batch, axis=1))
 
         return to_np(mean_be).tolist()