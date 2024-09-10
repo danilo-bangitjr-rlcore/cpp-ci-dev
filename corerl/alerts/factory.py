9952741ab0cd4e39e74b646fe257e7ed7abfba6b
Revan MacQueen <revan.macqueen@rlcore.ai>
2024-09-10 10:26:51 -0600

    added some asserts

--- corerl/alerts/factory.py
+++ corerl/alerts/factory.py
@@ -7,8 +7,11 @@ def init_alert(cfg: DictConfig, cumulant_start_ind: int, alert_args: dict) -> Ba
     """
     config files: corerl/config/alerts
     """
-    name = cfg.name
 
+    assert cfg.ensemble == cfg.buffer.ensemble == cfg.critic.critic_network.ensemble, "Ensure all config values of ensemble are equal"
+    assert cfg.reduct == cfg.critic.critic_network.reduct, "Ensure all config values of reduct are equal"
+
+    name = cfg.name
     if name == "action_value_trace":
         return ActionValueTraceAlert(cfg, cumulant_start_ind, **alert_args)
     elif name == "action_value_uncertainty":
