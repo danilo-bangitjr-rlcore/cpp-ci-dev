cd8f817896d9e9bb323394dff707a850879be2ba
Revan MacQueen <revan.macqueen@rlcore.ai>
2024-09-19 18:59:11 -0600

    Added new transition creator

--- corerl/data/transition_creator.py
+++ corerl/data/transition_creator.py
@@ -1,11 +1,14 @@
 import numpy as np
 from tqdm import tqdm
 from collections import deque
+from omegaconf import DictConfig
+from abc import ABC, abstractmethod
+
 from copy import deepcopy
 from typing import Optional
 
 from corerl.alerts.composite_alert import CompositeAlert
-from corerl.data.data import OldObsTransition, Transition, Trajectory
+from corerl.data.data import OldObsTransition, Transition, Trajectory, ObsTransition
 from corerl.state_constructor.base import BaseStateConstructor
 from corerl.interaction.anytime_interaction import AnytimeInteraction
 
@@ -351,3 +354,250 @@ class OldAnytimeTransitionCreator(object):
         new_transitions.reverse()
 
         return new_transitions
+
+
+def _get_n_step_cumulants(cumulant_queue: deque, gammas: np.ndarray) -> np.ndarray:
+    """
+    Given a queue of cumulants, return the n step cumulant where n is determined by the length of the queue
+    """
+    n_step_cumulants = np.zeros_like(cumulant_queue[0])
+    boot_len = len(cumulant_queue)
+    for step in range(boot_len):
+        cumulant_step = cumulant_queue[step]
+        n_step_cumulants += (gammas ** step) * cumulant_step
+    return n_step_cumulants
+
+
+def _get_n_step_reward(reward_queue: deque, gamma: float) -> float:
+    """
+    Given a queue of rewards, return the n step cumulant where n is determined by the length of the queue
+    """
+    n_step_reward = 0
+    boot_len = len(reward_queue)
+    for step in range(boot_len):
+        cumulant_step = reward_queue[step]
+        n_step_reward += (gamma ** step) * cumulant_step
+    return n_step_reward
+
+
+class BaseTransitionCreator(ABC):
+    def __init__(self, cfg: DictConfig, state_constuctor: BaseStateConstructor) -> None:
+        self.state = None
+        self.steps_per_decision = cfg.steps_per_decision
+        self.n_step = cfg.n_step
+        self.gamma = cfg.gamma
+        self.alert = None
+        self.state_constructor = state_constuctor
+
+        # n_step = 0: bootstrap off state at next decision point
+        # n_step > 0: bootstrap off state n steps into the future without crossing decision boundary
+        if self.n_step == 0 or self.n_step >= self.steps_per_decision:
+            self.queue_len = self.steps_per_decision
+        else:
+            self.queue_len = self.n_step
+
+        self.curr_obs_transitions = []
+        self.curr_states = []
+        self.curr_dps = []
+        self.curr_steps_until_decisions = []
+
+    def reset(self,
+              state: np.ndarray,
+              dp: bool,
+              steps_until_decision: int) -> None:
+        self.curr_obs_transitions = []
+        self.curr_states = [state]
+        self.curr_dps = [dp]
+        self.curr_steps_until_decisions = [steps_until_decision]
+
+    def feed(self,
+             obs_transition: ObsTransition,
+             next_state: np.ndarray,
+             next_dp: bool,
+             next_steps_until_decision: int) -> list[Transition]:
+        self.curr_obs_transitions.append(obs_transition)
+        self.curr_states.append(next_state)
+        self.curr_dps.append(next_dp)
+        self.curr_steps_until_decisions.append(next_steps_until_decision)
+
+        transitions = []
+        if next_dp:
+            assert len(self.curr_states) == len(self.curr_obs_transitions) + 1, \
+                'Should be one more state than obs transition. Did you forget to call reset()?'
+
+            transitions = self.make_decision_window_transitions()  # transitions is a list of anytime transitions
+            self.curr_obs_transitions = []
+            self.curr_states = [next_state]
+            self.curr_dps = [next_dp]
+            self.curr_steps_until_decisions = [next_steps_until_decision]
+
+        return transitions
+
+    @abstractmethod
+    def make_decision_window_transitions(self) -> list[Transition]:
+        raise NotImplementedError
+
+    def init_alerts(self, alert):
+        self.alert = alert
+
+    """
+    #### Alert stuff below ####
+    """
+
+    def _get_alert_cumulants(self) -> list:
+        """
+        Get cumulants used to train alert value functions
+        Currently passes the information required for Action-Value and GVF alerts.
+        """
+
+        assert self.alert is not None, "Alert not initialized, did you forget to call init_alerts()?"
+
+        cumulants = []
+        reward_list = [curr_obs_transition.reward for curr_obs_transition in self.curr_obs_transitions]
+        next_obs_list = [curr_obs_transition.next_obs for curr_obs_transition in self.curr_obs_transitions]
+
+        for i in range(len(reward_list)):
+            cumulant_args = {"reward": reward_list[i], "obs": next_obs_list[i]}
+            curr_cumulants = self.alert.get_cumulants(**cumulant_args)
+            curr_cumulants = np.array(curr_cumulants)
+            cumulants.append(curr_cumulants)
+
+        return cumulants
+
+
+class AnytimeTransitionCreator(BaseTransitionCreator):
+    def make_decision_window_transitions(self) -> list[Transition]:
+        """
+        Produce the agent and alert state transitions using the observation transitions
+        that occur between two decision points
+        """
+        using_alerts = self.alert is not None and self.alert.get_dim() > 0
+        alert_gammas, cumulants = None, None
+        if using_alerts:
+            # Alerts can use different discount factors than the agent's value functions
+            alert_gammas = np.array(self.alert.get_discount_factors())
+            cumulants = self._get_alert_cumulants()
+
+        boot_state_queue = deque([], self.queue_len)
+        boot_obs_queue = deque([], self.queue_len)
+        boot_dp_queue = deque([], self.queue_len)
+        reward_queue = deque([], self.queue_len)
+        cumulant_queue = deque([], self.queue_len)
+
+        boot_state_queue.appendleft(self.curr_states[-1])
+        boot_obs_queue.appendleft(self.curr_obs_transitions[-1].next_obs)
+        boot_dp_queue.appendleft(self.curr_dps[-1])
+
+        new_transitions = []
+        # Iteratively create the transitions by working backwards from the decision point
+        for step_idx in range(len(self.curr_obs_transitions) - 1, -1, -1):
+            curr_obs_transition = self.curr_obs_transitions[step_idx]
+            reward = curr_obs_transition.reward
+            reward_queue.appendleft(reward)
+            cumulant_queue.appendleft(cumulants[step_idx])
+            n_step_reward = _get_n_step_reward(reward_queue, self.gamma)
+            if using_alerts:
+                n_step_cumulants = _get_n_step_cumulants(cumulant_queue, alert_gammas)
+            else:
+                n_step_cumulants = None
+
+            state = self.curr_states[step_idx]
+            obs = self.curr_obs_transitions[step_idx].obs
+            state_dp = self.curr_dps[step_idx]
+
+            transition = Transition(
+                obs=obs,
+                state=state,
+                action=curr_obs_transition.action,
+                next_obs=curr_obs_transition.next_obs,
+                next_state=self.curr_states[step_idx + 1],
+                reward=curr_obs_transition.reward,
+                n_step_reward=n_step_reward,
+                n_step_cumulants=n_step_cumulants,
+                boot_obs=boot_obs_queue[-1],
+                boot_state=boot_state_queue[-1],
+                terminated=curr_obs_transition.terminated,
+                truncate=curr_obs_transition.truncate,
+                state_dp=state_dp,
+                next_state_dp=self.curr_dps[step_idx + 1],
+                boot_state_dp=boot_dp_queue[-1],
+                gamma_exponent=self.curr_steps_until_decisions[step_idx],
+                gap=curr_obs_transition.gap,
+                steps_until_decision=self.curr_steps_until_decisions[step_idx],
+                next_steps_until_decision=self.curr_steps_until_decisions[step_idx + 1],
+            )
+            new_transitions.append(transition)
+            boot_state_queue.appendleft(state)
+            boot_obs_queue.appendleft(obs)
+            boot_dp_queue.appendleft(state_dp)
+
+        new_transitions.reverse()
+        return new_transitions
+
+
+class RegularRLTransitionCreator(BaseTransitionCreator):
+
+    def make_decision_window_transitions(self) -> list[Transition]:
+        """
+        Produce the agent and alert state transitions using the observation transitions
+        that occur between two decision points.
+        """
+
+        if len(self.curr_obs_transitions) < self.steps_per_decision:
+            return []
+        elif len(self.curr_obs_transitions) > self.steps_per_decision:
+            assert False, "There should not be more than self.steps_per_decision obs transitions in len(self.curr_obs_transitions)"
+
+        using_alerts = self.alert is not None and self.alert.get_dim() > 0
+        n_step_cumulants = None
+        if using_alerts:
+            alert_gammas = np.array(
+                self.alert.get_discount_factors())  # Alerts can use different discount factors than the agent's value functions
+            cumulants = self._get_alert_cumulants()
+            cumulant_queue = deque(cumulants)
+            n_step_cumulants = _get_n_step_cumulants(cumulant_queue, alert_gammas)
+
+        rewards = [obs_transition.reward for obs_transition in self.curr_obs_transitions]
+        reward_queue = deque(rewards)
+        n_step_reward = _get_n_step_reward(reward_queue, self.gamma)
+
+        first_obs_transition = self.curr_obs_transitions[0]
+        last_obs_transition = self.curr_obs_transitions[-1]
+        first_state = self.curr_states[0]
+        next_state = self.curr_states[1]
+        last_state = self.curr_states[-1]
+
+        first_dp = self.curr_dps[0]
+        next_dp = self.curr_dps[1]
+        last_dp = self.curr_dps[-1]
+        assert first_dp
+        assert last_dp
+
+        gamma_exp = len(self.curr_obs_transitions)
+        first_step_until_decision = self.curr_steps_until_decisions[0]
+        assert first_step_until_decision == self.steps_per_decision
+
+        transition = Transition(
+            obs=first_obs_transition.obs,
+            state=first_state,
+            action=first_obs_transition.action,
+            next_obs=first_obs_transition.next_obs,
+            next_state=next_state,
+            reward=first_obs_transition.reward,
+            n_step_reward=n_step_reward,
+            n_step_cumulants=n_step_cumulants,
+            boot_obs=last_obs_transition.next_obs,
+            boot_state=last_state,
+            terminated=last_obs_transition.terminated,
+            truncate=last_obs_transition.truncate,
+            state_dp=first_dp,
+            next_state_dp=next_dp,
+            boot_state_dp=last_dp,
+            gamma_exponent=gamma_exp,
+            gap=last_obs_transition.gap,
+            steps_until_decision=first_step_until_decision,
+            next_steps_until_decision=first_step_until_decision - 1,
+            boot_steps_until_decision=first_step_until_decision
+        )
+
+        return [transition]
