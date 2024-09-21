4171b051368fda5e98b24830e59cbbf7d8c55504
Andy Patterson <andy@rlcore.ai>
2024-09-21 12:07:10 -0600

    style: use common abbreviations in short utility
    Typically we want to keep variable names clear and descriptive
    However, in some cases where the context is obviously bounded
    and there are commonly used abbreviations, the reduction in
    visual noise is far more valuable than the opacity in variable
    names.

--- corerl/utils/dict.py
+++ corerl/utils/dict.py
@@ -8,12 +8,12 @@ def drop(d: MutableMapping, to_drop: Sequence[str]) -> dict:
      }
 
 
-def flatten(dictionary: MutableMapping, parent_key: str = "", separator: str = "_") -> dict:
+def flatten(d: MutableMapping, parent_key: str = "", separator: str = "_") -> dict:
     items = []
-    for key, value in dictionary.items():
-        new_key = parent_key + separator + key if parent_key else key
-        if isinstance(value, MutableMapping):
-            items.extend(flatten(value, new_key, separator=separator).items())
+    for k, v in d.items():
+        new_key = parent_key + separator + k if parent_key else k
+        if isinstance(v, MutableMapping):
+            items.extend(flatten(v, new_key, separator=separator).items())
         else:
-            items.append((new_key, value))
+            items.append((new_key, v))
     return dict(items)
