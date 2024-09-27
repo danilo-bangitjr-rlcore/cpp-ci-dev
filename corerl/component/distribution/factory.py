05ff149b55716751abe88af79b45ba5c0681c849
Samuel Neumann <samuel@rlcore.ai>
2024-09-27 08:16:47 -0600

    fix: add missing return statement

--- corerl/component/distribution/factory.py
+++ corerl/component/distribution/factory.py
@@ -14,13 +14,13 @@ _dist_types = {
 }
 
 
-def get_dist_type(type_):
+def get_dist_type(type_) -> d.Distribution:
     if type_.lower() in _dist_types.keys():
         return _dist_types[type_.lower()]
     else:
         try:
             # Try to get the distribution from torch.distributions
-            getattr(d, type_)
+            return getattr(d, type_)
         except AttributeError:
             raise NotImplementedError(
                 f"unknown policy type '{type_}', known policy types include " +
