danilo-bangitjr-rlcore/cpp-ci-dev

commit 8e57989a81395c99a221f1551dbe7fe105a4151d

chore: deprecate functions in `network.utils`

corerl/component/network/utils.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 from corerl.utils.device import device as global_device
 from typing import Optional
 from corerl.component.layer import Identity
+import warnings
+import corerl.component.layer.activations as activations
 
 
 class Float(torch.nn.Module):
@@ -171,29 +173,21 @@ def to_np(t: numpy.ndarray | torch.Tensor) -> numpy.ndarray:
 
 
 def init_activation(name: str) -> nn.Module:
-    if name.lower() == "relu":
-        return torch.nn.ReLU
-    elif name.lower() == "softplus":
-        return torch.nn.Softplus
-    elif name.lower() == "relu6":
-        return torch.nn.ReLU6
-    elif name.lower() == "none":
-        return Identity
-    else:
-        raise NotImplementedError(f"unknown activation function {name}")
+    warnings.warn(
+        "init_activation in module utils is deprecated and will be removed, " +
+        "use activations.init_activation instead"
+    )
+
+    return activations.init_activation({"name": name})
 
 
 def init_activation_function(name: str):
-    if name.lower() == "relu":
-        return torch.nn.functional.relu
-    elif name.lower() == "softplus":
-        return torch.nn.functional.softplus
-    elif name.lower() == "relu6":
-        return torch.nn.functional.relu6
-    elif name.lower() == "none":
-        return Identity()
-    else:
-        raise NotImplementedError(f"unknown activation function {name}")
+    warnings.warn(
+        "init_activation in module utils is deprecated and will be removed, " +
+        "use activations.init_activation instead"
+    )
+
+    return activations.init_activation({"name": name})()
 
 
 def init_layer(init: str) -> callable:

commit c0761b04d9946a352dabc394d3704f8a1fbbba59

fix: address bug introduced in 8e57989a
Fixed a bug introduced in commit `8e57989a` where the `init_activation`
function from the `corerl.component.network.utils` module returned a
`torch.nn.Module` object while callers expected that the actual class
type was returned.

corerl/component/network/utils.py
@@ -178,7 +178,7 @@ def init_activation(name: str) -> type[nn.Module]:
         "use activations.init_activation instead"
     )
 
-    return activations.init_activation({"name": name})
+    return type(activations.init_activation({"name": name}))
 
 
 def init_activation_function(name: str) -> nn.Module: