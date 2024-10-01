2a06b70990c2dce964806e3d6973f4aa9b1c27e6
Andy Patterson <andy@rlcore.ai>
2024-10-01 10:44:54 -0600

    feat: add dictionary hashing utility
    The current hashing implementaiton is not robust to insertion order for
    dictionaries, which means that reordering one of our configs will lead
    to a different hash (even if the observable underlying config remains
    the same).
    
    This utility resolves that issue by ensuring a consistent ordering
    by sorting the keys alphabetically.
    
    Now that we are taking ownership of the hashing strategy, we can also
    make it more feature complete to simplify the business logic throughout
    the codebase. This is towards a larger initiative to separate business
    logic from implementation detail. An example would be ignoring keys
    while performing the hash (e.g. seed).

--- corerl/utils/dict.py
+++ corerl/utils/dict.py
@@ -1,4 +1,10 @@
-from collections.abc import MutableMapping, Sequence
+from __future__ import annotations
+
+import hashlib
+from collections.abc import Iterable, MutableMapping, Sequence
+from typing import Any
+
+import corerl.utils.nullable as nullable
 
 
 def drop(d: MutableMapping, to_drop: Sequence[str]) -> dict:
@@ -25,3 +31,32 @@ def flatten(
             _carry[new_k] = v
 
     return _carry
+
+
+def hash(
+    d: MutableMapping[str, Any],
+    ignore: Iterable[str] | None = None,
+    _parent_path: str = '',
+    _hash: hashlib._Hash | None = None,
+) -> str:
+    ignore = nullable.default(ignore, set)
+    _hash = nullable.default(_hash, hashlib.sha1)
+
+    for k in sorted(d.keys()):
+        path = _parent_path + k
+
+        if path in ignore:
+            continue
+
+        if isinstance(d[k], MutableMapping):
+            hash(
+                d[k],
+                ignore,
+                path + '.',
+                _hash,
+            )
+
+        else:
+            _hash.update(str(d[k]).encode('utf-8'))
+
+    return _hash.hexdigest()
