1923368c8c84e3df7c3d46371a6ec24f37ca4896
Andy Patterson <andy@rlcore.ai>
2024-10-03 16:01:45 -0600

    test: add smoke test for pendulum

--- test/smoke/test_main.py
+++ test/smoke/test_main.py
@@ -1,13 +1,19 @@
+import  pytest
 import subprocess
 
-def test_saturation():
+
+@pytest.mark.parametrize('config', [
+    'pendulum',
+    'saturation',
+])
+def test_main_configs(config: str):
     """
-    Should be able to execute the main script with the saturation experiment
+    Should be able to execute the main script for several configs
     without error. If an error code is returned (i.e. the process crashes),
     then test fails.
 
     This test does no checking of result validity.
     """
 
-    proc = subprocess.run(['uv', 'run', 'python', 'main.py', '--config-name', 'saturation.yaml', 'experiment.max_steps=25'])
+    proc = subprocess.run(['uv', 'run', 'python', 'main.py', '--config-name', f'{config}.yaml', 'experiment.max_steps=25'])
     proc.check_returncode()
