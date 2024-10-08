595749aa348d694a8039c1b566ee4281ce34da34
Revan MacQueen <revan.macqueen@rlcore.ai>
2024-10-08 15:27:41 -0700

    Started on transition_creator unit_tests. Currently they check if dp attributes are set properly and states are linked together correctly.

--- test/unit/utils/state_constructor.py
+++ test/unit/utils/state_constructor.py
@@ -4,30 +4,15 @@ from omegaconf import DictConfig
 from corerl.state_constructor.examples import MultiTrace, AnytimeMultiTrace
 
 
-@pytest.fixture
-def multi_trace():
+def make_anytime_multi_trace(warmup, steps_per_decision):
     cfg_d = {
         'trace_values': [0.9],
-        'warmup': 10,
-        'name': 'multi_trace'
-    }
-    cfg = DictConfig(cfg_d)
-    sc = MultiTrace(cfg)
-    return sc
-
-
-@pytest.fixture
-def anytime_multi_trace():
-    cfg_d = {
-        'trace_values': [0.9],
-        'warmup': 10,
+        'warmup': warmup,
         'name': 'anytime_multi_trace',
         'representation': 'countdown',
-        'steps_per_decision': 10,  # TODO: how can I get this to be set somewhere else?
+        'steps_per_decision': steps_per_decision,
         'use_indicator': False
     }
     cfg = DictConfig(cfg_d)
     sc = AnytimeMultiTrace(cfg)
     return sc
-
-
--- test/unit/utils/transition_creator.py
+++ test/unit/utils/transition_creator.py
@@ -1,22 +1,139 @@
 import pytest
+import numpy as np
 from omegaconf import DictConfig
 
 from corerl.data.transition_creator import AnytimeTransitionCreator, RegularRLTransitionCreator
+from corerl.data.data import ObsTransition
+from test.unit.utils.state_constructor import make_anytime_multi_trace
 
 
+def _make_anytime_transition_creator(sc, steps_per_decision, n_step) -> AnytimeTransitionCreator:
+    cfg_d = {
+        'steps_per_decision': steps_per_decision,
+        'n_step': n_step,
+        'gamma': 0.9,
+        'transition_kind': 'anytime'
+    }
+    cfg = DictConfig(cfg_d)
+    return AnytimeTransitionCreator(cfg, sc)
 
-@pytest.fixture
-def anytime_transition_creator():
-    pass
 
-@pytest.fixture
-def anytime_transition_creator():
-    pass
-    # cfg_d = {
-    #     'steps_per_decision': 3,
-    #     'n_step': 0,
-    #     'gamma': 0.9,
-    #     'transition_kind': 'anytime'
-    # }
-    # cfg = DictConfig(cfg_d)
+def _make_simple_obs_sequence(num_observations: int) -> list[np.array]:
+    obs_sequence = [np.array([1])]
+    for i in range(num_observations - 1):
+        obs_sequence.append(np.array([0]))
+    return obs_sequence
 
+
+def _make_simple_obs_transition_sequence(num_observations: int):
+    observations = _make_simple_obs_sequence(num_observations)
+    action = np.array([1])
+    reward = 1
+
+    obs_transitions = []
+    for i in range(len(observations) - 1):
+        new_obs_transition = ObsTransition(
+            obs=observations[i],
+            action=action,
+            reward=reward,
+            next_obs=observations[i + 1],
+            terminated=False,
+            truncate=False,
+            gap=False
+        )
+        obs_transitions.append(new_obs_transition)
+
+    return obs_transitions
+
+
+def _reset_sc(sc, obs_transitions):
+    sc.reset()
+    initial_obs = obs_transitions[0].obs
+    initial_action = obs_transitions[0].obs
+    dummy_action = np.zeros_like(initial_action)
+    state = sc(initial_obs, dummy_action, initial_state=True, decision_point=True)
+    return sc, state
+
+
+def test_anytime_transition_creator_init():
+    tc = make_anytime_multi_trace(warmup=0, steps_per_decision=5)
+    assert tc is not None
+
+
+def _check_dps(i, steps_per_decision, transition, n_step):
+    steps_until_decision = steps_per_decision - (i % steps_per_decision)
+    # check if state_dp set properly
+    if i % steps_per_decision == 0:
+        assert transition.state_dp
+
+    # check if boot_state_dp set properly
+    if n_step == 0:
+        assert transition.boot_state_dp
+    else:
+        if steps_until_decision == 0:
+            steps_until_decision = steps_per_decision
+
+        if n_step >= steps_until_decision:
+            assert transition.boot_state_dp
+
+    # check if next_state_dp set properly
+    if steps_until_decision == 1:
+        assert transition.next_state_dp
+
+
+@pytest.mark.parametrize("num_observations, steps_per_decision, n_step",
+    [
+        (10, 1, 0),
+        (21, 2, 0),
+        (1001, 5, 0),
+        (101, 10, 0),
+        (100, 3, 1),
+        (10, 5, 2),
+
+    ])
+def test_anytime_transition_creator_feed(
+        num_observations,
+        steps_per_decision,
+        n_step
+):
+    sc = make_anytime_multi_trace(warmup=0, steps_per_decision=steps_per_decision)
+    tc = _make_anytime_transition_creator(sc, steps_per_decision, n_step)
+
+    obs_transitions = _make_simple_obs_transition_sequence(num_observations)
+    sc, state = _reset_sc(sc, obs_transitions)
+
+    steps_until_decision = steps_per_decision
+
+    tc.reset(state, dp=True, steps_until_decision=steps_until_decision)
+    transitions = []
+
+    for i, obs_transition in enumerate(obs_transitions):
+        steps_until_decision -= 1
+
+        if steps_until_decision == 0:
+            decision_point = True
+            steps_until_decision = steps_per_decision
+        else:
+            decision_point = False
+
+        next_state = sc(obs_transition.next_obs, obs_transition.action, decision_point=decision_point)
+        new_transitions = tc.feed(
+            obs_transition=obs_transition,
+            next_state=next_state,
+            next_dp=decision_point,
+            next_steps_until_decision=steps_until_decision)
+        transitions += new_transitions
+
+        if decision_point:
+            assert len(new_transitions)
+        else:
+            assert not len(new_transitions)
+
+    for i, transition in enumerate(transitions):
+        last_transition = i == len(transitions) - 1
+        transition = transitions[i]
+        _check_dps(i, steps_per_decision, transition, n_step)
+
+        if not last_transition:
+            next_transition = transitions[i + 1]
+            assert np.allclose(transition.next_state, next_transition.state)
