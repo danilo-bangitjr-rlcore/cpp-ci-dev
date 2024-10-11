0c97d0db3ed2dfa6dc8e6cdadaab66ac977aef6a
Andy Patterson <andy@rlcore.ai>
2024-10-10 20:49:24 -0600

    refactor: move get_open_port to shared utility

--- test/unit/utils/fixture_opc.py
+++ test/unit/utils/fixture_opc.py
@@ -1,7 +1,7 @@
-import socket
 import pytest_asyncio
 from asyncua import Server, Node
 from corerl.utils.opc_connection import OpcConnection
+from test.infrastructure.networking import get_free_port
 
 
 class FakeOpcServer:
@@ -76,19 +76,3 @@ async def server_and_client():
 
     await client.disconnect()
     await server.close()
-
-
-
-def get_free_port(host: str):
-    # binding to port 0 will ask the OS to give us an arbitrary free port
-    # since we've just bound that free port, it is by definition no longer free,
-    # so we set that port as reuseable to allow another socket to bind to it
-    # then we immediately close the socket and release our connection.
-    sock = socket.socket()
-    sock.bind((host, 0))
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    port: int = sock.getsockname()[1]
-    sock.close()
-
-    return port
