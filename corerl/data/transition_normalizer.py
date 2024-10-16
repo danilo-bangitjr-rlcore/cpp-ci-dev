e781a12c92f05e7b751497687d75334ccb21e00b
Andy Patterson <andy@rlcore.ai>
2024-10-15 20:52:12 -0600

    fix: add missing type annotations

--- corerl/data/transition_normalizer.py
+++ corerl/data/transition_normalizer.py
@@ -1,16 +1,19 @@
 import logging
+import gymnasium as gym
 from copy import deepcopy
+from omegaconf import DictConfig
+from corerl.data.data import Transition
 from corerl.data.normalizer_utils import InvertibleNormalizer, init_action_normalizer, init_reward_normalizer, init_obs_normalizer
 
 log = logging.getLogger(__name__)
 
 class TransitionNormalizer:
-    def __init__(self, cfg, env):
+    def __init__(self, cfg: DictConfig, env: gym.Env):
         self.action_normalizer = init_action_normalizer(cfg.action_normalizer, env)
         self.reward_normalizer = init_reward_normalizer(cfg.reward_normalizer)
         self.obs_normalizer = init_obs_normalizer(cfg.obs_normalizer, env)
 
-    def denormalize(self, transition):
+    def denormalize(self, transition: Transition):
         transition_copy = deepcopy(transition)
 
         transition_copy.obs = self.obs_normalizer.denormalize(transition_copy.obs)
