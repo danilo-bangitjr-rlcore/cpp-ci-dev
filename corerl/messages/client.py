e251bd0dae3ebaaae884a6c34ac082c5799e4c29
Andy Patterson <andy@rlcore.ai>
2024-10-16 14:13:23 -0600

    feat: allow client to subscribe callbacks to an event

--- corerl/messages/client.py
+++ corerl/messages/client.py
@@ -1,4 +1,6 @@
 import asyncio
+from collections import defaultdict
+from collections.abc import Callable
 import logging
 from asyncio import AbstractEventLoop
 from concurrent.futures import ThreadPoolExecutor, Future
@@ -6,10 +8,13 @@ from typing import Protocol
 from websockets import ConnectionClosed
 from websockets.asyncio.client import connect, ClientConnection
 
-from corerl.messages.events import Event, EventType
+from corerl.messages.events import Event, EventType, SubscribeEvent, maybe_parse_event
+from corerl.utils.asyncio import MaybeAwaitable, maybe_await
 
 
 logger = logging.getLogger(__name__)
+
+Callback = Callable[[Event], MaybeAwaitable[None]]
 
 # ----------------
 # -- Websockets --
@@ -25,6 +30,8 @@ class WebsocketClient:
         self._reconnect_future: asyncio.Future | None = None
         self._loop: AbstractEventLoop | None = None
         self._pool: ThreadPoolExecutor | None = None
+
+        self._callbacks: dict[EventType, list[Callback]] = defaultdict(list)
 
 
     # ---------------
@@ -47,6 +54,16 @@ class WebsocketClient:
         maybe_cancel(self._reconnect_future)
         await maybe_close(self._websocket)
 
+
+    async def subscribe(
+        self,
+        to: EventType,
+        cb: Callable[[Event], MaybeAwaitable[None]],
+    ):
+        sub_event = SubscribeEvent(subscribe_to=to)
+        await self.emit_event(sub_event)
+
+        self._callbacks[to].append(cb)
 
     async def send_message(self, msg: str):
         try:
@@ -75,6 +92,10 @@ class WebsocketClient:
         assert self._websocket is not None
         return self._websocket
 
+
+    async def listen_forever(self):
+        assert self._reconnect_future is not None
+        await self._reconnect_future
 
     # --------------
     # -- Sync API --
@@ -141,14 +162,30 @@ class WebsocketClient:
                 self._first_connect_future.set_result(None)
 
             try:
-                msg = await socket.recv()
-                logging.debug(f'Received msg: <{msg}>')
+                await self._listen_forever(socket)
             except ConnectionClosed:
                 logging.exception('Connection closed unexpectedly')
 
             if max_attempts != -1 and attempts >= max_attempts:
                 break
 
+
+    async def _listen_forever(self, socket: ClientConnection):
+        while True:
+            msg = await socket.recv(decode=False)
+            logging.debug(f'Received msg: <{msg}>')
+            await self._handle_msg(msg)
+
+
+    async def _handle_msg(self, msg: str | bytes):
+        event = maybe_parse_event(msg)
+        if event is None:
+            return
+
+        await asyncio.gather(*[
+            maybe_await(cb(event))
+            for cb in self._callbacks[event.type]
+        ])
 
 # ------------------
 # -- Dummy Client --
