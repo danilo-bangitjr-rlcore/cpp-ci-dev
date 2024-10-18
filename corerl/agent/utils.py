c34f70a8722bf4b10769e07fbdbd59be4900d0ad
Andy Patterson <andy@rlcore.ai>
2024-10-18 14:52:41 -0600

    fix: remove default value bug in get_batch_actions_discrete

--- corerl/agent/utils.py
+++ corerl/agent/utils.py
@@ -1,21 +1,20 @@
 import torch
 import numpy as np
 from jaxtyping import Float
-from typing import Optional
 
 from corerl.component.network.utils import to_np, tensor
 from corerl.utils.device import device
 
 
 def get_batch_actions_discrete(state_batch: Float[torch.Tensor, "batch_size state_dim"], action_dim: int,
-                               samples=Optional[int]) \
+                               samples: int | None = None) \
         -> Float[torch.Tensor, "batch_size*action_dim action_dim"]:
     batch_size = state_batch.shape[0]
     if samples is None:
         actions = torch.arange(action_dim).reshape((1, -1))
         actions = actions.repeat_interleave(batch_size, dim=0).reshape((-1, 1))
     else:
-        actions = torch.randint(high=action_dim, size=(batch_size * samples)).reshape((-1, 1))
+        actions = torch.randint(high=action_dim, size=(batch_size * samples,)).reshape((-1, 1))
 
     a_onehot = torch.FloatTensor(actions.size()[0], action_dim)
     a_onehot.zero_()
