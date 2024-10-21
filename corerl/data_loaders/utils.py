380171a7444a27adb85b6dff7d802c6a6df6aa51
Andy Patterson <andy@rlcore.ai>
2024-10-20 21:00:03 -0600

    fix: fix type and style issues in data_loaders utils

--- corerl/data_loaders/utils.py
+++ corerl/data_loaders/utils.py
@@ -1,7 +1,8 @@
 import random
+from typing import Any
 
 
-def train_test_split(*lsts, train_split: float = 0.9, shuffle: bool = True) -> tuple[list[tuple], list[tuple]]:
+def train_test_split(*lsts, train_split: float = 0.9, shuffle: bool = True) -> list[tuple[Any, Any]]:
     num_samples = len(lsts[0])
     for a in lsts:
         assert len(a) == num_samples
@@ -13,11 +14,11 @@ def train_test_split(*lsts, train_split: float = 0.9, shuffle: bool = True) -> t
     train_samples = [lsts[:num_train_samples] for lsts in lsts]
     test_samples = [lsts[num_train_samples:] for lsts in lsts]
 
-    return list(zip(train_samples, test_samples))
+    return list(zip(train_samples, test_samples, strict=True))
 
 
 def parallel_shuffle(*args):
-    zipped_list = list(zip(*args))
+    zipped_list = list(zip(*args, strict=True))
     random.shuffle(zipped_list)
-    unzipped = zip(*zipped_list)
+    unzipped = zip(*zipped_list, strict=True)
     return list(unzipped)
