eb266ebfd261f44078f773b3b3e0d1e82fbba3fe
kerrick-rlcore <kerrick@rlcore.ai>
2024-10-23 16:18:30 -0600

    feat: add docker compose and handle setup/teardown in data_writer test file

--- test/unit/data_loaders/test_data_writer.py
+++ test/unit/data_loaders/test_data_writer.py
@@ -1,8 +1,33 @@
 from corerl.data_loaders.data_writer import DataWriter
 from datetime import datetime, UTC
+import subprocess
+from pathlib import Path
+import logging
+import pytest
+
+
+@pytest.fixture(scope="module", autouse=True)
+def docker_compose_up():
+    compose_path = Path.cwd() / Path("test/integration/assets/compose.yaml")
+    logging.info(compose_path)
+
+    proc = subprocess.run(['docker', 'compose', '-f', f'{compose_path}', 'up', 'timescaledb', '-d'])
+    yield
+
+    subprocess.run(['docker', 'compose', '-f', f'{compose_path}', 'down', 'timescaledb'])
+    return_code = proc.returncode
+    logging.info(return_code)
+    proc.check_returncode()
+
 
 def test_writing_datapt():
-    data_writer = DataWriter(username='postgres', password='password', db_name='sensors')
+    proc = subprocess.run(["docker", "ps", "--filter", "name=assets-timescaledb-1", "--format", "{{.ID}}"], capture_output=True)
+
+    out = proc.stdout
+    print(out) # for debugging if test fails
+    assert out is not None
+    
+    data_writer = DataWriter(username="postgres", password="password", db_name="sensors")
 
     ts = datetime.now(tz=UTC)
     sensor_name = "orp"
