282a8e683f6e8a2323b404a6baa7c6ef90c92001
Andy Patterson <andy@rlcore.ai>
2024-10-25 22:03:57 -0600

    test: fix anytimemultitrace builder in unit tests

--- test/small/state_constructor/state_constructor.py
+++ test/small/state_constructor/state_constructor.py
@@ -1,17 +1,16 @@
-from omegaconf import DictConfig
+from typing import Any
 
-from corerl.state_constructor.examples import AnytimeMultiTrace
+from corerl.state_constructor.examples import AnytimeMultiTrace, AnytimeMultiTraceConfig
 
 
 def make_anytime_multi_trace(warmup: int, steps_per_decision: int):
-    cfg_d = {
-        'trace_values': [0.9],
-        'warmup': warmup,
-        'name': 'anytime_multi_trace',
-        'representation': 'countdown',
-        'steps_per_decision': steps_per_decision,
-        'use_indicator': False
-    }
-    cfg = DictConfig(cfg_d)
-    sc = AnytimeMultiTrace(cfg)
+    cfg = AnytimeMultiTraceConfig(
+        trace_values=[0.9],
+        warmup=warmup,
+        representation='countdown',
+        steps_per_decision=steps_per_decision,
+        use_indicator=False,
+    )
+    env: Any = None
+    sc = AnytimeMultiTrace(cfg, env)
     return sc
