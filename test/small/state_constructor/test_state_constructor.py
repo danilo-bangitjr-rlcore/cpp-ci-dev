7b56c5b9dd4af665039397c681c698161b9a2991
Andy Patterson <andy@rlcore.ai>
2024-10-25 21:53:17 -0600

    test: test MultiTrace state_constructor
    This shows off the first integration with the hydra group
    dispatcher code, illustrating thta a MultiTrace sc is built
    from the factory simply by specifying the right config type.

---
+++ test/small/state_constructor/test_state_constructor.py
@@ -0,0 +1,50 @@
+import numpy as np
+from typing import Any
+from corerl.state_constructor.examples import MultiTrace, MultiTraceConfig
+from corerl.state_constructor.factory import init_state_constructor
+
+def test_multitrace1():
+    """
+    Calling the init_sc factory function
+    with a MultiTraceConfig gives back
+    a MultiTrace state constructor.
+    """
+    cfg = MultiTraceConfig(
+        trace_values=[0.9, 0.1],
+    )
+
+    env: Any = None
+    sc = init_state_constructor(cfg, env)
+
+    assert isinstance(sc, MultiTrace)
+
+
+def test_multitrace2():
+    """
+    MultiTrace state constructor generates a trace
+    with decay rate 0.1
+    """
+    cfg = MultiTraceConfig(
+        trace_values=[0.1],
+    )
+
+    env: Any = None
+    sc = init_state_constructor(cfg, env)
+
+    x = np.ones(2)
+    a = np.array([2.])
+    got = sc(x, a)
+
+    assert np.allclose(
+        got,
+        np.array([
+            # whether this is an initial state
+            # since we did not tell the sc that it is
+            # assume no
+            0.,
+            # the action concatenated with the obs vec
+            2., 1, 1,
+            # the trace of the [act, obs] features
+            1.8, 0.9, 0.9,
+        ]),
+    )
