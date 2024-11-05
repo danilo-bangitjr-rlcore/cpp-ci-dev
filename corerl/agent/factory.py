615980267d57db2b6eb1dcffec99ba37b96a9dc1
Andy Patterson <andy@rlcore.ai>
2024-11-05 11:00:32 -0700

    refactor: migrate greedy_ac to hydra structured config

--- corerl/agent/factory.py
+++ corerl/agent/factory.py
@@ -3,11 +3,11 @@ from corerl.agent.iql import IQL
 from corerl.agent.simple_ac import SimpleAC
 from corerl.agent.sarsa import EpsilonGreedySarsa
 from corerl.agent.inac import InAC
-from corerl.agent.greedy_ac import GreedyAC, GreedyACLineSearch, ExploreLSGAC
 from corerl.agent.greedy_iql import GreedyIQL
 from corerl.agent.random import RandomAgent
 from corerl.utils.hydra import DiscriminatedUnion
 
+import corerl.agent.greedy_ac # noqa: F401
 import corerl.agent.action_schedule # noqa: F401
 
 
@@ -29,14 +29,8 @@ def init_agent(cfg: DiscriminatedUnion, state_dim: int, action_dim: int) -> Base
         agent = InAC(cfg, state_dim, action_dim)
     elif cfg.name == 'epsilon_greedy_sarsa':
         agent = EpsilonGreedySarsa(cfg, state_dim, action_dim)
-    elif cfg.name == 'greedy_ac':
-        agent = GreedyAC(cfg, state_dim, action_dim)
     elif cfg.name == 'greedy_iql':
         agent = GreedyIQL(cfg, state_dim, action_dim)
-    elif cfg.name == 'greedy_ac_linesearch':
-        agent = GreedyACLineSearch(cfg, state_dim, action_dim)
-    elif cfg.name == 'greedy_ac_linesearch_explore':
-        agent = ExploreLSGAC(cfg, state_dim, action_dim)
     elif cfg.name == 'random':
         agent = RandomAgent(cfg, state_dim, action_dim)
     else:
