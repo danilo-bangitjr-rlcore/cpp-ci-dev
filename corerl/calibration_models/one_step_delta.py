e79b1f020c5393c0dd6dd329351d469fdd4dfa86
han-wang-rlcore <hanwang@rlcore.ai>
2024-11-06 12:26:32 -0700

    copy paste from epcor branch

---
+++ corerl/calibration_models/one_step_delta.py
@@ -0,0 +1,139 @@
+import os
+import torch
+import torch.nn as nn
+import json
+import numpy as np
+
+from tqdm import tqdm
+from omegaconf import DictConfig
+from pathlib import Path
+
+from corerl.component.buffer.factory import init_buffer
+from corerl.component.network.factory import init_custom_network
+from corerl.component.optimizers.factory import init_optimizer
+from corerl.component.network.utils import tensor, to_np
+from corerl.calibration_models.base import BaseCalibrationModel
+from corerl.utils.device import device
+
+
+class Obj:
+    # constructor
+    def __init__(self, dict1):
+        self.__dict__.update(dict1)
+    def keys(self):
+        return self.__dict__.keys()
+
+class ShortHorizonDelta(BaseCalibrationModel):
+    def __init__(self, cfg: DictConfig, train_info):
+        super().__init__(cfg, train_info)
+        train_transitions = train_info['train_transitions_cm']
+        self.rng = np.random.RandomState(cfg.seed)
+
+        self.buffer = init_buffer(cfg.buffer)
+        self.test_buffer = init_buffer(cfg.buffer)
+        self.train_itr = cfg.train_itr
+        self.batch_size = cfg.batch_size
+
+        self.buffer.load(train_transitions)
+
+        input_dim = len(train_transitions[0].state)
+        action_dim = len(train_transitions[0].action)
+        output_dim = len(train_transitions[0].obs[self.endo_inds])
+
+        self.model = init_custom_network(cfg.model, input_dim=input_dim + action_dim, output_dim=output_dim)
+        self.optimizer = init_optimizer(cfg.optimizer, list(self.model.parameters()))
+
+        self.train_losses = []
+        self.test_losses = []
+
+    def pid_estimation(self, datasets):
+
+        pid_prediction_models = []
+        linear_cfg = {
+            "name": "fc",
+            "layer_norm": 0,
+            "arch": [],
+            "head_activation": "None",
+            "activation": "ReLU",
+            "layer_init": "Xavier",
+            "bias": True,
+        }
+        linear_cfg = json.loads(json.dumps(linear_cfg), object_hook=Obj)
+        opt_cfg = {
+            "lr": 0.001,
+            "weight_decay": 0.0,
+            "name": "adam"
+        }
+        opt_cfg = json.loads(json.dumps(opt_cfg), object_hook=Obj)
+        in_sets, out_sets = datasets['inputs'], datasets['outputs']
+        for idx in range(len(in_sets)):
+            in_data, out_data = (tensor(in_sets[idx], device.device),
+                                 tensor(out_sets[idx], device.device))
+            pid_model = init_custom_network(linear_cfg, input_dim=3, output_dim=1)
+            optimizer = init_optimizer(opt_cfg, list(pid_model.parameters()))
+            data_size = len(in_data)
+            pbar = tqdm(range(self.train_itr))
+            print("Training PID parameter for controller {}".format(idx + 1))
+            for _ in pbar:
+                sampled_indices = self.rng.randint(0, data_size, self.batch_size)
+                in_batch, out_batch = in_data[sampled_indices], out_data[sampled_indices]
+                pred = pid_model(in_batch)
+                loss = torch.nn.functional.mse_loss(pred, out_batch)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                pbar.set_description("pid {} train loss: {:7.6f}".format(idx+1, to_np(loss)))
+            print("PID parameter of controller {}".format(idx + 1), [param.data for param in pid_model.parameters()])
+            pid_prediction_models.append(pid_model)
+        return pid_prediction_models
+
+    def _update(self):
+        batch = self.buffer.sample_mini_batch(self.batch_size)[0]
+        obs_batch, state_batch, action_batch, next_obs_batch = batch.obs, batch.state, batch.action, batch.next_obs
+        # we only predict the next endogenous component of the observation
+        endo_next_obs_batch = next_obs_batch[:, self.endo_inds]
+        delta_endo_next_obs_batch = endo_next_obs_batch - obs_batch[:, self.endo_inds]
+        # prediction = self.get_prediction(obs_batch, state_batch, action_batch, with_grad=True)
+        x = torch.concat((state_batch, action_batch), dim=1)
+        prediction = self.model(x)
+        loss = nn.functional.mse_loss(prediction, delta_endo_next_obs_batch)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        self.train_losses.append(loss.detach().numpy())
+
+    def train(self):
+        print('Training model...')
+        pbar = tqdm(range(self.train_itr))
+        for _ in pbar:
+            self._update()
+            pbar.set_description("train loss: {:7.6f}".format(self.train_losses[-1]))
+
+        return self.train_losses
+
+    def get_prediction(self, obs: torch.Tensor, state: torch.Tensor, action: torch.Tensor, with_grad: bool = False):
+        x = torch.concat((state, action), dim=1)
+        if with_grad:
+            y = self.model(x)
+        else:
+            with torch.no_grad():
+                y = self.model(x)
+        y += obs[:, self.endo_inds]
+        y = torch.clamp(y, min=0, max=1)
+        return y
+
+    def _get_next_endo_obs(self, state, action, kwargs):
+        state_tensor = tensor(state).reshape((1, -1))
+        action_tensor = tensor(action).reshape((1, -1))
+        return self.get_prediction(kwargs["prev_obs"], state_tensor, action_tensor)
+
+    def save(self, path) -> None:
+        path.mkdir(parents=True, exist_ok=True)
+        net_path = os.path.join(path, "model")
+        torch.save(self.model.state_dict(), net_path)
+
+    def load(self, path):
+        net_path = os.path.join(path, "model")
+        self.model.load_state_dict(torch.load(net_path, map_location=device.device))
