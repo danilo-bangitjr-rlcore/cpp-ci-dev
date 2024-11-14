a41de399d766c230f1306320d28c8e5f5875242c
Kerrick Johnstonbaugh <kerrick@rlcore.ai>
2024-11-14 12:10:30 -0700

    feat: add datareader option to aggregate using last value in bucket

--- corerl/data_loaders/data_reader.py
+++ corerl/data_loaders/data_reader.py
@@ -1,11 +1,13 @@
-from datetime import datetime, timedelta, UTC
-from corerl.sql_logging.sql_logging import get_sql_engine, SQLEngineConfig
+import logging
+from datetime import UTC, datetime, timedelta
+from typing import List
+
+import numpy as np
 import pandas as pd
 from sqlalchemy import Engine
-from typing import List
-import numpy as np
+
 from corerl.data_loaders.utils import try_connect
-import logging
+from corerl.sql_logging.sql_logging import SQLEngineConfig, get_sql_engine
 
 logger = logging.getLogger(__name__)
 
@@ -17,23 +19,23 @@ class DataReader:
         self.connection = try_connect(self.engine)
 
     def batch_aggregated_read(
-        self, names: List[str], start_time: datetime, end_time: datetime, bucket_width: timedelta
+        self,
+        names: List[str],
+        start_time: datetime,
+        end_time: datetime,
+        bucket_width: timedelta,
+        aggregation: str = "avg",
     ):
-        """
-        NOTE: Addition of bucket_width in the select statement ensures that the labels
-        for the time buckets align with the end of the bucket rather than the beginnning.
-        """
-
         query_str = f"""
             SELECT
-              time_bucket(INTERVAL '{bucket_width}', time) + '{bucket_width}' as time_bucket,
-              name,
-              avg({_parse_jsonb('fields')}) AS avg_val
+                {_time_bucket(bucket_width=bucket_width, time_col='time')} as time_bucket,
+                name,
+                {_aggregator(aggregation=aggregation, val_col=_parse_jsonb('fields'), time_col='time')} AS val
             FROM {self.sensor_table_name}
             WHERE {_time_between('time', start_time, end_time)}
             AND {_filter_any('name', names)}
             GROUP BY time_bucket, name
-            ORDER BY time_bucket DESC, name ASC;
+            ORDER BY time_bucket ASC, name ASC;
         """
 
         sensor_data = pd.read_sql(sql=query_str, con=self.connection)
@@ -41,24 +43,28 @@ class DataReader:
             logger.warning(f"failed query:\n{query_str}")
             raise Exception("dataframe returned from timescale was empty.")
 
-        sensor_data = sensor_data.pivot(columns="name", values="avg_val", index="time_bucket")
+        sensor_data = sensor_data.pivot(columns="name", values="val", index="time_bucket")
 
         missing_cols = set(names) - set(sensor_data.columns)
         sensor_data[list(missing_cols)] = np.nan
 
         return sensor_data
 
-    def single_aggregated_read(self, names: List[str], start_time: datetime, end_time: datetime):
+    def single_aggregated_read(
+        self, names: List[str], start_time: datetime, end_time: datetime, aggregation: str = "avg"
+    ):
+        bucket_width = end_time - start_time
 
         query_str = f"""
             SELECT
-              name,
-              avg({_parse_jsonb('fields')}) AS avg_val
+                {_time_bucket(bucket_width=bucket_width, time_col='time', origin=start_time)} as time_bucket,
+                name,
+                {_aggregator(aggregation=aggregation, val_col=_parse_jsonb('fields'), time_col='time')} AS val
             FROM {self.sensor_table_name}
             WHERE {_time_between('time', start_time, end_time)}
             AND {_filter_any('name', names)}
-            GROUP BY name
-            ORDER BY name ASC;
+            GROUP BY time_bucket, name
+            ORDER BY time_bucket ASC, name ASC;
         """
 
         sensor_data = pd.read_sql(sql=query_str, con=self.connection)
@@ -67,8 +73,13 @@ class DataReader:
             raise Exception("dataframe returned from timescale was empty.")
 
         # add time column to enable pivot
-        sensor_data["time"] = end_time
-        sensor_data = sensor_data.pivot(columns="name", values="avg_val", index="time")
+        sensor_data = sensor_data.pivot(columns="name", values="val", index="time_bucket")
+        n_rows = sensor_data.shape[0]
+        if n_rows != 1:
+            logger.warning(
+                f"single_aggregated_read returned {n_rows}, expected 1. Taking the last row (newest data)..."
+            )
+            sensor_data = sensor_data.iloc[-1]
 
         missing_cols = set(names) - set(sensor_data.columns)
         sensor_data[list(missing_cols)] = np.nan
@@ -78,6 +89,31 @@ class DataReader:
     def close(self) -> None:
         self.connection.close()
 
+
+def _time_bucket(bucket_width: timedelta, time_col: str, origin: datetime | None = None) -> str:
+    """
+    NOTE: Addition of bucket_width in the timebucket definition ensures that the labels
+    for the time buckets align with the end of the bucket rather than the beginnning.
+    """
+    if origin is None:
+        return f"time_bucket(INTERVAL '{bucket_width}', {time_col}) + '{bucket_width}'"
+    else:
+        assert origin.tzinfo == UTC
+        origin_ts = f"TIMESTAMP '{origin.isoformat()}'"
+        return f"time_bucket(INTERVAL '{bucket_width}', {time_col}, origin => {origin_ts}) + '{bucket_width}'"
+
+
+def _aggregator(aggregation: str, val_col: str, time_col: str | None = None) -> str:
+    assert aggregation in ["avg", "last"]
+    if aggregation == "avg":
+        return f"avg({val_col})"
+    elif aggregation == "last":
+        assert time_col is not None
+        return f"last({val_col}, {time_col})"
+    else:
+        raise NotImplementedError
+
+
 def _time_between(time_col: str, start: datetime, end: datetime) -> str:
     assert start.tzinfo == UTC
     assert end.tzinfo == UTC
@@ -86,9 +122,11 @@ def _time_between(time_col: str, start: datetime, end: datetime) -> str:
         AND {time_col} < TIMESTAMP '{end.isoformat()}'
     """
 
-def _parse_jsonb(col: str, attribute: str = 'val', type_str: str = 'float') -> str:
+
+def _parse_jsonb(col: str, attribute: str = "val", type_str: str = "float") -> str:
     return f"({col}->'{attribute}')::{type_str}"
 
+
 def _filter_any(col: str, vals: list[str]) -> str:
-    s = '\tOR '.join([f"{col} = '{v}'\n" for v in vals])
-    return f'({s})'
+    s = "\tOR ".join([f"{col} = '{v}'\n" for v in vals])
+    return f"({s})"
--- test/medium/data_loaders/test_data_reader.py
+++ test/medium/data_loaders/test_data_reader.py
@@ -1,21 +1,24 @@
+from datetime import UTC, datetime, timedelta
+from typing import Generator, List
+
+import pytest
 from pandas import DataFrame
+
 from corerl.data_loaders.data_reader import DataReader
 from corerl.data_loaders.data_writer import DataWriter
-import pytest
 from corerl.sql_logging.sql_logging import SQLEngineConfig
-from test.medium.data_loaders.test_data_writer import write_n_random_vals
-from typing import Generator, List
-from datetime import datetime, timedelta, UTC
+from test.medium.data_loaders.test_data_writer import data_writer, write_n_random_vals  # noqa: F401
+from test.medium.data_loaders.utils import timescale_docker  # noqa: F401
 
 
 @pytest.fixture(scope="module")
-def data_reader(timescale_docker) -> Generator[DataReader, None, None]:
+def data_reader(timescale_docker) -> Generator[DataReader, None, None]:  # noqa: F811
     db_cfg = SQLEngineConfig(
         drivername="postgresql+psycopg2",
         username="postgres",
         password="password",
         ip="localhost",
-        port=5433, # default is 5432, but we want to use different port for test db
+        port=5433,  # default is 5432, but we want to use different port for test db
     )
 
     db_name = "pytest"
@@ -30,11 +33,17 @@ def data_reader(timescale_docker) -> Generator[DataReader, None, None]:
 class TestDataReader:
     sensor_names: List[str] = ["sensor1", "sensor2", "sensor3"]
 
+    @pytest.fixture(autouse=True, scope="class")
+    def now(self):
+        """
+        scope="class" should mean this is only executed once and the timestamp is shared
+        """
+        return datetime.now(UTC)
+
     @pytest.fixture(autouse=False, scope="class")
-    def populate_db(self, data_writer: DataWriter):
+    def populate_db(self, data_writer: DataWriter, now):  # noqa: F811
         n_vals = 50
         names = TestDataReader.sensor_names
-        now = datetime.now(UTC)
         for name in names:
             write_n_random_vals(n=n_vals, name=name, data_writer=data_writer, end_time=now)
 
@@ -49,33 +58,54 @@ class TestDataReader:
             )
 
     @pytest.mark.skip(reason="github actions do not yet support docker")
-    def test_single_aggregated_read(self, data_reader: DataReader, populate_db):
-        end_time = datetime.now(UTC)
+    def test_single_avg_read(self, data_reader: DataReader, populate_db, now):
+        end_time = now
         start_time = end_time - timedelta(minutes=5)
         result_df = data_reader.single_aggregated_read(
-            names=TestDataReader.sensor_names, start_time=start_time, end_time=end_time
+            names=TestDataReader.sensor_names, start_time=start_time, end_time=end_time, aggregation="avg"
         )
         assert TestDataReader.sensor_names == result_df.columns.tolist()
 
     @pytest.mark.skip(reason="github actions do not yet support docker")
-    def test_batch_aggregated_read(self, data_reader: DataReader, populate_db):
-        end_time = datetime.now(UTC) + timedelta(minutes=1)
+    def test_single_last_read(self, data_reader: DataReader, populate_db, now):
+        end_time = now
+        start_time = end_time - timedelta(minutes=5)
+        result_df = data_reader.single_aggregated_read(
+            names=TestDataReader.sensor_names, start_time=start_time, end_time=end_time, aggregation="last"
+        )
+        assert TestDataReader.sensor_names == result_df.columns.tolist()
+
+    @pytest.mark.skip(reason="github actions do not yet support docker")
+    def test_batch_avg_read(self, data_reader: DataReader, populate_db, now):
+        end_time = now + timedelta(minutes=1)
         start_time = end_time - timedelta(minutes=30)
         result_df = data_reader.batch_aggregated_read(
             names=TestDataReader.sensor_names,
             start_time=start_time,
             end_time=end_time,
             bucket_width=timedelta(seconds=10),
+            aggregation="avg",
         )
 
-        print(end_time)
-        print(result_df)
-        print(len(result_df))
         self._ensure_names_included(result_df)
 
     @pytest.mark.skip(reason="github actions do not yet support docker")
-    def test_missing_col_batch_aggregated_read(self, data_reader: DataReader, populate_db):
-        end_time = datetime.now(UTC) + timedelta(minutes=1)
+    def test_batch_last_read(self, data_reader: DataReader, populate_db, now):
+        end_time = now + timedelta(minutes=1)
+        start_time = end_time - timedelta(minutes=30)
+        result_df = data_reader.batch_aggregated_read(
+            names=TestDataReader.sensor_names,
+            start_time=start_time,
+            end_time=end_time,
+            bucket_width=timedelta(seconds=10),
+            aggregation="last",
+        )
+
+        self._ensure_names_included(result_df)
+
+    @pytest.mark.skip(reason="github actions do not yet support docker")
+    def test_missing_col_batch_aggregated_read(self, data_reader: DataReader, populate_db, now):
+        end_time = now + timedelta(minutes=1)
         start_time = end_time - timedelta(minutes=30)
         missing_sensor_name = "sensor_x"
 
@@ -90,8 +120,8 @@ class TestDataReader:
         self._ensure_names_included(result_df)
 
     @pytest.mark.skip(reason="github actions do not yet support docker")
-    def test_missing_col_single_aggregated_read(self, data_reader: DataReader, populate_db):
-        end_time = datetime.now(UTC) + timedelta(minutes=1)
+    def test_missing_col_single_aggregated_read(self, data_reader: DataReader, populate_db, now):
+        end_time = now + timedelta(minutes=1)
         start_time = end_time - timedelta(minutes=30)
         missing_sensor_name = "sensor_x"
 
