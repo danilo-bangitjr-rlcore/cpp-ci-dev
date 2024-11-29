fc36afb3be2c8cb210dfbaa80b731177d70cd0b0
Andy Patterson <andy@rlcore.ai>
2024-11-29 11:28:57 -0700

    fix: ensure sc components are registered to hydra group

--- corerl/data_pipeline/state_constructors/sc.py
+++ corerl/data_pipeline/state_constructors/sc.py
@@ -4,6 +4,10 @@ from corerl.data_pipeline.datatypes import PipelineFrame, StageCode
 from corerl.data_pipeline.state_constructors.interface import TransformCarry
 from corerl.data_pipeline.state_constructors.components.base import BaseTransformConfig, sc_group, StateTransform
 
+# ensure components are registered
+import corerl.data_pipeline.state_constructors.components.norm # noqa: F401
+import corerl.data_pipeline.state_constructors.components.trace # noqa: F401
+
 
 
 type SC_TS = dict[
