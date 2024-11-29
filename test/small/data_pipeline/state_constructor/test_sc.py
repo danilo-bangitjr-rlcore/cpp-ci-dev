6a25c5696b6d10074c68c0538fba38c4b1c5cd09
Andy Patterson <andy@rlcore.ai>
2024-11-29 14:52:32 -0700

    feat: add ability to concatenate raw observations as a sc step

--- test/small/data_pipeline/state_constructor/test_sc.py
+++ test/small/data_pipeline/state_constructor/test_sc.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from corerl.data_pipeline.datatypes import CallerCode, PipelineFrame
 from corerl.data_pipeline.state_constructors.components.norm import NormalizerConfig
+from corerl.data_pipeline.state_constructors.components.add_raw import AddRawConfig
 from corerl.data_pipeline.state_constructors.sc import StateConstructor
 from corerl.data_pipeline.state_constructors.components.trace import TraceConfig
 
@@ -56,5 +57,37 @@ def test_norm_sc():
     expected = pd.DataFrame({
         'tag-1_norm_trace-0.1':  [np.nan, 0, .225, .4725, .72225, .972225, np.nan, .25, .475],
         'tag-1_norm_trace-0.01': [np.nan, 0, .2475, .497475, .747475, .997475, np.nan, .25, .4975],
+    })
+    assert dfs_close(pf.data, expected)
+
+
+def test_sc_add_raw():
+    raw_obs = pd.DataFrame({
+        'tag_1': [np.nan, 1, 2, 3, np.nan, 1, 2],
+        'tag_2': [1, 2, 3, np.nan, 1, 2, np.nan],
+    })
+
+    pf = PipelineFrame(
+        data=raw_obs,
+        caller_code=CallerCode.REFRESH,
+    )
+
+    sc = StateConstructor(
+        cfgs=[
+            TraceConfig(trace_values=[0.1, 0.01]),
+            AddRawConfig(),
+        ]
+    )
+
+    for tag_name in ('tag_1', 'tag_2'):
+        pf = sc(pf, tag_name)
+
+    expected = pd.DataFrame({
+        'tag_1':            [np.nan, 1, 2, 3, np.nan, 1, 2],
+        'tag_2':            [1, 2, 3, np.nan, 1, 2, np.nan],
+        'tag_1_trace-0.1':  [np.nan, 1., 1.9, 2.89, np.nan, 1., 1.9],
+        'tag_1_trace-0.01': [np.nan, 1., 1.99, 2.9899, np.nan, 1., 1.99],
+        'tag_2_trace-0.1':  [1., 1.9, 2.89, np.nan, 1., 1.9, np.nan],
+        'tag_2_trace-0.01': [1., 1.99, 2.9899, np.nan, 1., 1.99, np.nan],
     })
     assert dfs_close(pf.data, expected)
