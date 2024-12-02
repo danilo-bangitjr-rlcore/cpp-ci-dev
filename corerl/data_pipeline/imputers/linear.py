8bd344f6343f6208f1cc566ff8467f4e70072663
Alex Trudeau <alexandre.trudeau@rlcore.ai>
2024-12-02 15:09:45 -0700

    Updated LinearImputer implementation to operate on numpy arrays rather than dataframes. Also now using numba

--- corerl/data_pipeline/imputers/linear.py
+++ corerl/data_pipeline/imputers/linear.py
@@ -1,170 +1,108 @@
 import numpy as np
-import pandas as pd
 
 from dataclasses import dataclass
-from typing import Any, cast
+from numba import njit
+from typing import Any
 from omegaconf import MISSING
 
-from corerl.data_pipeline.datatypes import PipelineFrame, MissingType
-from corerl.data_pipeline.imputers.base import BaseImputer, BaseImputerConfig, imputer_group, ImputerTemporalState
-from corerl.data_pipeline.tag_config import TagConfig
+from corerl.data_pipeline.datatypes import PipelineFrame, StageCode
+from corerl.data_pipeline.imputers.base import BaseImputer, BaseImputerConfig, imputer_group
 
 
 @dataclass
 class LinearImputerConfig(BaseImputerConfig):
     name: str = "linear"
-    imputation_horizon: int = MISSING
+    max_gap: int = MISSING # Maximum number of NaNs between the two values used in interpolation
+
+
+@dataclass
+class LinearImputerTemporalState:
+    prev_val: float | None = None # Last non-NaN value in the previous PipelineFrame, otherwise None
+    num_nans: int = 0 # Number of NaNs after 'prev_val' in the previous PipelineFrame
 
 
 class LinearImputer(BaseImputer):
     def __init__(self, cfg: LinearImputerConfig, tag_cfg: Any):
         super().__init__(cfg, tag_cfg)
-        self.imputation_horizon = cfg.imputation_horizon
-        assert isinstance(tag_cfg, TagConfig)
-        self.bounds = tag_cfg.bounds
+        self.max_gap = cfg.max_gap
 
-    def _get_ts_aware_history(self,
-                              data: pd.Series | pd.DataFrame,
-                              impute_ind_int: int,
-                              imputer_ts: ImputerTemporalState) -> pd.Series | pd.DataFrame:
-        """
-        If impute_ind_int < imputation_horizon, try constructing a backward looking history of size imputation_horizon
-        that comprises the indices before impute_ind in 'data' as well as the indices in the Temporal State that are
-        within the imputation horizon. If ImputerTemporalState.prev_pf_data is None, simply return the previous indices
-        in 'data'
-        """
-        if imputer_ts.prev_pf_data is None:
-            return data.iloc[:impute_ind_int]
+    def __call__(self, pf: PipelineFrame, tag: str):
+        ts = pf.temporal_state.get(StageCode.IMPUTER)
+        ts = ts or {}
+        assert isinstance(ts, dict)
 
-        ts_horizon = np.clip(self.imputation_horizon - impute_ind_int, 0, len(imputer_ts.prev_pf_data))
-        ts_data = imputer_ts.prev_pf_data.iloc[-ts_horizon:]
-        curr_pf_data = data.iloc[:impute_ind_int]
-        backtrack_vals = pd.concat([ts_data, curr_pf_data])
+        tag_ts = ts.get(tag, LinearImputerTemporalState())
+        ts[tag] = tag_ts
+        assert isinstance(tag_ts, LinearImputerTemporalState)
 
-        return backtrack_vals
+        tag_data = pf.data[tag].to_numpy()
 
-    def _get_backtrack_data(self,
-                            data: pd.Series | pd.DataFrame,
-                            impute_ind_int: int,
-                            imputer_ts: ImputerTemporalState) -> pd.Series | pd.DataFrame:
-        if impute_ind_int < self.imputation_horizon:
-            # Consider backtrack values in temporal state
-            backtrack_vals = self._get_ts_aware_history(data, impute_ind_int, imputer_ts)
-        else:
-            backtrack_vals = data.iloc[impute_ind_int - self.imputation_horizon : impute_ind_int]
-        assert isinstance(backtrack_vals, pd.Series | pd.DataFrame)
-        backtrack_vals = cast(pd.Series | pd.DataFrame, backtrack_vals)
+        prev = np.nan if tag_ts.prev_val is None else tag_ts.prev_val
+        new_tag_data, new_prev, new_num_nans = linear_interpolation(tag_data, prev, tag_ts.num_nans, self.max_gap)
 
-        return backtrack_vals
+        tag_ts.prev_val = new_prev
+        tag_ts.num_nans = new_num_nans
 
-    def _get_lookahead_data(self,
-                            data: pd.Series | pd.DataFrame,
-                            impute_ind_int: int) -> pd.Series | pd.DataFrame:
-        lookahead_horizon = np.clip(impute_ind_int + self.imputation_horizon + 1, 0, len(data))
-        lookahead_vals = data.iloc[impute_ind_int + 1 : lookahead_horizon]
+        pf.data[tag] = new_tag_data
+        pf.temporal_state[StageCode.IMPUTER] = ts
 
-        return lookahead_vals
-
-    def _get_slope(self,
-                   backtrack_val: float,
-                   lookahead_val: float,
-                   ind_diff: int) -> float:
-        return float(lookahead_val - backtrack_val) / float(ind_diff)
-
-    def _linear_interpolation(self,
-                              backtrack_vals: pd.Series | pd.DataFrame,
-                              backtrack_ind: pd.Timestamp,
-                              lookahead_vals: pd.Series | pd.DataFrame,
-                              lookahead_ind: pd.Timestamp) -> float:
-        backtrack_val = backtrack_vals.loc[backtrack_ind]
-        lookahead_val = lookahead_vals.loc[lookahead_ind]
-        backtrack_ind_int = backtrack_vals.index.get_loc(backtrack_ind)
-        lookahead_ind_int = lookahead_vals.index.get_loc(lookahead_ind)
-        assert isinstance(backtrack_ind_int, int)
-        assert isinstance(lookahead_ind_int, int)
-        ind_diff = (self.imputation_horizon - backtrack_ind_int) + (lookahead_ind_int + 1)
-        slope = self._get_slope(backtrack_val, lookahead_val, ind_diff)
-        imputed_val = slope * (self.imputation_horizon - backtrack_ind_int) + backtrack_val
-
-        return imputed_val
-
-    def _get_imputed_val(self,
-                         data: pd.Series | pd.DataFrame,
-                         impute_ind: pd.Timestamp,
-                         imputer_ts: ImputerTemporalState) -> float:
-        impute_ind_int = data.index.get_loc(impute_ind)
-        assert isinstance(impute_ind_int, int)
-
-        # Try finding non-NaN value before impute_ind within imputation horizon
-        backtrack_vals = self._get_backtrack_data(data, impute_ind_int, imputer_ts)
-        backtrack_ind = backtrack_vals.last_valid_index()
-        backtrack_ind = cast(pd.Timestamp | None, backtrack_ind)
-
-        # Try finding non-NaN value after impute_ind within imputation horizon
-        lookahead_vals = self._get_lookahead_data(data, impute_ind_int)
-        lookahead_ind = lookahead_vals.first_valid_index()
-        lookahead_ind = cast(pd.Timestamp | None, lookahead_ind)
-
-        if backtrack_ind is not None and lookahead_ind is not None:
-            # Found non-NaN values before and after impute_ind within imputation horizon. Can perform interpolation.
-            assert isinstance(backtrack_ind, pd.Timestamp)
-            assert isinstance(lookahead_ind, pd.Timestamp)
-            imputed_val = self._linear_interpolation(backtrack_vals, backtrack_ind, lookahead_vals, lookahead_ind)
-            return imputed_val
-        else:
-            # Can't find non-NaN values before and after impute_ind within imputation horizon
-            return np.nan
-
-    def _get_imputed_vals(self,
-                          data: pd.Series | pd.DataFrame,
-                          imputed_inds: pd.DatetimeIndex,
-                          imputer_ts: ImputerTemporalState) -> np.ndarray:
-        imputed_vals = []
-        for ind in imputed_inds:
-            imputed_val = self._get_imputed_val(data, ind, imputer_ts)
-            imputed_vals.append(imputed_val)
-
-        return np.array(imputed_vals)
-
-    def _get_new_ts_history(self,
-                            data: pd.Series | pd.DataFrame,
-                            imputer_ts: ImputerTemporalState) -> ImputerTemporalState:
-        """
-        Set the ImputerTemporalState to be the last self.imputation_horizon rows of the current PipelineFrame's
-        DataFrame so that the next PipelineFrame can potentially perform Copy Imputation if the PipelineFrame's
-        are temporally continuous
-        """
-        imputer_ts.prev_pf_data = data.iloc[-self.imputation_horizon:].copy()
-
-        return imputer_ts
-
-    def _inner_call(self,
-                    pf: PipelineFrame,
-                    tag: str,
-                    imputer_ts: ImputerTemporalState | None) -> tuple[PipelineFrame, ImputerTemporalState]:
-        # Make sure imputer_ts is an ImputerTemporalState
-        if imputer_ts is None:
-            imputer_ts = ImputerTemporalState()
-        assert isinstance(imputer_ts, ImputerTemporalState)
-
-        data = pf.data
-        missing_info = pf.missing_info
-        tag_data = data[tag]
-        tag_missing_info = missing_info[tag]
-
-        # Find indices with NaNs
-        missing_inds: Any = tag_missing_info.index[tag_missing_info > MissingType.NULL]
-        if len(missing_inds) == 0:
-            # No imputation necessary
-            imputer_ts = self._get_new_ts_history(tag_data, imputer_ts)
-            return pf, imputer_ts
-
-        assert isinstance(missing_inds, pd.DatetimeIndex)
-        imputed_vals = self._get_imputed_vals(tag_data, missing_inds, imputer_ts)
-        imputer_ts = self._get_new_ts_history(tag_data, imputer_ts)
-        data.loc[missing_inds, tag] = imputed_vals
-
-        return pf, imputer_ts
+        return pf
 
 
 imputer_group.dispatcher(LinearImputer)
+
+
+
+@njit
+def impute_gap(backtrack_val: float,
+               lookahead_val: float,
+               num_nans: int) -> np.ndarray:
+    """
+    Use linear interpolation to impute all of the values between 'backtrack_val
+    and 'lookahead_val'. Imputed values returned as a numpy array.
+    """
+    bias = np.ones(num_nans) * backtrack_val
+    slope = float(lookahead_val - backtrack_val) / float(num_nans + 1)
+    delta = np.arange(1, num_nans + 1) * slope
+    imputed_vals = bias + delta
+
+    return imputed_vals
+
+
+@njit
+def linear_interpolation(x: np.ndarray, prev: float, num_nans: int, max_gap: int) -> tuple[np.ndarray, float, int]:
+    """
+    Traverse 'x' and replace NaNs with values imputed using linear interpolation.
+    A given index's value can only be imputed if the first non-NaN value before and after
+    the index are separated by at most 'max_gap' NaNs
+    """
+    insert_ind = 0
+    for i in range(len(x)):
+        v = x[i]
+        assert isinstance(v, float)
+        reset_prev = False
+        if np.isnan(prev):
+            # Can't perform linear interpolation while 'prev' is NaN
+            reset_prev = True
+        elif np.isnan(v):
+            # If 'prev' isn't NaN, increment 'num_nans' when you encounter a NaN
+            num_nans += 1
+            if num_nans > max_gap:
+                # When 'num_nans' exceeds 'max_gap', can't perform interpolation with current 'prev'
+                reset_prev = True
+        elif num_nans > 0:
+            # 'v' and 'prev' are within 'max_gap' so perform interpolation
+            imputed_vals = impute_gap(prev, v, num_nans)
+            # impute_gap() can impute values for indices in the temporal state. Drop those imputed values
+            x[insert_ind: i] = imputed_vals[-(i - insert_ind):]
+            reset_prev = True
+        else:
+            # If 'v' and 'prev' are adjacent, don't bother interpolating
+            reset_prev = True
+
+        if reset_prev:
+            prev = v
+            insert_ind = i + 1
+            num_nans = 0
+
+    return x, prev, num_nans
