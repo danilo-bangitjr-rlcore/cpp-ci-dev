ff79b535ae983e7add22ec2b96ba34d3082d20e6
Alex Trudeau <alexandre.trudeau@rlcore.ai>
2024-12-03 12:36:43 -0700

    Fixed LinearImputer tests

--- corerl/data_pipeline/imputers/linear.py
+++ corerl/data_pipeline/imputers/linear.py
@@ -12,13 +12,13 @@ from corerl.data_pipeline.imputers.base import BaseImputer, BaseImputerConfig, i
 @dataclass
 class LinearImputerConfig(BaseImputerConfig):
     name: str = "linear"
-    max_gap: int = MISSING # Maximum number of NaNs between the two values used in interpolation
+    max_gap: int = MISSING # Maximum number of NaNs between the two values used in linear interpolation
 
 
 @dataclass
 class LinearImputerTemporalState:
-    prev_val: float | None = None # Last non-NaN value in the previous PipelineFrame, otherwise None
-    num_nans: int = 0 # Number of NaNs after 'prev_val' in the previous PipelineFrame
+    prev_val: float = np.nan # Last non-NaN value in the previous PipelineFrame, otherwise np.nan
+    num_nans: int = 0 # Number of NaNs after 'prev_val' in the previous PipelineFrame. Can't exceed 'max_gap'
 
 
 class LinearImputer(BaseImputer):
@@ -37,8 +37,10 @@ class LinearImputer(BaseImputer):
 
         tag_data = pf.data[tag].to_numpy()
 
-        prev = np.nan if tag_ts.prev_val is None else tag_ts.prev_val
-        new_tag_data, new_prev, new_num_nans = linear_interpolation(tag_data, prev, tag_ts.num_nans, self.max_gap)
+        new_tag_data, new_prev, new_num_nans = linear_interpolation(tag_data,
+                                                                    tag_ts.prev_val,
+                                                                    tag_ts.num_nans,
+                                                                    self.max_gap)
 
         tag_ts.prev_val = new_prev
         tag_ts.num_nans = new_num_nans
@@ -52,14 +54,10 @@ class LinearImputer(BaseImputer):
 imputer_group.dispatcher(LinearImputer)
 
 
-
 @njit
-def impute_gap(backtrack_val: float,
-               lookahead_val: float,
-               num_nans: int) -> np.ndarray:
+def impute_gap(backtrack_val: float, lookahead_val: float, num_nans: int) -> np.ndarray:
     """
-    Use linear interpolation to impute all of the values between 'backtrack_val
-    and 'lookahead_val'. Imputed values returned as a numpy array.
+    Use linear interpolation to impute all of the values between 'backtrack_val' and 'lookahead_val'
     """
     bias = np.ones(num_nans) * backtrack_val
     slope = float(lookahead_val - backtrack_val) / float(num_nans + 1)
@@ -76,7 +74,7 @@ def linear_interpolation(x: np.ndarray, prev: float, num_nans: int, max_gap: int
     A given index's value can only be imputed if the first non-NaN value before and after
     the index are separated by at most 'max_gap' NaNs
     """
-    insert_ind = 0
+    insert_ind = 0 # Where to insert the imputed list of vals in 'x'
     for i in range(len(x)):
         v = x[i]
         assert isinstance(v, float)
@@ -88,13 +86,14 @@ def linear_interpolation(x: np.ndarray, prev: float, num_nans: int, max_gap: int
             # If 'prev' isn't NaN, increment 'num_nans' when you encounter a NaN
             num_nans += 1
             if num_nans > max_gap:
-                # When 'num_nans' exceeds 'max_gap', can't perform interpolation with current 'prev'
+                # When 'num_nans' exceeds 'max_gap', can't perform linear interpolation with current 'prev'
                 reset_prev = True
         elif num_nans > 0:
-            # 'v' and 'prev' are within 'max_gap' so perform interpolation
+            # 'v' and 'prev' are within 'max_gap' so perform linear interpolation
             imputed_vals = impute_gap(prev, v, num_nans)
-            # impute_gap() can impute values for indices in the temporal state. Drop those imputed values
-            x[insert_ind: i] = imputed_vals[-(i - insert_ind):]
+            # impute_gap() can impute values for NaNs that were in the temporal state (i.e if the passed 'num_nans' > 0)
+            # Drop those imputed values
+            x[insert_ind : i] = imputed_vals[-(i - insert_ind):]
             reset_prev = True
         else:
             # If 'v' and 'prev' are adjacent, don't bother interpolating
