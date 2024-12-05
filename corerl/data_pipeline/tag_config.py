cf372a41de5a40d5f62b0b2423d6c8eb8621ceb4
Andy Patterson <andy@rlcore.ai>
2024-12-04 22:17:31 -0700

    feat: make normalization default for tag sc

--- corerl/data_pipeline/tag_config.py
+++ corerl/data_pipeline/tag_config.py
@@ -6,6 +6,7 @@ from corerl.data_pipeline.imputers.identity import IdentityImputerConfig
 from corerl.data_pipeline.oddity_filters.base import BaseOddityFilterConfig
 from corerl.data_pipeline.oddity_filters.ema_filter import EMAFilterConfig
 from corerl.data_pipeline.state_constructors.components.base import BaseTransformConfig
+from corerl.data_pipeline.state_constructors.components.norm import NormalizerConfig
 from corerl.utils.hydra import list_
 
 
@@ -16,5 +17,5 @@ class TagConfig:
     bounds: tuple[float | None, float | None] = (None, None)
     outlier: BaseOddityFilterConfig = field(default_factory=EMAFilterConfig)
     imputer: BaseImputerConfig = field(default_factory=IdentityImputerConfig)
-    state_constructor: list[BaseTransformConfig] = list_()
+    state_constructor: list[BaseTransformConfig] = list_([NormalizerConfig()])
     is_action: bool = False
