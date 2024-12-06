abbc93e31f8f0dad0769933adf26d146461f6ae7
Kerrick Johnstonbaugh <kerrick@rlcore.ai>
2024-12-06 11:00:36 -0700

    add tests for lessthan and greaterthan transforms in reward constructor

--- test/small/data_pipeline/reward/test_reward.py
+++ test/small/data_pipeline/reward/test_reward.py
@@ -3,11 +3,14 @@ import pandas as pd
 
 from corerl.data_pipeline.datatypes import CallerCode, PipelineFrame
 from corerl.data_pipeline.reward.rc import RewardComponentConstructor, RewardConstructor
+from corerl.data_pipeline.transforms.less_than import LessThanConfig
+from corerl.data_pipeline.transforms.greater_than import GreaterThanConfig
 from corerl.data_pipeline.transforms.norm import NormalizerConfig
 from corerl.data_pipeline.transforms.trace import TraceConfig
 from corerl.data_pipeline.transforms.null import NullConfig
 from test.infrastructure.utils.pandas import dfs_close
-
+from corerl.data_pipeline.tag_config import TagConfig
+import datetime
 
 def test_rc1():
     raw_obs = pd.DataFrame(
@@ -83,5 +86,107 @@ def test_null_xform():
     expected_reward_vals = expected_components.sum(axis=1, skipna=False)
     expected_reward_df = pd.DataFrame({"reward": expected_reward_vals})
     expected_df = pd.concat((raw_obs, expected_reward_df), axis=1, copy=True)
+
+    assert dfs_close(pf.data, expected_df)
+
+def test_lessthan_xform():
+    tag_cfgs=[
+        TagConfig(
+            name='tag-1',
+            # default null reward config
+        ),
+        TagConfig(
+            name='tag-2',
+            reward_constructor=[
+                LessThanConfig(threshold=5),
+            ],
+        ),
+    ]
+    start = datetime.datetime.now(datetime.UTC)
+    Δ = datetime.timedelta(minutes=5)
+
+    dates = [start + i * Δ for i in range(7)]
+    idx = pd.DatetimeIndex(dates)
+
+    cols = pd.Index(['tag-1', 'tag-2'])
+    df = pd.DataFrame(
+        data=[
+            [np.nan, 0],
+            [0,      2],
+            [1,      4],
+            [2,      6],
+            [np.nan, np.nan],
+            [4,      10],
+            [5,      12],
+        ],
+        columns=cols,
+        index=idx,
+    )
+
+    reward_components = {cfg.name: RewardComponentConstructor(cfg.reward_constructor) for cfg in tag_cfgs}
+    reward_constructor = RewardConstructor(reward_components)
+    pf = PipelineFrame(
+        data=df,
+        caller_code=CallerCode.ONLINE,
+    )
+
+
+    # call reward constructor
+    pf = reward_constructor(pf)
+
+    expected_reward_vals = np.array([True, True, True, False, np.nan, False, False])
+    expected_reward_df = pd.DataFrame(data=expected_reward_vals, columns=pd.Index(["reward"]), index=idx)
+    expected_df = pd.concat((df, expected_reward_df), axis=1, copy=True)
+
+    assert dfs_close(pf.data, expected_df)
+
+def test_greaterthan_xform():
+    tag_cfgs=[
+        TagConfig(
+            name='tag-1',
+            # default null reward config
+        ),
+        TagConfig(
+            name='tag-2',
+            reward_constructor=[
+                GreaterThanConfig(threshold=5),
+            ],
+        ),
+    ]
+    start = datetime.datetime.now(datetime.UTC)
+    Δ = datetime.timedelta(minutes=5)
+
+    dates = [start + i * Δ for i in range(7)]
+    idx = pd.DatetimeIndex(dates)
+
+    cols = pd.Index(['tag-1', 'tag-2'])
+    df = pd.DataFrame(
+        data=[
+            [np.nan, 0],
+            [0,      2],
+            [1,      4],
+            [2,      6],
+            [np.nan, np.nan],
+            [4,      10],
+            [5,      12],
+        ],
+        columns=cols,
+        index=idx,
+    )
+
+    reward_components = {cfg.name: RewardComponentConstructor(cfg.reward_constructor) for cfg in tag_cfgs}
+    reward_constructor = RewardConstructor(reward_components)
+    pf = PipelineFrame(
+        data=df,
+        caller_code=CallerCode.ONLINE,
+    )
+
+
+    # call reward constructor
+    pf = reward_constructor(pf)
+
+    expected_reward_vals = np.array([False, False, False, True, np.nan, True, True])
+    expected_reward_df = pd.DataFrame(data=expected_reward_vals, columns=pd.Index(["reward"]), index=idx)
+    expected_df = pd.concat((df, expected_reward_df), axis=1, copy=True)
 
     assert dfs_close(pf.data, expected_df)
