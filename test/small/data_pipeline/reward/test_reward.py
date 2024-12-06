010d686a524c52a25d707b8e9a067e9a8a9b16f3
Kerrick Johnstonbaugh <kerrick@rlcore.ai>
2024-12-06 11:08:37 -0700

    add test for reward that penalizes when a tag value is greater than some threshold

--- test/small/data_pipeline/reward/test_reward.py
+++ test/small/data_pipeline/reward/test_reward.py
@@ -1,22 +1,25 @@
+import datetime
+
 import numpy as np
 import pandas as pd
 
 from corerl.data_pipeline.datatypes import CallerCode, PipelineFrame
 from corerl.data_pipeline.reward.rc import RewardComponentConstructor, RewardConstructor
-from corerl.data_pipeline.transforms.less_than import LessThanConfig
-from corerl.data_pipeline.transforms.greater_than import GreaterThanConfig
-from corerl.data_pipeline.transforms.norm import NormalizerConfig
-from corerl.data_pipeline.transforms.trace import TraceConfig
-from corerl.data_pipeline.transforms.null import NullConfig
-from test.infrastructure.utils.pandas import dfs_close
 from corerl.data_pipeline.tag_config import TagConfig
-import datetime
+from corerl.data_pipeline.transforms.greater_than import GreaterThanConfig
+from corerl.data_pipeline.transforms.less_than import LessThanConfig
+from corerl.data_pipeline.transforms.norm import NormalizerConfig
+from corerl.data_pipeline.transforms.null import NullConfig
+from corerl.data_pipeline.transforms.scale import ScaleConfig
+from corerl.data_pipeline.transforms.trace import TraceConfig
+from test.infrastructure.utils.pandas import dfs_close
+
 
 def test_rc1():
     raw_obs = pd.DataFrame(
         {
-            "obs_1": [np.nan, 1, 2, 3,      np.nan, 1, 2],
-            "obs_2": [1,      2, 3, np.nan, 1,      2, np.nan],
+            "obs_1": [np.nan, 1, 2, 3, np.nan, 1, 2],
+            "obs_2": [1, 2, 3, np.nan, 1, 2, np.nan],
         }
     )
 
@@ -39,8 +42,8 @@ def test_rc1():
 
     expected_components = pd.DataFrame(
         {
-            "obs_1_trace-0.1": [np.nan, 1.0, 1.9,  2.89,   np.nan, 1.0, 1.9],
-            "obs_2_trace-0.1": [1.0,    1.9, 2.89, np.nan, 1.0,    1.9, np.nan],
+            "obs_1_trace-0.1": [np.nan, 1.0, 1.9, 2.89, np.nan, 1.0, 1.9],
+            "obs_2_trace-0.1": [1.0, 1.9, 2.89, np.nan, 1.0, 1.9, np.nan],
         }
     )
     expected_reward_vals = expected_components.sum(axis=1, skipna=False)
@@ -48,6 +51,7 @@ def test_rc1():
     expected_df = pd.concat((raw_obs, expected_reward_df), axis=1, copy=True)
 
     assert dfs_close(pf.data, expected_df)
+
 
 def test_null_xform():
     raw_obs = pd.DataFrame(
@@ -89,14 +93,15 @@ def test_null_xform():
 
     assert dfs_close(pf.data, expected_df)
 
+
 def test_lessthan_xform():
-    tag_cfgs=[
+    tag_cfgs = [
         TagConfig(
-            name='tag-1',
+            name="tag-1",
             # default null reward config
         ),
         TagConfig(
-            name='tag-2',
+            name="tag-2",
             reward_constructor=[
                 LessThanConfig(threshold=5),
             ],
@@ -108,16 +113,16 @@ def test_lessthan_xform():
     dates = [start + i * Δ for i in range(7)]
     idx = pd.DatetimeIndex(dates)
 
-    cols = pd.Index(['tag-1', 'tag-2'])
+    cols = pd.Index(["tag-1", "tag-2"])
     df = pd.DataFrame(
         data=[
             [np.nan, 0],
-            [0,      2],
-            [1,      4],
-            [2,      6],
+            [0, 2],
+            [1, 4],
+            [2, 6],
             [np.nan, np.nan],
-            [4,      10],
-            [5,      12],
+            [4, 10],
+            [5, 12],
         ],
         columns=cols,
         index=idx,
@@ -130,7 +135,6 @@ def test_lessthan_xform():
         caller_code=CallerCode.ONLINE,
     )
 
-
     # call reward constructor
     pf = reward_constructor(pf)
 
@@ -140,14 +144,15 @@ def test_lessthan_xform():
 
     assert dfs_close(pf.data, expected_df)
 
+
 def test_greaterthan_xform():
-    tag_cfgs=[
+    tag_cfgs = [
         TagConfig(
-            name='tag-1',
+            name="tag-1",
             # default null reward config
         ),
         TagConfig(
-            name='tag-2',
+            name="tag-2",
             reward_constructor=[
                 GreaterThanConfig(threshold=5),
             ],
@@ -159,16 +164,16 @@ def test_greaterthan_xform():
     dates = [start + i * Δ for i in range(7)]
     idx = pd.DatetimeIndex(dates)
 
-    cols = pd.Index(['tag-1', 'tag-2'])
+    cols = pd.Index(["tag-1", "tag-2"])
     df = pd.DataFrame(
         data=[
             [np.nan, 0],
-            [0,      2],
-            [1,      4],
-            [2,      6],
+            [0, 2],
+            [1, 4],
+            [2, 6],
             [np.nan, np.nan],
-            [4,      10],
-            [5,      12],
+            [4, 10],
+            [5, 12],
         ],
         columns=cols,
         index=idx,
@@ -181,11 +186,69 @@ def test_greaterthan_xform():
         caller_code=CallerCode.ONLINE,
     )
 
-
     # call reward constructor
     pf = reward_constructor(pf)
 
     expected_reward_vals = np.array([False, False, False, True, np.nan, True, True])
+    expected_reward_df = pd.DataFrame(data=expected_reward_vals, columns=pd.Index(["reward"]), index=idx)
+    expected_df = pd.concat((df, expected_reward_df), axis=1, copy=True)
+
+    assert dfs_close(pf.data, expected_df)
+
+
+def test_greaterthan_penalty_reward():
+    """
+    This tests the following reward:
+        if tag_2 > 5:
+            r = -10
+        else:
+            r = 0
+    """
+    tag_cfgs = [
+        TagConfig(
+            name="tag-1",
+            # default null reward config
+        ),
+        TagConfig(
+            name="tag-2",
+            reward_constructor=[
+                GreaterThanConfig(threshold=5),
+                ScaleConfig(factor=-10) # penalty
+            ],
+        ),
+    ]
+    start = datetime.datetime.now(datetime.UTC)
+    Δ = datetime.timedelta(minutes=5)
+
+    dates = [start + i * Δ for i in range(7)]
+    idx = pd.DatetimeIndex(dates)
+
+    cols = pd.Index(["tag-1", "tag-2"])
+    df = pd.DataFrame(
+        data=[
+            [np.nan, 0],
+            [0, 2],
+            [1, 4],
+            [2, 6],
+            [np.nan, np.nan],
+            [4, 10],
+            [5, 12],
+        ],
+        columns=cols,
+        index=idx,
+    )
+
+    reward_components = {cfg.name: RewardComponentConstructor(cfg.reward_constructor) for cfg in tag_cfgs}
+    reward_constructor = RewardConstructor(reward_components)
+    pf = PipelineFrame(
+        data=df,
+        caller_code=CallerCode.ONLINE,
+    )
+
+    # call reward constructor
+    pf = reward_constructor(pf)
+
+    expected_reward_vals = np.array([0.0, 0.0, 0.0, -10.0, np.nan, -10.0, -10.0])
     expected_reward_df = pd.DataFrame(data=expected_reward_vals, columns=pd.Index(["reward"]), index=idx)
     expected_df = pd.concat((df, expected_reward_df), axis=1, copy=True)
 
