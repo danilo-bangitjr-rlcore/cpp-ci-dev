e5f029636c6b86d6f15e0be51540f9e6524b1079
Revan MacQueen <revan.macqueen@rlcore.ai>
2024-12-11 13:19:06 -0700

    Added: initial draft of all the time with tests

---
+++ corerl/data_pipeline/transition_creators/all_the_time.py
@@ -0,0 +1,196 @@
+import warnings
+import torch
+import pandas as pd
+import math
+
+from collections import deque
+from dataclasses import dataclass
+
+from corerl.data_pipeline.tag_config import TagConfig
+from corerl.component.network.utils import tensor
+from corerl.data_pipeline.datatypes import PipelineFrame, Step, NewTransition
+from corerl.utils.hydra import interpolate
+from corerl.data_pipeline.transition_creators.base import (
+    BaseTransitionCreator,
+    BaseTransitionCreatorConfig,
+    transition_creator_group,
+    TransitionCreatorTemporalState,
+)
+
+
+@dataclass
+class AllTheTimeTCConfig(BaseTransitionCreatorConfig):
+    name: str = "all-the-time"
+    steps_per_decision: int = interpolate('${interaction.steps_per_decision}')
+    gamma: float = interpolate('${experiment.gamma}')
+    min_n_step: int = 1
+    max_n_step: int = interpolate('${interaction.steps_per_decision}')
+
+    # FILTERS
+    only_dp_transitions: bool = False  # whether we only want to return transitions between decision points
+
+
+class NStepInfo:
+    def __init__(self, n: int):
+        self.step_q = deque(maxlen=n + 1)
+        self.n_step_reward: int | None = None
+        self.n_step_gamma: int | None = None
+
+
+@dataclass
+class AllTheTimeTS(TransitionCreatorTemporalState):
+    step_info: dict[int, NStepInfo]
+
+
+def has_nan(obj):
+    for attr_name, value in vars(obj).items():
+        if isinstance(value, torch.Tensor):
+            if torch.isnan(value).any():
+                return True
+        elif isinstance(value, float) and math.isnan(value):
+            return True
+    return False
+
+
+def get_tags(df: pd.DataFrame, tags: list[str] | str) -> torch.Tensor:
+    return tensor(df[tags].to_numpy())
+
+
+def get_n_step_reward(step_q):
+    last_step = step_q[-1]
+    n_step_reward = last_step.reward
+    n_step_gamma = last_step.gamma
+
+    for step_backwards in range(len(step_q) - 2, 0, -1):
+        step = step_q[step_backwards]
+        n_step_reward = step.reward + step.gamma * n_step_reward
+        n_step_gamma *= step.gamma
+
+    return n_step_reward, n_step_gamma
+
+
+def update_n_step_reward_gamma(n_step_reward, n_step_gamma, step_q):
+    assert (n_step_reward is None) == (n_step_gamma is None)
+
+    if n_step_gamma is None:
+        n_step_reward, n_step_gamma = get_n_step_reward(step_q)
+
+    else:  # update n_step_reward online with a recurrence relation
+        assert n_step_reward is not None
+        discount = n_step_gamma / step_q[-1].gamma
+        n_step_gamma = discount * step_q[0].gamma
+        n_step_reward = (n_step_reward - step_q[-1].reward) / step_q[-1].gamma
+        n_step_reward = n_step_reward + discount * step_q[0].reward
+
+    return n_step_reward, n_step_gamma
+
+
+def make_transition(
+        step_q: deque[Step],
+        n_step_reward: float | None = None,
+        n_step_gamma: float | None = None) -> tuple[NewTransition, float, float]:
+    n_step_reward, n_step_gamma = update_n_step_reward_gamma(n_step_reward, n_step_gamma, step_q)
+
+    pre_step = step_q[0]
+    post_step = Step(
+        reward=n_step_reward,
+        action=step_q[-1].action,
+        gamma=n_step_gamma,
+        state=step_q[-1].state,
+        dp=False,  #TODO: set this
+    )
+
+    transition = NewTransition(
+        pre_step,
+        post_step,
+        n_steps=len(step_q) - 1
+    )
+
+    return transition, n_step_reward, n_step_gamma
+
+
+class AllTheTimeTC(BaseTransitionCreator):
+    def __init__(
+            self,
+            cfg: AllTheTimeTCConfig,
+            tag_configs: list[TagConfig],
+    ):
+        super().__init__(cfg, tag_configs)
+
+        self.steps_per_decision = cfg.steps_per_decision
+        self.gamma = cfg.gamma
+        self.min_n_step = cfg.min_n_step
+        self.max_n_step = cfg.max_n_step
+        self.step_info = None
+        self.init_step_info()
+
+        self.only_dp_transitions = cfg.only_dp_transitions
+
+    def init_step_info(self):
+        self.step_info: dict[int, NStepInfo] = {
+            n: NStepInfo(n) for n in range(self.min_n_step, self.max_n_step + 1)
+        }
+
+    def _inner_call(self,
+                    pf: PipelineFrame,
+                    tc_ts: TransitionCreatorTemporalState | None) \
+            -> tuple[list[NewTransition], TransitionCreatorTemporalState | None]:
+
+        assert isinstance(tc_ts, AllTheTimeTS | None)
+
+        transitions = []
+
+        if pf.data.empty:  # pretend like nothing happened but raise a warning...
+            warnings.warn("Empty dataframe passed to transition creator", stacklevel=2)
+            return transitions, tc_ts
+
+        df = pf.data
+        actions = get_tags(df, self.action_tags)
+        state_tags = sorted(
+            set(df.columns) - set(self.action_tags + ['reward'])
+        )
+        states = get_tags(df, state_tags)
+        rewards = df['reward'].to_numpy()
+        assert len(actions) == len(states) and len(states) == len(rewards)
+
+        for i in range(len(actions)):
+            step = Step(
+                reward=rewards[i],
+                action=actions[i],
+                gamma=self.gamma,
+                state=states[i],
+                dp=False,
+            )
+
+            if has_nan(step):
+                self.init_step_info()  # nuke the step info
+            else:
+                transitions += self._update(step)
+
+        if tc_ts is None:
+            tc_ts = AllTheTimeTS(self.step_info)
+        else:
+            tc_ts.step_info = self.step_info
+
+        return transitions, tc_ts
+
+    def _update(self, step: Step) -> list[NewTransition]:
+        new_transitions = []
+        for n in range(self.min_n_step, self.max_n_step + 1):
+            n_step_info = self.step_info[n]
+            step_q = n_step_info.step_q
+            step_q.append(step)
+
+            is_full = len(step_q) == n + 1
+            if is_full:
+                n_step_reward = n_step_info.n_step_reward
+                n_step_gamma = n_step_info.n_step_gamma
+                new_transition, n_step_reward, n_step_gamma = make_transition(step_q, n_step_reward, n_step_gamma)
+                new_transitions.append(new_transition)
+                n_step_info.n_step_reward = n_step_reward
+                n_step_info.n_step_gamma = n_step_gamma
+
+        return new_transitions
+
+
+transition_creator_group.dispatcher(AllTheTimeTC)
---
+++ test/small/data_pipeline/transition_creator/test_all_the_time.py
@@ -0,0 +1,296 @@
+import numpy as np
+import pandas as pd
+import datetime
+from collections import deque
+from math import comb
+
+from corerl.utils.torch import tensor_allclose
+from torch import Tensor
+
+from corerl.data_pipeline.tag_config import TagConfig
+from corerl.data_pipeline.datatypes import PipelineFrame, CallerCode, NewTransition, Step, StageCode, TemporalState
+from corerl.data_pipeline.transition_creators.all_the_time import (
+    get_n_step_reward,
+    update_n_step_reward_gamma,
+    make_transition,
+    AllTheTimeTCConfig,
+    AllTheTimeTC,
+    AllTheTimeTS
+
+)
+
+
+def get_test_prior_step(state: Tensor, dp=False) -> Step:
+    return Step(
+        state=state,
+        gamma=0,
+        action=Tensor([0.]),
+        reward=0,
+        dp=dp,
+    )
+
+
+def transitions_equal_test(t0: NewTransition, t1: NewTransition):
+    return (
+            tensor_allclose(t0.prior.state, t1.prior.state)
+            and t0.prior.dp == t1.prior.dp
+            and t0.post == t1.post
+            and t0.n_steps == t1.n_steps
+    )
+
+
+def test_get_n_step_reward_1():
+    q = deque(maxlen=3)
+
+    step_0 = Step(
+        state=Tensor([0.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.9,
+        dp=True
+    )
+
+    step_1 = Step(
+        state=Tensor([1.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.9,
+        dp=True
+    )
+
+    step_2 = Step(
+        state=Tensor([2.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.9,
+        dp=True
+    )
+
+    q.appendleft(step_0)
+    q.appendleft(step_1)
+    q.appendleft(step_2)
+
+    n_step_reward, n_step_gamma = get_n_step_reward(q)
+
+    assert n_step_reward == 1.9
+    assert n_step_gamma == 0.81
+
+
+def test_update_n_step_reward_gamma_1():
+    q = deque(maxlen=3)
+
+    step_0 = Step(
+        state=Tensor([0.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.9,
+        dp=True
+    )
+
+    step_1 = Step(
+        state=Tensor([1.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.9,
+        dp=True
+    )
+
+    step_2 = Step(
+        state=Tensor([2.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.9,
+        dp=True
+    )
+
+    q.appendleft(step_0)
+    q.appendleft(step_1)
+    q.appendleft(step_2)
+
+    n_step_reward, n_step_gamma = None, None
+
+    n_step_reward, n_step_gamma = update_n_step_reward_gamma(n_step_reward, n_step_gamma, q)
+    assert n_step_reward == 1.9
+    assert n_step_gamma == 0.81
+
+    step_3 = Step(
+        state=Tensor([3.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.9,
+        dp=True
+    )
+
+    q.appendleft(step_3)
+    n_step_reward, n_step_gamma = update_n_step_reward_gamma(n_step_reward, n_step_gamma, q)
+    assert n_step_reward == 1.9
+    assert n_step_gamma == 0.81
+
+    step_4 = Step(
+        state=Tensor([4.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.9,
+        dp=True
+    )
+
+    q.appendleft(step_4)
+    n_step_reward, n_step_gamma = update_n_step_reward_gamma(n_step_reward, n_step_gamma, q)
+    assert n_step_reward == 1.9
+    assert n_step_gamma == 0.81
+
+
+def test_update_n_step_reward_gamma_2():
+    q = deque(maxlen=3)
+
+    step_0 = Step(
+        state=Tensor([0.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.9,
+        dp=True
+    )
+
+    step_1 = Step(
+        state=Tensor([1.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.9,
+        dp=True
+    )
+
+    step_2 = Step(
+        state=Tensor([2.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.9,
+        dp=True
+    )
+
+    q.appendleft(step_0)
+    q.appendleft(step_1)
+    q.appendleft(step_2)
+
+    n_step_reward, n_step_gamma = None, None
+
+    n_step_reward, n_step_gamma = update_n_step_reward_gamma(n_step_reward, n_step_gamma, q)
+    assert n_step_reward == 1.9
+    assert n_step_gamma == 0.81
+
+    step_3 = Step(
+        state=Tensor([3.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.5,
+        dp=True
+    )
+
+    q.appendleft(step_3)
+    n_step_reward, n_step_gamma = update_n_step_reward_gamma(n_step_reward, n_step_gamma, q)
+    assert n_step_reward == 1.9
+    assert n_step_gamma == 0.45
+
+    step_4 = Step(
+        state=Tensor([4.]),
+        action=Tensor([0.]),
+        reward=1.,
+        gamma=0.5,
+        dp=True
+    )
+
+    q.appendleft(step_4)
+    n_step_reward, n_step_gamma = update_n_step_reward_gamma(n_step_reward, n_step_gamma, q)
+    assert n_step_reward == 1.5
+    assert n_step_gamma == 0.25
+
+
+def test_all_the_time_1():
+    state_col = np.arange(3)
+    cols = {"state": state_col, "action": [0, 0, 0], "reward": [1, 1, 1]}
+    dates = [
+        datetime.datetime(2024, 1, 1, 1, i) for i in range(3)
+    ]
+    datetime_index = pd.DatetimeIndex(dates)
+    df = pd.DataFrame(cols, index=datetime_index)
+    pf = PipelineFrame(
+        df,
+        caller_code=CallerCode.OFFLINE,
+    )
+    tags = [
+        TagConfig(
+            name='state',
+        ),
+        TagConfig(
+            name='action',
+            is_action=True,
+        ),
+        TagConfig(
+            name='reward',
+        )
+    ]
+
+    cfg = AllTheTimeTCConfig(
+        steps_per_decision=2,
+        gamma=0.9,
+        max_n_step=2,
+    )
+
+    tc = AllTheTimeTC(cfg, tags)
+    pf = tc(pf)
+    transitions = pf.transitions
+    assert isinstance(transitions, list)
+    assert len(transitions) == comb(3, 2)
+
+    t_0 = transitions[0]
+    expected_0 = NewTransition(
+        prior=get_test_prior_step(
+            Tensor([0.,]),
+            dp=False,
+        ),
+        post=Step(
+            state=Tensor([1.]),
+            action=Tensor([0.]),
+            reward=1.0,
+            gamma=0.9,
+            dp=False
+        ),
+        n_steps=1
+    )
+    assert transitions_equal_test(t_0, expected_0)
+
+    t_1 = transitions[1]
+    expected_1 = NewTransition(
+        prior=get_test_prior_step(
+            Tensor([1.,]),
+            dp=False,
+        ),
+        post=Step(
+            state=Tensor([2.]),
+            action=Tensor([0.]),
+            reward=1.0,
+            gamma=0.9,
+            dp=False
+        ),
+        n_steps=1
+    )
+    assert transitions_equal_test(t_1, expected_1)
+
+    t_2 = transitions[2]
+    expected_2 = NewTransition(
+        prior=get_test_prior_step(
+            Tensor([0.,]),
+            dp=False,
+        ),
+        post=Step(
+            state=Tensor([2.]),
+            action=Tensor([0.]),
+            reward=1.9,
+            gamma=0.81,
+            dp=False
+        ),
+        n_steps=2
+    )
+    assert transitions_equal_test(t_2, expected_2)
+
+
+
