f72ed5fe3be260bf2e3c856d92b91fc2418f9501
Andy Patterson <andy@rlcore.ai>
2024-12-16 15:50:17 -0700

    fix: ensure config is importable with no modifications

--- corerl/config.py
+++ corerl/config.py
@@ -2,7 +2,8 @@ from dataclasses import field
 from typing import Any
 
 from corerl.agent import AgentConfig
-from corerl.configs.config import config
+from corerl.agent.random import RandomAgentConfig
+from corerl.configs.config import MISSING, config
 from corerl.environment.async_env.sim_async_env import SimAsyncEnvConfig
 from corerl.experiment.config import ExperimentConfig
 from corerl.data_pipeline.pipeline import PipelineConfig
@@ -10,12 +11,12 @@ from corerl.data_pipeline.pipeline import PipelineConfig
 
 @config()
 class MainConfig:
-    action_period: int
-    obs_period: int
-    agent: AgentConfig
-    alerts: Any
-    env: SimAsyncEnvConfig
-
+    action_period: int = MISSING
+    obs_period: int = MISSING
+    alerts: Any = None
     use_alerts: bool = False
+    env: SimAsyncEnvConfig = field(default_factory=SimAsyncEnvConfig)
+
+    agent: AgentConfig = field(default_factory=GreedyACConfig)
     experiment: ExperimentConfig = field(default_factory=ExperimentConfig)
     pipeline: PipelineConfig = field(default_factory=PipelineConfig)
