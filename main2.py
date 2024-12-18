fa2c7023f9b0b29edb733a620d44d451f46032ab
Alexander Wong <alex@rlcore.ai>
2024-12-18 15:32:47 -0700

    chore: working pipeline call with scrubber reward and EPCOR data

--- main2.py
+++ main2.py
@@ -6,6 +6,8 @@ EPCOR Scrubber data directly from TSDB.
 import shelve
 from datetime import UTC, datetime, timedelta
 
+from omegaconf import DictConfig
+
 from corerl.data_pipeline.db.data_reader import DataReader, TagDBConfig
 from corerl.data_pipeline.tag_config import TagConfig
 from corerl.data_pipeline.pipeline import Pipeline, PipelineConfig
@@ -13,7 +15,8 @@ from corerl.data_pipeline.datatypes import StageCode
 from corerl.data_pipeline.transition_creators.anytime import AnytimeTransitionCreatorConfig
 from corerl.data_pipeline.state_constructors.sc import SCConfig
 from corerl.data_pipeline.state_constructors.countdown import CountdownConfig
-
+from corerl.environment.reward.scrubber import ScrubberReward
+from corerl.data_pipeline.oddity_filters.identity import IdentityFilterConfig
 
 raw_names = """
 AI0879A
@@ -69,12 +72,6 @@ def main():
 
     data_reader = DataReader(db_cfg=db_cfg)
     names = [n.strip() for n in raw_names.splitlines() if n.strip()]
-    
-    # this appears necessary for AnytimeTransitionCreator
-    # https://github.com/rlcoretech/core-rl/blob/97d7661edc48e42c81786ce8782db5a6ebfeec60/corerl/data_pipeline/transition_creators/anytime.py#L103
-    # todo? how to specify a column as the reward column?
-    # what is the reward within the EPCOR Scrubber4 environment?
-    names.append("reward")
 
     with shelve.open('raw_tag_config') as rtc:
         tag_configs = rtc['tag_configs'] if 'tag_configs' in rtc else []
@@ -82,17 +79,19 @@ def main():
         if len(tag_configs) == 0:
             for name in names:
                 tag_stats = data_reader.get_tag_stats(name)
-                print(name)
-                print(tag_stats)
+                # print(name)
+                # print(tag_stats)
 
                 tag_configs.append(TagConfig(
                     name=name,
                     bounds=(tag_stats.min, tag_stats.max),
-                    is_action= name.endswith("_SP")
+                    outlier=IdentityFilterConfig(
+                        name="identity",
+                    ),
+                    is_action= name.endswith("_SP"),
                 ))
                 rtc['tag_configs'] = tag_configs
 
-    # print(tag_configs)
 
     end_date = datetime(2024, 12, 15, tzinfo=UTC)
     start_date = datetime(2024, 12, 14, tzinfo=UTC)
@@ -104,6 +103,20 @@ def main():
         end_date,
         bucket_width=timedelta(hours=1),
     )
+
+    # this is needed to set transitions, if reward is NaN transitions do not get emitted.
+    reward_cfg = DictConfig({
+        'reward_tags': None,
+        'steps_per_interaction': 1,
+        'only_dp_transitions': False,
+        'type': 'mean_efficiency_cost',
+    })
+    reward = ScrubberReward(reward_cfg)
+
+    # appears to only require
+    # AI0879C, AIC3731_OUT, AIC3730_OUT
+    # https://github.com/rlcoretech/core-rl/blob/85c3ee1e3f9ef35fd5522fb87faf7e45a0a6aa68/corerl/environment/reward/scrubber.py#L62
+    res['reward'] = reward(None, df_rows=res)
 
     pipeline_config = PipelineConfig(
         tags=tag_configs,
@@ -130,15 +143,19 @@ def main():
         res,
         stages=(
             StageCode.BOUNDS,
-            # todo: when this is uncommented and passed into stages, many `NaN` values are introduced
-            # StageCode.ODDITY,
+            StageCode.ODDITY,
             StageCode.IMPUTER,
             StageCode.RC,
             StageCode.SC, # appears to reorder the dataframe, putting actions first?
             StageCode.TC
         )
     )
-    print(resp)
-
+    print(
+        resp.df,
+        len(resp.transitions) if resp.transitions else None
+    )
+    print(pipeline.get_state_action_dims())
+    print(res.shape)
+    print(len(names))
 if __name__ == "__main__":
     main()
