d80e265650d9e4cce6bbc4a268edd66d322f7bc0
Andy Patterson <andy@rlcore.ai>
2024-12-18 12:00:16 -0700

    refactor: remove unused boilerplate code from imputers

--- corerl/data_pipeline/imputers/identity.py
+++ corerl/data_pipeline/imputers/identity.py
@@ -1,10 +1,10 @@
 from typing import Literal
-from pydantic.dataclasses import dataclass as config
+from corerl.configs.config import config
 from corerl.data_pipeline.datatypes import PipelineFrame
-from corerl.data_pipeline.imputers.base import BaseImputer, BaseImputerConfig, imputer_group, ImputerTemporalState
+from corerl.data_pipeline.imputers.base import BaseImputer, BaseImputerConfig, imputer_group
 
 
-@config(config={'extra': 'forbid'})
+@config()
 class IdentityImputerConfig(BaseImputerConfig):
     name: Literal['identity'] = "identity"
 
@@ -13,14 +13,9 @@ class IdentityImputer(BaseImputer):
     def __init__(self, cfg: IdentityImputerConfig):
         super().__init__(cfg)
 
-    def _inner_call(self, pf: PipelineFrame, tag: str, imputer_ts: ImputerTemporalState | None) \
-            -> tuple[PipelineFrame, ImputerTemporalState]:
-        # Make sure imputer_ts is an ImputerTemporalState
-        if imputer_ts is None:
-            imputer_ts = ImputerTemporalState()
-        assert isinstance(imputer_ts, ImputerTemporalState)
 
-        return pf, imputer_ts
+    def __call__(self, pf: PipelineFrame, tag: str) -> PipelineFrame:
+        return pf
 
 
 imputer_group.dispatcher(IdentityImputer)
