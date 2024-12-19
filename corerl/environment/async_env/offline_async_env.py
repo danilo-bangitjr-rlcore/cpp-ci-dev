885980ef8a4ed25c20f78615d8fca282d7f09a58
Javier <javier@rlcore.ai>
2024-12-19 16:46:12 -0700

    feat: epcor tsdb scrubber is working with main

--- corerl/environment/async_env/offline_async_env.py
+++ corerl/environment/async_env/offline_async_env.py
@@ -1,15 +1,13 @@
 import numpy as np
 import pandas as pd
-import gymnasium as gym
-from datetime import datetime, timedelta, UTC
-from corerl.configs.config import config, MISSING
-from dataclasses import dataclass
-from corerl.data_pipeline.tag_config import TagConfig
-from corerl.environment.async_env.async_env import AsyncEnv, StepData
-from corerl.utils.gym import space_bounds
-from corerl.data_pipeline.db.data_reader import DataReader, TagDBConfig
+from omegaconf import DictConfig
+from datetime import datetime, UTC
 
-import yaml
+from corerl.configs.config import config, MISSING
+from corerl.data_pipeline.tag_config import TagConfig
+from corerl.environment.async_env.async_env import AsyncEnv
+from corerl.data_pipeline.db.data_reader import DataReader, TagDBConfig
+from corerl.environment.reward.scrubber import ScrubberReward
 
 
 @config()
@@ -17,7 +15,7 @@ class OfflineAsyncEnvConfig:
     name: str = "offline_async_env"
     seed: int = 0
     discrete_control: bool = False
-    # gym_name: str = MISSING
+    gym_name: str = MISSING
     bucket_width: str = MISSING
     aggregation: str = MISSING
     env_start_time: str = MISSING
@@ -28,11 +26,13 @@ class OfflineAsyncEnvConfig:
 
 class OfflineAsyncEnv(AsyncEnv):
     def __init__(self, cfg: OfflineAsyncEnvConfig, tags: list[TagConfig]):
+        self.gym_name = cfg.gym_name
 
         self.bucket_width = pd.Timedelta(cfg.bucket_width).to_pytimedelta()
         self.env_step_time = pd.Timedelta(cfg.env_step_time).to_pytimedelta()
         self._data_reader = DataReader(db_cfg=cfg.db)
 
+        # Getting defaults for start and end time
         if cfg.env_start_time == MISSING or cfg.env_end_time == MISSING:
             time_stats = self._data_reader.get_time_stats()
 
@@ -50,40 +50,63 @@ class OfflineAsyncEnv(AsyncEnv):
                 cfg.env_end_time,
                 '%Y-%m-%d %H:%M:%S').replace(tzinfo=UTC)
 
-        print(self.env_start_time, self.env_end_time)
         self.current_start_time = self.env_start_time
 
         self.names = [
             tag.name for tag in tags
         ]
 
-        self._action: np.ndarray | None = None
-        self._last_step: StepData | None = None
+        self.obs_names = [
+            tag.name for tag in tags if not tag.is_action
+        ]
+
+        self.action_names = [
+            tag.name for tag in tags if tag.is_action
+        ]
+
+        # This could come from a corerl reward function factory
+        self._reward_func = self._init_reward()
 
     def emit_action(self, action: np.ndarray) -> None:
-        self._action = action
+        pass
 
     def get_latest_obs(self) -> pd.DataFrame:
-        obs = self._data_reader.batch_aggregated_read(
+
+        # Since this is ONLINE, we use single aggregated read
+        res = self._data_reader.single_aggregated_read(
             self.names,
             self.current_start_time,
             self.current_start_time + self.env_step_time,
-            bucket_width=self.bucket_width,
         )
 
         self.current_start_time += self.env_step_time
         if self.current_start_time >= self.env_end_time:
             self.current_start_time = self.env_start_time
+            term = True
+        else:
+            term = False
 
-        # increment time
-        # if its finished set term to true
-        # reset
-        return obs
+        res["reward"] = self._reward_func(res)
+        res["trunc"] = 0.0
+        res["term"] = term
 
-    def _reset():
-        # Reset the counter
-        # Return first obs
-        ...
+        return res
 
+    def _init_reward(self):
+        match self.gym_name:
+            case "epcor_tsdb_scrubber":
+                reward_cfg = DictConfig({
+                    'reward_tags': None,
+                    'steps_per_interaction': 1,
+                    'only_dp_transitions': False,
+                    'type': 'mean_efficiency_cost',
+                })
+                scrubber_reward = ScrubberReward(reward_cfg)
 
+                # This wrapper is for calling scrubber_reward in a simpler manner
+                def wrapped_scrubber_reward(res: pd.DataFrame):
+                    return scrubber_reward(None, df_rows=res)
 
+                return wrapped_scrubber_reward
+            case _:
+                raise NotImplementedError
