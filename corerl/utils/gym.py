05254fb56609344f8896a687787f8684645b7646
Andy Patterson <andy@rlcore.ai>
2024-12-19 14:55:18 -0700

    refactor: migrate entirely from gym to gymnasium

--- corerl/utils/gym.py
+++ corerl/utils/gym.py
@@ -1,15 +1,9 @@
-import gym
-import gym.spaces
-
 import gymnasium
 import gymnasium.spaces
 from typing import Any
 
-def space_bounds(space: gym.Space[Any] | gymnasium.Space[Any]):
-    assert isinstance(
-        space,
-        (gym.spaces.Box, gymnasium.spaces.Box),
-    )
+def space_bounds(space: gymnasium.Space[Any]):
+    assert isinstance(space, gymnasium.spaces.Box)
     return (
         space.low,
         space.high,
