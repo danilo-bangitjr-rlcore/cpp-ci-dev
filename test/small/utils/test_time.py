43bf916667548f549d9c230c1337b9490f3545f5
Kerrick Johnstonbaugh <kerrick@rlcore.ai>
2025-01-13 09:43:12 -0700

    chore: simplify split_into_chunks util given updates to data reader

--- test/small/utils/test_time.py
+++ test/small/utils/test_time.py
@@ -9,9 +9,9 @@ def test_split_into_chunks():
     chunks = time_util.split_into_chunks(start, end, Δ)
 
     assert list(chunks) == [
-        (datetime(2024, 8, 1, 1), datetime(2024, 8, 1, 1, 59, 59)),
-        (datetime(2024, 8, 1, 2), datetime(2024, 8, 1, 2, 59, 59)),
-        (datetime(2024, 8, 1, 3), datetime(2024, 8, 1, 3, 59, 59)),
-        (datetime(2024, 8, 1, 4), datetime(2024, 8, 1, 4, 59, 59)),
+        (datetime(2024, 8, 1, 1), datetime(2024, 8, 1, 2)),
+        (datetime(2024, 8, 1, 2), datetime(2024, 8, 1, 3)),
+        (datetime(2024, 8, 1, 3), datetime(2024, 8, 1, 4)),
+        (datetime(2024, 8, 1, 4), datetime(2024, 8, 1, 5)),
         (datetime(2024, 8, 1, 5), datetime(2024, 8, 1, 5, 30)),
     ]
