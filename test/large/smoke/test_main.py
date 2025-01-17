0d9847b7ce8208ef716b4a0082e79cdbd08adacb
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-01-17 15:05:25 -0700

    chore: explicitly added metrics.name =db to large tests

--- test/large/smoke/test_main.py
+++ test/large/smoke/test_main.py
@@ -31,7 +31,7 @@ def test_main_configs(
     proc = subprocess.run([
         'uv', 'run', 'python', 'main.py',
         '--config-name', f'{config_name}', 'experiment.max_steps=25',
-        f'metrics.port={port}', 'metrics.enabled=True',
+        f'metrics.port={port}', 'metrics.enabled=True', 'metrics.name=db',
         f'metrics.db_name={tsdb_tmp_db_name}', 'metrics.lo_wm=1',
     ])
     proc.check_returncode()
