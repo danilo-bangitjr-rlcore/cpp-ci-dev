ecc860fe5f16bca5ef1cfdc2b138a2086e6e3934
Andy Patterson <andy@rlcore.ai>
2025-01-19 22:02:07 -0700

    feat: add masked autoencoder imputation strategy

--- corerl/data_pipeline/imputers/factory.py
+++ corerl/data_pipeline/imputers/factory.py
@@ -2,14 +2,17 @@ from typing import Annotated
 
 from pydantic import Field
 
+from corerl.data_pipeline.imputers.auto_encoder import MaskedAEConfig, MaskedAutoencoder
 from corerl.data_pipeline.imputers.base import imputer_group
 from corerl.data_pipeline.imputers.imputer_stage import PerTagImputer, PerTagImputerConfig
 from corerl.data_pipeline.tag_config import TagConfig
 
 imputer_group.dispatcher(PerTagImputer)
+imputer_group.dispatcher(MaskedAutoencoder)
 
 ImputerStageConfig = Annotated[(
     PerTagImputerConfig
+    | MaskedAEConfig
 ), Field(discriminator='name')]
 
 def init_imputer(imputer_cfg: ImputerStageConfig, tag_cfgs: list[TagConfig]):
