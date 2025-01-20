166e01bdf7727ec0c3ec94d838833f82a3e5dead
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-01-20 08:21:00 -0700

    feat: adding reward metric in sim interaction

--- corerl/interaction/sim_interaction.py
+++ corerl/interaction/sim_interaction.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from corerl.agent.base import BaseAgent
 from corerl.configs.config import config
-from corerl.data_pipeline.datatypes import CallerCode, PipelineFrame, StageCode
+from corerl.data_pipeline.datatypes import CallerCode, PipelineFrame, StageCode, Transition
 from corerl.data_pipeline.pipeline import Pipeline
 from corerl.environment.async_env.async_env import AsyncEnv
 from corerl.interaction.interaction import Interaction
@@ -30,18 +30,28 @@ class SimInteraction(Interaction):
         self._pipeline = pipeline
         self._env = env
         self._agent = agent
+        self._app_state = app_state
 
         self._column_desc = pipeline.column_descriptions
 
         self._should_reset = True
         self._last_state: np.ndarray | None = None
+        self._last_reward: float | None = None
         self._pipeline.register_hook(StageCode.SC, self._capture_last_state)
+        self._pipeline.register_hook(StageCode.RC, self._capture_last_reward)
+
 
 
     def step(self):
         o = self._env.get_latest_obs()
         pr = self._pipeline(o, caller_code=CallerCode.ONLINE, reset_temporal_state=self._should_reset)
         assert pr.transitions is not None
+        r = self._get_latest_reward()
+        assert r is not None
+        self._app_state.metrics.write(
+            metric='reward',
+            value=r,
+        )
         self._agent.update_buffer(pr.transitions)
         self._agent.update()
 
@@ -70,6 +80,17 @@ class SimInteraction(Interaction):
             .to_numpy(dtype=np.float32)
         )
 
+    def _capture_last_reward(self, pf: PipelineFrame):
+        if pf.caller_code != CallerCode.ONLINE:
+            return
+
+        row = pf.data.tail(1)
+
+        self._last_reward = float(
+            row["reward"]
+            .iloc[0]
+        )
+
 
     def _get_latest_state(self) -> np.ndarray | None:
         if self._last_state is None:
@@ -77,3 +98,11 @@ class SimInteraction(Interaction):
             return None
 
         return self._last_state
+
+
+    def _get_latest_reward(self) -> float | None:
+        if self._last_reward is None:
+            logger.error("Tried to get interaction reward, but none existed")
+            return None
+
+        return self._last_reward
