c191b318083de62b235e7a94891d822e877a949e
jdong-rlcore <jdong@rlcore.ai>
2025-01-23 11:56:55 -0700

    fix: fix ensemble reduction config

--- corerl/component/network/networks.py
+++ corerl/component/network/networks.py
@@ -11,7 +11,7 @@ import corerl.component.layer as layer
 import corerl.component.network.utils as utils
 from corerl.component.layer.activations import ActivationConfig
 from corerl.component.network.base import BaseNetworkConfig
-from corerl.component.network.ensemble.reductions import MeanReduct, bootstrap_reduct_group
+from corerl.component.network.ensemble.reductions import MeanReduct, Reduct, bootstrap_reduct_group
 from corerl.configs.config import config, list_
 from corerl.utils.device import device
 
@@ -35,8 +35,8 @@ class NNTorsoConfig(BaseNetworkConfig):
 class EnsembleCriticNetworkConfig(BaseNetworkConfig):
     name: Literal['ensemble'] = 'ensemble'
     ensemble: int = 1
-    bootstrap_reduct: MeanReduct = field(default_factory=MeanReduct)
-    policy_reduct: MeanReduct = field(default_factory=MeanReduct)
+    bootstrap_reduct: Reduct = field(default_factory=MeanReduct)
+    policy_reduct: Reduct = field(default_factory=MeanReduct)
     vmap: bool = False
 
     base: NNTorsoConfig = field(default_factory=NNTorsoConfig)
