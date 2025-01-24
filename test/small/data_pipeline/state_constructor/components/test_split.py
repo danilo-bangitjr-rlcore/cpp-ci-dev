e8413b793b31925633131cbf4b2651b886f03b96
Andy Patterson <andy@rlcore.ai>
2025-01-24 16:31:27 -0700

    test: add more complex temporal state test for split xform

--- test/small/data_pipeline/state_constructor/components/test_split.py
+++ test/small/data_pipeline/state_constructor/components/test_split.py
@@ -7,6 +7,8 @@ from corerl.data_pipeline.constructors.sc import SCConfig, StateConstructor
 from corerl.data_pipeline.datatypes import CallerCode, PipelineFrame, StageCode
 from corerl.data_pipeline.state_constructors.countdown import CountdownConfig
 from corerl.data_pipeline.tag_config import TagConfig
+from corerl.data_pipeline.transforms import DeltaConfig
+from corerl.data_pipeline.transforms.delta import DeltaTemporalState
 from corerl.data_pipeline.transforms.split import SplitConfig, SplitTemporalState
 from corerl.data_pipeline.transforms.trace import TraceConfig, TraceTemporalState
 from test.infrastructure.utils.pandas import dfs_close
@@ -55,11 +57,14 @@ def test_split_ts1():
     })
 
     ts = SplitTemporalState(
-        left_state=TraceTemporalState(
-            mu={
-                'tag_1': np.array([100.]),
-            },
-        ),
+        left_state=[
+            TraceTemporalState(
+                mu={'tag_1': np.array([100.])},
+            ),
+            DeltaTemporalState(
+                last=np.array([5]),
+            ),
+        ],
         right_state=None,
     )
 
@@ -80,7 +85,10 @@ def test_split_ts1():
         cfg=SCConfig(
             defaults=[
                 SplitConfig(
-                    left=[TraceConfig(trace_values=[0.1])],
+                    left=[
+                        TraceConfig(trace_values=[0.1]),
+                        DeltaConfig(),
+                    ],
                     right=[TraceConfig(trace_values=[0.01])],
                 ),
             ],
@@ -93,7 +101,7 @@ def test_split_ts1():
 
     pf = sc(pf)
     expected_data = pd.DataFrame({
-        'tag_1_trace-0.1':  [10.9, 2.89, 2.989, 3.8989, np.nan, 1.0, 1.9, 2.89, 3.889],
+        'tag_1_trace-0.1_delta':  [5.9, -8.01, 0.099, 0.9099, np.nan, np.nan, 0.9, 0.99, 0.999],
         'tag_1_trace-0.01': [1., 1.99, 2.9899, 3.989899, np.nan, 1., 1.99, 2.9899, 3.989899],
     })
 
