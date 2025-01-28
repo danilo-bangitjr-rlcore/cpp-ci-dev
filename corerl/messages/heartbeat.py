bf0ad2f5075d03fe8ba76b99582ec5064daacab2
Kerrick Johnstonbaugh <kerrick@rlcore.ai>
2025-01-28 14:50:01 -0700

    fix: default heartbeat to None instead of relying on enabled flag

--- corerl/messages/heartbeat.py
+++ corerl/messages/heartbeat.py
@@ -5,22 +5,22 @@ from threading import Thread
 
 from asyncua.sync import Client
 
-from corerl.configs.config import MISSING, config
+from corerl.configs.config import config
 from corerl.utils.time import clock_generator, wait_for_timestamp
 
 logger = logging.getLogger(__name__)
 
+
 @config()
 class HeartbeatConfig:
-    opc_conn_url: str = MISSING
-    heartbeat_node_id: str = MISSING
+    opc_conn_url: str | None = None
+    heartbeat_node_id: str | None = None
     heartbeat_period: timedelta = timedelta(seconds=5)
     max_counter: int = 1000
-    enabled: bool = False
 
 
 def heartbeat(cfg: HeartbeatConfig):
-    if not cfg.enabled:
+    if cfg.opc_conn_url is None or cfg.heartbeat_node_id is None:
         return
 
     with Client(cfg.opc_conn_url) as opc_client:
@@ -43,6 +43,7 @@ def heartbeat(cfg: HeartbeatConfig):
         next_heartbeat_ts = next(heartbeat_clock)
         wait_for_timestamp(next_heartbeat_ts)
 
+
 class Heartbeat:
     def __init__(self, cfg: HeartbeatConfig) -> None:
         self.cfg = cfg
@@ -59,6 +60,6 @@ class Heartbeat:
         return heartbeat_thread
 
     def healthcheck(self):
-        if self.cfg.enabled and not self._heartbeat_thread.is_alive():
+        if self.cfg.opc_conn_url is not None and not self._heartbeat_thread.is_alive():
             logger.error("Heartbeat stopped -- defibrillating...")
             self.start_heartbeat(self.cfg)
