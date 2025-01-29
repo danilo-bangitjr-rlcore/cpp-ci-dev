bd575318232e68f2035883d7ee1ecf105637de63
Alex Trudeau <alexandre.trudeau@rlcore.ai>
2025-01-29 16:03:43 -0700

    feat: Implemented Actor-Critic evaluator

--- corerl/eval/config.py
+++ corerl/eval/config.py
@@ -1,9 +1,11 @@
-from dataclasses import field
 from functools import partial
+
+from pydantic import Field
 
 from corerl.agent.base import BaseAgent
 from corerl.configs.config import config
 from corerl.data_pipeline.pipeline import Pipeline
+from corerl.eval.actor_critic import ActorCriticEvalConfig
 from corerl.eval.monte_carlo import MonteCarloEvalConfig
 from corerl.eval.raw_data import RawDataEvalConfig, raw_data_eval
 from corerl.state import AppState
@@ -11,8 +13,9 @@ from corerl.state import AppState
 
 @config()
 class EvalConfig:
-    monte_carlo: MonteCarloEvalConfig = field(default_factory=MonteCarloEvalConfig)
-    raw_data : RawDataEvalConfig = field(default_factory=RawDataEvalConfig)
+    actor_critic: ActorCriticEvalConfig = Field(default_factory=ActorCriticEvalConfig)
+    monte_carlo: MonteCarloEvalConfig = Field(default_factory=MonteCarloEvalConfig)
+    raw_data : RawDataEvalConfig = Field(default_factory=RawDataEvalConfig)
 
 
 def register_evals(cfg: EvalConfig, agent: BaseAgent, pipeline: Pipeline, app_state: AppState):
