f090f866fb3fec93ba169e83d8906ae82ba33088
Alex Trudeau <alexandre.trudeau@rlcore.ai>
2025-01-30 15:22:50 -0700

    feat: implemented Metrics Table read() method for both TSDB and Pandas

--- corerl/state.py
+++ corerl/state.py
@@ -1,10 +1,10 @@
 from corerl.eval.evals import EvalWriterProtocol
-from corerl.eval.metrics import MetricsWriterProtocol
+from corerl.eval.metrics import MetricsTableProtocol
 from corerl.messages.event_bus import EventBus
 
 
 class AppState:
-    def __init__(self, metrics: MetricsWriterProtocol, evals: EvalWriterProtocol, event_bus: EventBus):
+    def __init__(self, metrics: MetricsTableProtocol, evals: EvalWriterProtocol, event_bus: EventBus):
         self.metrics = metrics
         self.evals = evals
         self.event_bus = event_bus
