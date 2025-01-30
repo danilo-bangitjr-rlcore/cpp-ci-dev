3e1c3b86f238106cbb15a824f8ad009e2cc79007
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-01-30 10:40:59 -0700

    chore: configified

--- corerl/eval/coverage.py
+++ corerl/eval/coverage.py
@@ -1,18 +1,20 @@
-from dataclasses import dataclass
-from typing import Protocol
+from dataclasses import dataclass, field
+from typing import List, Protocol, Union
 
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
 from sklearn.neighbors import KDTree
 
+from corerl.config import config
+
 
 @dataclass
 class Dataset:
-    data : pd.DataFrame
-    action_tags : list[str] | None = None
-    state_tags  : list[str] | None = None
-    reward_tags : list[str] | None = None
+    data: pd.DataFrame
+    action_tags: list[str] = field(default_factory=list)
+    state_tags: list[str] = field(default_factory=list)
+    reward_tags: list[str] = field(default_factory=list)
 
     @property
     def size(self):
@@ -20,17 +22,12 @@ class Dataset:
 
     @property
     def action_dim(self):
-        if self.action_tags is None:
+        if not self.action_tags:
             return 0
         return len(self.action_tags)
 
     def subset(self, data: pd.DataFrame) -> "Dataset":
-        return Dataset(
-            data,
-            self.action_tags,
-            self.state_tags,
-            self.reward_tags,
-        )
+        return Dataset(data, self.action_tags, self.state_tags, self.reward_tags)
 
 
 # ---------------------------- coverage functions ---------------------------- #
@@ -38,132 +35,134 @@ class Dataset:
 
 class CoverageProtocol(Protocol):
     def unnorm_cov(self, state_action: np.ndarray) -> np.ndarray:
+        """
+        Returns an unnormalized coverage value for state, action pairs. This coverage is in [0, inf), where
+        higher values indicate "less covered".
+        """
         ...
 
     def cov(self, state_action: np.ndarray) -> np.ndarray:
+        """
+        Returns an normalized coverage value for state, action pairs.
+        """
         ...
 
 
-class KDECoverage():
-    def __init__(self, epsilon: float, n_norm_samples: int):
+@config()
+class BaseCoverageConfig:
+    epsilon: float = 0.01
+    n_norm_samples: int = 1000
+
+
+class KDECoverage:
+    def __init__(self, cfg: BaseCoverageConfig):
         self.estimator = None
-        self.epsilon = epsilon
-        self.n_norm_samples = n_norm_samples
+        self.cfg = cfg
         self.norm_const = None
 
     def unnorm_cov(self, state_action: np.ndarray) -> np.ndarray:
         assert self.estimator is not None
         # scipy expects df to have shape (#dim, #dimensions)
-        density = self.estimator(state_action.T)
-        mapped_density = np.arctan(density) *(2/np.pi)
-        return 1-mapped_density
+        density = self.estimator(state_action.T)  # in [0, inf)
+        mapped_density = np.arctan(density) * (2 / np.pi)  # in [0, 1]
+        return 1 - mapped_density  # makes 0 correspond to "covered"
 
     def cov(self, state_action: np.ndarray) -> np.ndarray:
         assert self.norm_const is not None
-        return (self.unnorm_cov(state_action) / self.norm_const)
+        return self.unnorm_cov(state_action) / self.norm_const
 
     def fit(self, dataset: Dataset) -> None:
         # scipy expects df to have shape (#dim, #dimensions)
         self.estimator = stats.gaussian_kde(dataset.data.transpose())
-        self.norm_const = get_norm_const(self, dataset, self.epsilon, self.n_norm_samples)
+        self.norm_const = get_norm_const(self, dataset, self.cfg.epsilon, self.cfg.n_norm_samples)
 
 
-class NeighboursCoverage():
-    def __init__(self, epsilon: float, n_norm_samples: int):
-        self.estimator = None
-        self.n_neighbours = 1
-        self.epsilon = epsilon
-        self.n_norm_samples = n_norm_samples
+@config()
+class NeighboursCoverageConfig(BaseCoverageConfig):
+    n_neighbours: int = 1
+    metric: str = "l2"
+
+
+class NeighboursCoverage:
+    def __init__(self, cfg: NeighboursCoverageConfig):
+        self.tree = None
+        self.cfg = cfg
         self.norm_const = None
         self.mapping = lambda x: x
-        self.metric = "l2"
 
     def unnorm_cov(self, state_action: np.ndarray) -> np.ndarray:
-        dist, _ = self.tree.query(state_action, k=self.n_neighbours)
+        assert self.tree is not None
+        dist, _ = self.tree.query(state_action, k=self.cfg.n_neighbours)
         return np.mean(dist, axis=1)
 
     def cov(self, state_action: np.ndarray) -> np.ndarray:
         assert self.norm_const is not None
-        return (self.unnorm_cov(state_action) / self.norm_const)
+        return self.unnorm_cov(state_action) / self.norm_const
 
     def fit(self, dataset: Dataset) -> None:
-        self.tree = KDTree(dataset.data.to_numpy(), metric=self.metric)
-        self.norm_const = get_norm_const(self, dataset, self.epsilon, self.n_norm_samples)
-
+        self.tree = KDTree(dataset.data.to_numpy(), metric=self.cfg.metric)
+        self.norm_const = get_norm_const(self, dataset, self.cfg.epsilon, self.cfg.n_norm_samples)
 
 
 # --------------------------------- utilities -------------------------------- #
 
 
-def sample_epsilon_ball(
-        center: np.ndarray,
-        epsilon: float,
-        n_samples:int=1
-    ) -> np.ndarray:
+def sample_epsilon_ball(center: np.ndarray, epsilon: float, n_samples: int = 1) -> np.ndarray:
     dimension = len(center)
     directions = np.random.normal(0, 1, (n_samples, dimension))
-    directions /= np.linalg.norm(directions, axis=1, keepdims=True) # directions with unit length.
+    directions /= np.linalg.norm(directions, axis=1, keepdims=True)
     points = center + epsilon * directions
     return points
 
 
-def get_norm_const(
-        coverage_fn : CoverageProtocol,
-        dataset: Dataset,
-        epsilon:float=0.01,
-        n_samples:int=1000,
-    ):
+def get_norm_const(coverage_fn: CoverageProtocol, dataset: Dataset, epsilon: float = 0.01, n_samples: int = 1000):
     data = dataset.data
     cumulative_coverage = 0
     for _, row in data.iterrows():
         row_np = row.to_numpy()
         samples = sample_epsilon_ball(row_np, epsilon, n_samples)
-        coverages =  coverage_fn.unnorm_cov(samples)
+        coverages = coverage_fn.unnorm_cov(samples)
         cumulative_coverage += np.sum(coverages)
-
-    return cumulative_coverage / (n_samples*dataset.size)
+    return cumulative_coverage / (n_samples * dataset.size)
 
 
 # --------------------------------- samplers --------------------------------- #
 
 
-class UniformDatasetSampler():
-    def eval(
-            self,
-            dataset: Dataset,
-            coverage_fn: CoverageProtocol,
-            n_samples: int = 1
-        ) -> float:
-        sampled_data = dataset.data.sample(n=n_samples)
+@dataclass
+class BaseSamplerConfig:
+    n_state_samples: int = 1
+    action_low: np.ndarray | float = 0.0
+    action_high: np.ndarray | float = 1.0
+
+
+class UniformDatasetSampler:
+    def __init__(self, cfg: BaseSamplerConfig):
+        self.cfg = cfg
+
+    def eval(self, dataset: Dataset, coverage_fn: CoverageProtocol) -> float:
+        sampled_data = dataset.data.sample(n=self.cfg.n_state_samples)
         coverages = coverage_fn.cov(sampled_data.to_numpy())
         return coverages.mean()
 
 
-class UniformActionSampler():
-    """
-    Samples states from dataset, actions from bounds UAR
-    """
-    def __init__(self, action_low: np.ndarray | float = 0., action_high: np.ndarray | float = 1.):
-        self.action_low = action_low
-        self.action_high = action_high
+@dataclass
+class ActionSamplerConfig(BaseSamplerConfig):
+    n_action_samples: int = 1
 
-    def eval(self,
-            dataset: Dataset,
-            coverage_fn: CoverageProtocol,
-            n_state_samples: int = 1,
-            n_action_samples: int = 1,
-        ) -> float:
 
-        sampled_data = dataset.data.sample(n=n_state_samples)
-        repeated_samples = pd.concat([sampled_data] * n_action_samples, ignore_index=True)
-        sampled_actions =  np.random.uniform(
-            self.action_low,
-            self.action_high,
-            (n_state_samples*n_action_samples, dataset.action_dim))
+class UniformActionSampler:
+    def __init__(self, cfg: ActionSamplerConfig):
+        self.cfg = cfg
 
+    def eval(self, dataset: Dataset, coverage_fn: CoverageProtocol) -> float:
+        sampled_data = dataset.data.sample(n=self.cfg.n_state_samples)
+        repeated_samples = pd.concat([sampled_data] * self.cfg.n_state_samples, ignore_index=True)
+        sampled_actions = np.random.uniform(
+            self.cfg.action_low,
+            self.cfg.action_high,
+            (self.cfg.n_state_samples * self.cfg.n_state_samples, dataset.action_dim),
+        )
         repeated_samples[dataset.action_tags] = sampled_actions
         coverages = coverage_fn.cov(repeated_samples.to_numpy())
         return float(np.mean(coverages))
-
-
-
--- test/small/eval/test_coverage.py
+++ test/small/eval/test_coverage.py
@@ -3,10 +3,14 @@ import pandas as pd
 import pytest
 
 from corerl.eval.coverage import (
+    ActionSamplerConfig,
+    BaseCoverageConfig,
+    BaseSamplerConfig,
     CoverageProtocol,
     Dataset,
     KDECoverage,
     NeighboursCoverage,
+    NeighboursCoverageConfig,
     UniformActionSampler,
     UniformDatasetSampler,
     get_norm_const,
@@ -16,10 +20,12 @@ from corerl.eval.coverage import (
 
 @pytest.fixture
 def dataset():
-    data = pd.DataFrame({
-        'tag-1':  np.arange(0, 1, 0.1),
-        'tag-2':  np.array([0, 1] * 5),
-    })
+    data = pd.DataFrame(
+        {
+            "tag-1": np.arange(0, 1, 0.1),
+            "tag-2": np.array([0, 1] * 5),
+        }
+    )
     dataset = Dataset(
         data=data,
     )
@@ -28,23 +34,23 @@ def dataset():
 
 @pytest.fixture
 def tiny_dataset():
-    data = pd.DataFrame({
-        'tag-1':  [0, 1],
-    })
+    data = pd.DataFrame(
+        {
+            "tag-1": [0, 1],
+        }
+    )
     dataset = Dataset(
         data=data,
     )
     return dataset
 
 
-
 class DumbCoverage:
-    def __init__(self, epsilon: float, n_samples: int):
-        self.epsilon = epsilon
-        self.n_samples = n_samples
+    def __init__(self, cfg: BaseCoverageConfig):
+        self.cfg = cfg
         self.normalization = None
 
-    def unnorm_cov(self, state_action: np.ndarray) ->  np.ndarray:
+    def unnorm_cov(self, state_action: np.ndarray) -> np.ndarray:
         assert self.dataset is not None
         return np.array([0.5])
 
@@ -54,33 +60,37 @@ class DumbCoverage:
 
     def fit(self, dataset: Dataset) -> None:
         self.dataset = dataset
-        self.normalization = get_norm_const(self, dataset, self.epsilon, self.n_samples)
+        self.normalization = get_norm_const(self, dataset, self.cfg.epsilon, self.cfg.n_norm_samples)
 
 
 def test_sample_epsilon_ball():
+    """
+    Samples vector in epsilon ball around center and makes sure they are right shape and length.
+    """
     center = np.array([0.5, 0.5])
     epsilon = 0.1
-    n_samples = 10
+    n_samples = 100
     samples = sample_epsilon_ball(center, epsilon, n_samples)
     assert samples.shape == (n_samples, len(center)), "Sample shape mismatch"
     assert np.allclose(np.linalg.norm(samples - center, axis=1), epsilon), "Samples should be within epsilon distance"
 
 
 def test_get_norm_const(dataset: Dataset):
-    epsilon = 0.1
-    n_samples = 100
-    test_coverage = DumbCoverage(epsilon, n_samples)
+    """
+    Tests that the normalization constant is greater than 0 for the DumbCoverage function.
+    """
+    cfg = BaseCoverageConfig(epsilon=0.1, n_norm_samples=100)
+    test_coverage = DumbCoverage(cfg)
     test_coverage.fit(dataset)
 
-    normalization = get_norm_const(test_coverage, dataset, epsilon, n_samples)
+    normalization = get_norm_const(test_coverage, dataset, cfg.epsilon, cfg.n_norm_samples)
     assert normalization > 0, "Normalization should be greater than 0"
     assert isinstance(normalization, float), "Normalization should be a float value"
 
 
 def test_kde_coverage(dataset: Dataset):
-    epsilon = 0.1
-    n_samples = 100
-    kde_coverage = KDECoverage(epsilon, n_samples)
+    cfg = BaseCoverageConfig(epsilon=0.1, n_norm_samples=100)
+    kde_coverage = KDECoverage(cfg)
     kde_coverage.fit(dataset)
 
     # Test the __call__ method for arbitrary state_action pair
@@ -89,139 +99,138 @@ def test_kde_coverage(dataset: Dataset):
     assert coverage_value > 0, "Coverage value should be greater than 0"
 
     # Test that the normalization is greater than 0
-    normalization = get_norm_const(kde_coverage, dataset, epsilon, n_samples)
+    normalization = get_norm_const(kde_coverage, dataset, cfg.epsilon, cfg.n_norm_samples)
     assert normalization > 0, "Normalization should be greater than 0"
 
 
+def _test_uniform_dataset_sampler(sampler: UniformDatasetSampler, coverage_fn: CoverageProtocol, dataset: Dataset):
+    """
+    Tests the UniformDataetSampler's ability to evaluate action coverage on a dataset.
 
-def _test_uniform_dataset_sampler(
-        coverage_fn : CoverageProtocol,
-        dataset: Dataset,
-        n_state_samples: int,
-        ):
+    This test performs the following steps:
+    * Evaluates the coverage on the provided dataset and asserts that the coverage value is between 0 and 1.
+    * Creates a test dataset with low coverage and evaluates its coverage is greater than 1.
+    """
 
-    sampler = UniformDatasetSampler()
     # Evaluate coverage on the dataset itself (should be very well-covered)
-    coverage_value = sampler.eval(dataset, coverage_fn, n_state_samples)
-    assert 0 <= abs(coverage_value ) <= 1, "Coverage value should be between 0 and 1."
+    coverage_value_1 = sampler.eval(dataset, coverage_fn)
+    assert 0 <= coverage_value_1, "Coverage value should greater than zero."
+    assert abs(coverage_value_1 - 1) <= 1.1, "Coverage value should be around 1."
 
     # Evaluate coverage on a test dataset which is very far from the original dataset.
-    test_data = pd.DataFrame({
-        'tag-1':  [10, 10],
-    })
+    test_data = pd.DataFrame(
+        {
+            "tag-1": [10, 10],
+        }
+    )
     test_dataset = Dataset(
         data=test_data,
     )
 
-    coverage_value = sampler.eval(test_dataset, coverage_fn, n_state_samples)
-    assert abs(coverage_value) >= 1, "Coverage value should be larger than 1."
-
+    coverage_value_2 = sampler.eval(test_dataset, coverage_fn)
+    assert coverage_value_2 >= 1, "Coverage value should be larger than 1."
+    assert coverage_value_2 >= coverage_value_1
 
 
 def test_kde_with_uniform_dataset_sampler(tiny_dataset: Dataset):
     np.random.seed(42)
-    epsilon = 0.1
-    n_norm_samples = 1000
-    n_state_samples = 1
+    coverage_cfg = BaseCoverageConfig(epsilon=0.1, n_norm_samples=1000)
+    sampler_cfg = BaseSamplerConfig(n_state_samples=1)
 
-    kde_coverage = KDECoverage(epsilon, n_norm_samples)
+    kde_coverage = KDECoverage(coverage_cfg)
     kde_coverage.fit(tiny_dataset)
-    _test_uniform_dataset_sampler(kde_coverage, tiny_dataset, n_state_samples)
+    sampler = UniformDatasetSampler(sampler_cfg)
+    _test_uniform_dataset_sampler(sampler, kde_coverage, tiny_dataset)
 
 
 def test_neighbours_with_uniform_dataset_sampler(tiny_dataset: Dataset):
     np.random.seed(42)
-    epsilon = 0.1
-    n_norm_samples = 1000
-    n_state_samples = 1
+    coverage_cfg = NeighboursCoverageConfig(epsilon=0.1, n_norm_samples=1000, n_neighbours=1, metric="l2")
+    sampler_cfg = BaseSamplerConfig(n_state_samples=1)
 
-    neighbours_coverage = NeighboursCoverage(epsilon, n_norm_samples)
+    neighbours_coverage = NeighboursCoverage(coverage_cfg)
     neighbours_coverage.fit(tiny_dataset)
-    _test_uniform_dataset_sampler(neighbours_coverage, tiny_dataset, n_state_samples)
+    sampler = UniformDatasetSampler(sampler_cfg)
+    _test_uniform_dataset_sampler(sampler, neighbours_coverage, tiny_dataset)
 
 
-def _test_uniform_action_sampler(
-        coverage_fn: CoverageProtocol,
-        dataset: Dataset,
-        n_state_samples: int,
-        n_action_samples: int,
-    ):
-    sampler = UniformActionSampler()
+def _test_uniform_action_sampler(coverage_fn: CoverageProtocol, dataset: Dataset, cfg: ActionSamplerConfig):
+    """
+    Tests the UniformActionSampler's ability to evaluate action coverage on a dataset.
+
+    This test performs the following steps:
+    * Evaluates the coverage on the provided dataset and asserts that the coverage value is greater than 0.
+    * Creates a test dataset with high action coverage and evaluates its coverage.
+    * Creates a test dataset with low action coverage and evaluates its coverage.
+    * Asserts that the coverage value for the high action coverage dataset is less than or equal to the coverage value for the low action coverage dataset.
+    """
+    sampler = UniformActionSampler(cfg)
 
     # Evaluate coverage on the dataset itself
-    coverage_value = sampler.eval(dataset, coverage_fn, n_state_samples, n_action_samples)
-    assert coverage_value > 0
+    coverage_value_0 = sampler.eval(dataset, coverage_fn)
+    assert coverage_value_0 > 0
 
     half_dataset_size = len(dataset.data) // 2
     test_tag_2 = list(np.random.uniform(0, 1, half_dataset_size))
 
     # make a test dataset with only the states with high action coverage
-    test_data_high_cov = pd.DataFrame({
-        'tag-1':  [0]*half_dataset_size, # "observation" tag
-        'tag-2': test_tag_2, # "action" tag
-    })
-
-    test_dataset_high_cov = Dataset(
-        data=test_data_high_cov,
-        action_tags=['tag-2']
+    test_data_high_cov = pd.DataFrame(
+        {
+            "tag-1": [0] * half_dataset_size,  # "observation" tag
+            "tag-2": test_tag_2,  # "action" tag
+        }
     )
+    test_dataset_high_cov = Dataset(data=test_data_high_cov, action_tags=["tag-2"])
 
-    coverage_value_1 = sampler.eval(test_dataset_high_cov, coverage_fn, n_state_samples, n_action_samples)
+    coverage_value_1 = sampler.eval(test_dataset_high_cov, coverage_fn)
 
     # make a test dataset with only the states with low action coverage
-    test_data_low_cov = pd.DataFrame({
-        'tag-1':  [1]*half_dataset_size, # "observation" tag
-        'tag-2':  test_tag_2 # "action" tag
-    })
-
-    test_dataset_low_cov = Dataset(
-        data=test_data_low_cov,
-        action_tags=['tag-2']
+    test_data_low_cov = pd.DataFrame(
+        {
+            "tag-1": [1] * half_dataset_size,  # "observation" tag
+            "tag-2": test_tag_2,  # "action" tag
+        }
     )
-    coverage_value_2 = sampler.eval(test_dataset_low_cov, coverage_fn, n_state_samples, n_action_samples)
+    test_dataset_low_cov = Dataset(data=test_data_low_cov, action_tags=["tag-2"])
+
+    coverage_value_2 = sampler.eval(test_dataset_low_cov, coverage_fn)
 
     assert coverage_value_1 <= coverage_value_2
 
 
 def test_kde_with_uniform_action_sampler():
-    np.random.seed(42)
-    epsilon = 0.1
-    n_norm_samples = 1000
-    n_state_samples = 1
-    n_action_samples = 100
+    np.random.seed(0)
+    coverage_cfg = BaseCoverageConfig(epsilon=0.01, n_norm_samples=1000)
+    sampler_cfg = ActionSamplerConfig(n_state_samples=1, n_action_samples=100)
 
-    half_dataset_size = 50
-    data = pd.DataFrame({
-        'tag-1':  [0]*half_dataset_size + [1]*half_dataset_size,
-        'tag-2':  list(np.random.uniform(0, 1, half_dataset_size)) + [0] * half_dataset_size,
-    })
-    dataset = Dataset(
-        data=data,
-        action_tags=['tag-2']
+    half_dataset_size = 100
+    data = pd.DataFrame(
+        {
+            "tag-1": [0] * half_dataset_size + [1] * half_dataset_size,
+            "tag-2": list(np.random.uniform(0, 1, half_dataset_size)) + [0] * half_dataset_size,
+        }
     )
+    dataset = Dataset(data=data, action_tags=["tag-2"])
 
-    kde_coverage = KDECoverage(epsilon, n_norm_samples)
+    kde_coverage = KDECoverage(coverage_cfg)
     kde_coverage.fit(dataset)
-    _test_uniform_action_sampler(kde_coverage, dataset, n_state_samples, n_action_samples)
+    _test_uniform_action_sampler(kde_coverage, dataset, sampler_cfg)
 
 
 def test_neighbours_with_uniform_action_sampler():
     np.random.seed(42)
-    epsilon = 0.1
-    n_norm_samples = 1000
-    n_state_samples = 1
-    n_action_samples = 100
+    coverage_cfg = NeighboursCoverageConfig(epsilon=0.1, n_norm_samples=1000, n_neighbours=1, metric="l2")
+    sampler_cfg = ActionSamplerConfig(n_state_samples=1, n_action_samples=100)
 
     half_dataset_size = 50
-    data = pd.DataFrame({
-        'tag-1':  [0]*half_dataset_size + [1]*half_dataset_size,
-        'tag-2':  list(np.random.uniform(0, 1, half_dataset_size)) + [0] * half_dataset_size,
-    })
-    dataset = Dataset(
-        data=data,
-        action_tags=['tag-2']
+    data = pd.DataFrame(
+        {
+            "tag-1": [0] * half_dataset_size + [1] * half_dataset_size,
+            "tag-2": list(np.random.uniform(0, 1, half_dataset_size)) + [0] * half_dataset_size,
+        }
     )
+    dataset = Dataset(data=data, action_tags=["tag-2"])
 
-    neighbours_coverage = NeighboursCoverage(epsilon, n_norm_samples)
+    neighbours_coverage = NeighboursCoverage(coverage_cfg)
     neighbours_coverage.fit(dataset)
-    _test_uniform_action_sampler(neighbours_coverage, dataset, n_state_samples, n_action_samples)
+    _test_uniform_action_sampler(neighbours_coverage, dataset, sampler_cfg)
