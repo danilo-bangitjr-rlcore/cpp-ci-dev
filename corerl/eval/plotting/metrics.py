f339d47ad0fde57f5047704fbc8b50b9177c73c1
Alex Trudeau <alexandre.trudeau@rlcore.ai>
2025-02-03 13:58:17 -0700

    feat: implemented Monte-Carlo Eval plotting function and a function that calls all metrics plotting functions

---
+++ corerl/eval/plotting/metrics.py
@@ -0,0 +1,88 @@
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+
+from datetime import datetime
+from pathlib import Path
+
+from corerl.config import MainConfig
+from corerl.configs.config import list_
+from corerl.state import AppState
+
+def make_mc_eval_plot(
+    cfg: MainConfig,
+    app_state: AppState,
+    save_path: Path,
+    step_start: int | None = None,
+    step_end: int | None = None,
+    start_time: datetime | None = None,
+    end_time: datetime | None = None,
+    labels: list[str] = list_(),
+):
+    if not cfg.eval_cfgs.monte_carlo.enabled:
+        return
+
+    for label in labels:
+        print("Make MC Eval Plot Label:", label)
+        state_v_df = app_state.metrics.read(
+            metric=f"state_v_{label}",
+            step_start=step_start,
+            step_end=step_end,
+            start_time=start_time,
+            end_time=end_time
+        )
+        observed_a_q_df = app_state.metrics.read(
+            metric=f"observed_a_q_{label}",
+            step_start=step_start,
+            step_end=step_end,
+            start_time=start_time,
+            end_time=end_time
+        )
+        partial_return_df = app_state.metrics.read(
+            metric=f"partial_return_{label}",
+            step_start=step_start,
+            step_end=step_end,
+            start_time=start_time,
+            end_time=end_time
+        )
+
+        if "time" in state_v_df:
+            x_axis = state_v_df["time"].to_numpy()
+        elif "agent_step" in state_v_df:
+            x_axis = state_v_df["agent_step"].to_numpy()
+        else:
+            x_axis = list(range(len(state_v_df)))
+
+        state_vs = state_v_df["value"].to_numpy()
+        observed_a_qs = observed_a_q_df["value"].to_numpy()
+        partial_returns = partial_return_df["value"].to_numpy()
+
+        plt.scatter(x_axis, state_vs, s=10, label="Agent Q(s, a~pi(s))")
+        plt.scatter(x_axis, observed_a_qs, s=10, label="Agent Q(s, observed_a)")
+        plt.scatter(x_axis, partial_returns, s=10, label="Observed Return")
+
+        if "time" in state_v_df:
+            interval = int(float((end_time - start_time).days) / 15.0)
+            plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+            plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=interval))
+            plt.gcf().autofmt_xdate()
+
+        plt.ylabel("Return")
+        plt.xlabel("Time")
+        plt.legend()
+        plt.savefig(save_path / f"monte_carlo_eval_{label}.png")
+        plt.close()
+
+def plot_metrics(
+    cfg: MainConfig,
+    app_state: AppState,
+    save_path: Path,
+    step_start: int | None = None,
+    step_end: int | None = None,
+    start_time: datetime | None = None,
+    end_time: datetime | None = None,
+    labels: list[str] = list_(),
+):
+    if len(labels) == 0:
+        labels = [""]
+
+    make_mc_eval_plot(cfg, app_state, save_path, step_start, step_end, start_time, end_time, labels)
