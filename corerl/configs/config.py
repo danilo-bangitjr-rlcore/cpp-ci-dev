6f2d3d1abf8421210377c7edd6effa949a4bc36f
Andy Patterson <andy@rlcore.ai>
2025-02-05 19:33:54 -0700

    feat: add computed and sanitizer config utilities
    The computed utility mutates an existing config key based on
    intermediate config values found anywhere else in the config tree.
    A trivial example is interpolation --- the computed value is a copy of
    a value stored somewhere else on the config tree.
    
    The sanitizer is a post-processing sanity checker. It can raise
    arbitrary exceptions and asserts, which will get picked up by pydantic
    and turned into nice error messages.
    
    Both of these hook into the second pass of the config parser. This means
    they can access values based on discriminated union resolutions. However
    there is no guarantee on parsing or execution order, so computeds should
    not rely on the outcome of other computeds.

--- corerl/configs/config.py
+++ corerl/configs/config.py
@@ -1,8 +1,9 @@
 import copy
+from collections.abc import Callable
 from dataclasses import field
 from typing import Any, dataclass_transform
 
-from pydantic import Field, PrivateAttr
+from pydantic import Field, PrivateAttr, ValidationInfo, model_validator
 from pydantic.dataclasses import dataclass as pydantic_dataclass
 
 MISSING: Any = "|???|"
@@ -16,6 +17,41 @@ def list_(vals: list[Any] | None = None) -> Any:
 
 def interpolate(path: str) -> Any:
     return path
+
+
+def sanitizer[M](f: Callable[[Any, M], Any]):
+    def _inner(self: Any, info: ValidationInfo):
+        if info.context is None:
+            return self
+
+        f(self, info.context)
+        return self
+
+    return model_validator(mode='after')(_inner)
+
+
+def computed[M](key: str):
+    def _computed_processor(f: Callable[[type[Any], M], Any]):
+        def _model_validator_callback(cls: Any, data: object, info: ValidationInfo):
+            if info.context is None:
+                return data
+
+            assert isinstance(data, dict)
+
+            # if the value is already specified in the data
+            # then we've overridden the computed logic
+            if key in data and data[key] != MISSING:
+                return data
+
+            # because f is _actually_ a descriptor instead of a function
+            # due to classmethod, then we need to get to the underlying
+            # function using the descriptor api
+            data[key] = f.__get__(None, cls)(info.context)
+            return data
+
+        return model_validator(mode='before')(_model_validator_callback)
+    return _computed_processor
+
 
 
 @dataclass_transform(field_specifiers=(field, Field, PrivateAttr))
