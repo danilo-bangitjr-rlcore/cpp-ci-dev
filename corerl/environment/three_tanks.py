482f6a46c246993026133b6ee6dcc2fd4abb5540
Alex Trudeau <alexandre.trudeau@rlcore.ai>
2025-02-07 11:24:10 -0700

    style: trying to fix three_tanks issues pt 2

--- corerl/environment/three_tanks.py
+++ corerl/environment/three_tanks.py
@@ -68,7 +68,8 @@ class ThreeTankEnvBase:
         type2_c = np.array(type2_c, dtype=float)
         sys2 = signal.TransferFunction([self.processgain], type2_c)
         sys2 = sys2.to_ss()
-        sys2 = sys2.to_discrete(1) # pyright: ignore[reportAttributeAccessIssue, reportOptionalOperand]
+        sys2 = sys2.to_discrete(1) # type: ignore
+        assert sys2 is not None
         self.isoffline = isoffline
         if self.isoffline:
             self.A = sys2.A * 0.9
