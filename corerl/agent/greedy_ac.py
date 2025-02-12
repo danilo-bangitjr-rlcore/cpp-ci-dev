c57b60d276d20b7bded5be3f57b7ac97e02a7c3d
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-02-12 08:46:01 -0700

    feat: metric logging loss on n_most_recent samples

--- corerl/agent/greedy_ac.py
+++ corerl/agent/greedy_ac.py
@@ -119,6 +119,7 @@ class GreedyACConfig(BaseACConfig):
 
     # metrics
     ingress_loss : bool = True
+    split_batch_loss : bool = True
 
     actor: NetworkActorConfig = Field(default_factory=NetworkActorConfig)
     critic: EnsembleCriticConfig = Field(default_factory=EnsembleCriticConfig)
@@ -336,6 +337,18 @@ class GreedyAC(BaseAC):
         batches = self.critic_buffer.sample()
         q_loss = self._compute_critic_loss(batches, with_grad=True, log_metrics=True)
 
+        log_split_batch_loss = self.cfg.split_batch_loss and self.critic_buffer.n_most_recent > 0
+        if log_split_batch_loss:
+            # grab the most recent samples from the batch and log the loss on only these samples
+            batch_slices = [b[:self.critic_buffer.n_most_recent] for b in batches]
+            n_most_recent_loss = self._compute_critic_loss(batch_slices)
+
+            self._app_state.metrics.write(
+                    agent_step=self._app_state.agent_step,
+                    metric=f"critic_loss_{self.policy_buffer.n_most_recent}_most_recent",
+                    value=n_most_recent_loss,
+            )
+
         if self.eval_batch:
             eval_batches = self.critic_buffer.sample()
         else:
@@ -474,6 +487,21 @@ class GreedyAC(BaseAC):
             metric=policy_name + "_loss",
             value=to_np(loss),
         )
+
+        log_split_batch_loss = self.cfg.split_batch_loss and self.policy_buffer.n_most_recent > 0
+        if log_split_batch_loss:
+            # grab the most recent samples from the batch and log the loss on only these samples
+            n_most_recent_loss = self._compute_policy_loss(
+                policy,
+                update_batch[:self.policy_buffer.n_most_recent],
+                percentile,
+            )
+
+            self._app_state.metrics.write(
+                    agent_step=self._app_state.agent_step,
+                    metric=f"{policy_name}_loss_{self.policy_buffer.n_most_recent}_most_recent",
+                    value=n_most_recent_loss,
+            )
 
         if self.eval_batch:
             # sample another batch for the evaluation of updates when using line search.
