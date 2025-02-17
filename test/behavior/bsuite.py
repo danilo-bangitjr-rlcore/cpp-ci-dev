4e8528880f9b946ddae068d4fd98b503f07797bd
Andy Patterson <andy@rlcore.ai>
2025-02-16 21:35:06 -0700

    test: refactor out bsuite test logic from saturation

---
+++ test/behavior/bsuite.py
@@ -0,0 +1,85 @@
+import subprocess
+import warnings
+
+import numpy as np
+import pandas as pd
+from sqlalchemy import Engine
+
+from corerl.sql_logging.sql_logging import table_exists
+
+
+class BSuiteTestCase:
+    name: str
+    config: str
+    lower_bounds: dict[str, float] = {}
+    upper_bounds: dict[str, float] = {}
+    lower_warns: dict[str, float] = {}
+    upper_warns: dict[str, float] = {}
+
+    overrides: dict[str, object] | None = None
+
+    def __init__(self):
+        self._overrides = self.overrides or {}
+
+
+    def execute_test(self, tsdb: Engine, port: int, db_name: str):
+        overrides = self._overrides | {
+            'infra.db.port': port,
+            'infra.db.db_name': db_name,
+        }
+
+        parts = [f'{k}={v}' for k, v in overrides.items()]
+
+        proc = subprocess.run([
+            'uv', 'run', 'python', 'main.py',
+            '--base', '.',
+            '--config-name', self.config,
+        ] + parts)
+        proc.check_returncode()
+
+        # ensure metrics table exists
+        assert table_exists(tsdb, 'metrics')
+
+        # ensure some metrics were logged to table
+        with tsdb.connect() as conn:
+            metrics_table = pd.read_sql_table('metrics', con=conn)
+
+        metrics_table = metrics_table.sort_values('agent_step', ascending=True)
+        self._evaluate_outcomes(metrics_table)
+
+
+    def _evaluate_outcomes(self, metrics_table: pd.DataFrame):
+        self._evaluate_warnings(metrics_table)
+        self._evaluate_bounds(metrics_table)
+
+
+    def _evaluate_bounds(self, metrics_table: pd.DataFrame):
+        for metric, expected in self.lower_bounds.items():
+            values = get_metric(metrics_table, metric)
+            got = values[-100:].mean()
+            assert got >= expected, \
+                f'[{self.name}] - {metric} outside of lower bound - {got} >= {expected}'
+
+        for metric, expected in self.upper_bounds.items():
+            values = get_metric(metrics_table, metric)
+            got = values[-100:].mean()
+            assert got <= expected, \
+                f'[{self.name}] - {metric} outside of upper bound - {got} <= {expected}'
+
+
+    def _evaluate_warnings(self, metrics_table: pd.DataFrame):
+        for metric, expected in self.lower_bounds.items():
+            values = get_metric(metrics_table, metric)
+            got = values[-100:].mean()
+            if got < expected:
+                warnings.warn(f'[{self.name}] - {metric} outside of lower bound - {got} >= {expected}', stacklevel=0)
+
+        for metric, expected in self.upper_bounds.items():
+            values = get_metric(metrics_table, metric)
+            got = values[-100:].mean()
+            if got > expected:
+                warnings.warn(f'[{self.name}] - {metric} outside of upper bound - {got} <= {expected}', stacklevel=0)
+
+
+def get_metric(df: pd.DataFrame, metric: str) -> np.ndarray:
+    return df[df['metric'] == metric]['value'].to_numpy()
--- test/behavior/saturation/test_main.py
+++ test/behavior/saturation/test_main.py
@@ -1,59 +1,32 @@
-import subprocess
-
-import numpy as np
-import pandas as pd
 import pytest
 from sqlalchemy import Engine
 
-from corerl.sql_logging.sql_logging import table_exists
+from test.behavior.bsuite import BSuiteTestCase
 
 
-@pytest.mark.parametrize('config,overrides,expected_outcomes', [
-    ('config.yaml', {},   {'reward': -0.2, 'critic_loss': 0.02, 'actor_loss': -1.0}),
-    ('delta.yaml',  {},   {'reward': -0.25, 'critic_loss': 0.01, 'actor_loss': -1.0}),
+class SaturationTest(BSuiteTestCase):
+    name = 'saturation'
+    config = 'test/behavior/saturation/config.yaml'
+
+    lower_bounds = { 'reward': -0.25 }
+    upper_warns = { 'critic_loss': 0.05, 'actor_loss': -1.0 }
+
+
+class DeltaSaturationTest(SaturationTest):
+    name = 'delta saturation'
+    config = 'test/behavior/saturation/delta.yaml'
+
+
+@pytest.mark.parametrize('test_case', [
+    SaturationTest(),
+    DeltaSaturationTest(),
 ])
 @pytest.mark.timeout(900)
 def test_saturation(
-    config: str,
-    overrides: dict[str, object],
-    expected_outcomes: dict[str, float],
+    test_case: BSuiteTestCase,
     tsdb_engine: Engine,
     tsdb_tmp_db_name: str,
-
 ):
     port = tsdb_engine.url.port
     assert port is not None
-
-    overrides |= {
-        'infra.db.port': port,
-        'infra.db.db_name': tsdb_tmp_db_name,
-    }
-    parts = [f'{k}={v}' for k, v in overrides.items()]
-
-    proc = subprocess.run([
-        'uv', 'run', 'python', 'main.py',
-        '--base', 'test/behavior/saturation/',
-        '--config-name', config,
-    ] + parts)
-    proc.check_returncode()
-
-    # ensure metrics table exists
-    assert table_exists(tsdb_engine, 'metrics')
-
-    # ensure some metrics were logged to table
-    with tsdb_engine.connect() as conn:
-        metrics = pd.read_sql_table('metrics', con=conn)
-
-    metrics = metrics.sort_values('agent_step', ascending=True)
-
-    for metric, expected in expected_outcomes.items():
-        values = get_metric(metrics, metric)
-
-        if metric in {'reward'}:
-            assert values[-100:].mean() >= expected
-        else:
-            assert values[-100:].mean() <= expected
-
-
-def get_metric(df: pd.DataFrame, metric: str) -> np.ndarray:
-    return df[df['metric'] == metric]['value'].to_numpy()
+    test_case.execute_test(tsdb_engine, port, tsdb_tmp_db_name)
