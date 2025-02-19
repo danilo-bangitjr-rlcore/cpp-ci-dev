d1353496c63f9d12525072521a6f291f79bf7bd0
Andy Patterson <andy@rlcore.ai>
2025-02-19 14:42:03 -0700

    docs: add some docstrings to the post_processor and computed cfg systems

--- corerl/configs/config.py
+++ corerl/configs/config.py
@@ -16,6 +16,17 @@ def list_(vals: list[Any] | None = None) -> Any:
 
 
 def post_processor[M](f: Callable[[Any, M], Any]):
+    """"
+    A post_processor is any arbitrary function that hooks onto the final stage of config
+    processing. There are two common use cases:
+      1. Sanity checking and early validation of config values that are beyond the scope of Pydantic's
+         built-in tools.
+      2. Mutating the config by filling in parts of the config tree based on other config values. For
+         example, enabling configs based on feature flags.
+
+    The post_processor receives the current (partially) validated config and a pointer to the top
+    of the config tree (e.g. the MainConfig).
+    """
     def _inner(self: Any, info: ValidationInfo):
         if info.context is None:
             return self
@@ -27,6 +38,32 @@ def post_processor[M](f: Callable[[Any, M], Any]):
 
 
 def computed[M](key: str):
+    """
+    A config decorator for specifying config values that can be computed (or inferred)
+    from other config values somewhere else in the config tree. This is executed after
+    default values have been assigned and config group resolution has occurred.
+
+    A computed config cannot mutate the current config instance. Instead, it must be a
+    pure function of other config values in the tree. For example:
+    ```python
+    @computed('name')
+    @classmethod
+    def _some_meaningful_name(cls, cfg: 'MainConfig'):
+        ...
+    ```
+    implies that the `name` attribute of this config can be computed by the given function
+    of other configs.
+
+    It is worth noting that the computed must be a `classmethod`. By definition, a computed
+    config will cause recursive import errors unless the top of the tree is imported with
+    a type guard.
+    ```python
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from corerl.config import MainConfig
+    ```
+    """
     def _computed_processor(f: Callable[[type[Any], M], Any]):
         def _model_validator_callback(cls: Any, data: object, info: ValidationInfo):
             if info.context is None:
