64187243adc0578a6ed66484311eb6edb41fec8c
Kerrick Johnstonbaugh <kerrick@rlcore.ai>
2025-02-20 13:22:11 -0700

    feat: add exponential backoff to heartbeat OPC write

--- corerl/messages/heartbeat.py
+++ corerl/messages/heartbeat.py
@@ -9,6 +9,7 @@ from asyncua.crypto.security_policies import SecurityPolicyBasic256Sha256
 
 from corerl.configs.config import config
 from corerl.environment.async_env.async_env import OPCEnvConfig
+from corerl.utils.asyncio import exponential_backoff
 from corerl.utils.time import clock_generator, wait_for_timestamp
 
 logger = logging.getLogger(__name__)
@@ -21,44 +22,50 @@ class HeartbeatConfig:
     max_counter: int = 1000
 
 
-async def heartbeat(cfg: HeartbeatConfig, opc_env_config: OPCEnvConfig):
+def heartbeat(cfg: HeartbeatConfig, opc_env_config: OPCEnvConfig):
     if cfg.heartbeat_node_id is None:
         return
 
-    opc_client = Client(opc_env_config.opc_conn_url)
-
-    if opc_env_config.client_cert_path and opc_env_config.client_private_key_path:
-        assert opc_env_config.application_uri is not None
-        opc_client.application_uri = opc_env_config.application_uri
-        # NOTE: this does not exist within the Sync variant of OPC Client and is the source of why we need to
-        # add these hacky async snippets into our synchronous codebase
-        await opc_client.set_security(
-            SecurityPolicyBasic256Sha256,
-            certificate=opc_env_config.client_cert_path,
-            private_key=opc_env_config.client_private_key_path,
-            mode=ua.MessageSecurityMode.SignAndEncrypt,
-            server_certificate=opc_env_config.server_cert_path,
-        )
-    async with opc_client:
-        heartbeat_node = opc_client.get_node(cfg.heartbeat_node_id)
-
+    heartbeat_node_id = cfg.heartbeat_node_id
     heartbeat_clock = clock_generator(tick_period=cfg.heartbeat_period)
     heartbeat_counter = 0
 
     while True:
-        # write counter
-        async with opc_client:
-            await opc_client.write_values([heartbeat_node], [float(heartbeat_counter)])
 
+        @exponential_backoff()
+        async def _beat():
+            # initialize client on every beat
+            opc_client = Client(opc_env_config.opc_conn_url)
+
+            if opc_env_config.client_cert_path and opc_env_config.client_private_key_path:
+                assert opc_env_config.application_uri is not None
+                opc_client.application_uri = opc_env_config.application_uri
+                # NOTE: this does not exist within the Sync variant of OPC Client and is the source of why we need to
+                # add these hacky async snippets into our synchronous codebase
+                await opc_client.set_security(
+                    SecurityPolicyBasic256Sha256,
+                    certificate=opc_env_config.client_cert_path,
+                    private_key=opc_env_config.client_private_key_path,
+                    mode=ua.MessageSecurityMode.SignAndEncrypt,
+                    server_certificate=opc_env_config.server_cert_path,
+                )
+            async with opc_client:
+                heartbeat_node = opc_client.get_node(heartbeat_node_id)
+            # write counter
+            async with opc_client:
+                await opc_client.write_values([heartbeat_node], [float(heartbeat_counter)])
+
+            # wait for next heartbeat
+            next_heartbeat_ts = next(heartbeat_clock)
+            wait_for_timestamp(next_heartbeat_ts)
+
+        asyncio.run(_beat())
         # increment counter
         heartbeat_counter += 1
         heartbeat_counter %= cfg.max_counter
         logger.debug(f"{heartbeat_counter=}")
 
-        # wait for next heartbeat
-        next_heartbeat_ts = next(heartbeat_clock)
-        wait_for_timestamp(next_heartbeat_ts)
-
+        # wrap beat in a fn decorated with exp backoff
 
 class Heartbeat:
     def __init__(self, cfg: HeartbeatConfig, opc_env_config: OPCEnvConfig) -> None:
@@ -68,8 +75,8 @@ class Heartbeat:
 
     def start_heartbeat(self, cfg: HeartbeatConfig, opc_env_config: OPCEnvConfig) -> Thread:
         heartbeat_thread = threading.Thread(
-            target=asyncio.run,
-            args=(heartbeat(cfg, opc_env_config),),
+            target=heartbeat,
+            args=(cfg, opc_env_config,),
             daemon=True,
             name="corerl_heartbeat",
         )
