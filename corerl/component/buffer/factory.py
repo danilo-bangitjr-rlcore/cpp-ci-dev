d6cfa6cfcba564b48125be9a9f35dc3ba4a5bdd1
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-02-21 10:27:45 -0700

    nuke: old buffers

--- corerl/component/buffer/factory.py
+++ corerl/component/buffer/factory.py
@@ -1,22 +1,9 @@
-from typing import Annotated
 
-from pydantic import Field
-
-from corerl.component.buffer.ensemble import (
-    EnsembleUniformReplayBufferConfig,
-    buffer_group,
-)
 from corerl.component.buffer.mixed_history import MixedHistoryBufferConfig
-from corerl.component.buffer.priority import PriorityReplayBufferConfig
-from corerl.component.buffer.uniform import UniformReplayBufferConfig
 from corerl.state import AppState
 
-BufferConfig = Annotated[
-    UniformReplayBufferConfig
-    | PriorityReplayBufferConfig
-    | EnsembleUniformReplayBufferConfig
-    | MixedHistoryBufferConfig
-, Field(discriminator='name')]
+BufferConfig = MixedHistoryBufferConfig
+from corerl.component.buffer.base import buffer_group
 
 
 def init_buffer(cfg: BufferConfig, app_state: AppState):
