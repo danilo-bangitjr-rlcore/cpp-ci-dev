d6f70182319dc49d43ccd472c9cec0358e03ae56
Andy Patterson <andy@rlcore.ai>
2025-02-24 13:29:40 -0700

    refactor: remove simple AC algorithm

--- corerl/agent/__init__.py
+++ corerl/agent/__init__.py
@@ -4,7 +4,6 @@ from pydantic import Field
 
 from corerl.agent.base import BaseAgent
 from corerl.agent.greedy_ac import GreedyAC, GreedyACConfig
-from corerl.agent.simple_ac import SimpleAC, SimpleACConfig
 from corerl.configs.group import Group
 from corerl.data_pipeline.pipeline import ColumnDescriptions
 from corerl.state import AppState
@@ -16,10 +15,8 @@ agent_group = Group[
 
 AgentConfig = Annotated[
     GreedyACConfig
-    | SimpleACConfig
 , Field(discriminator='name')]
 
 
 def register():
     agent_group.dispatcher(GreedyAC)
-    agent_group.dispatcher(SimpleAC)
