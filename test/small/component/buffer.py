fd217061a01c870a41dfd7d52a5220bcec33c23f
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-02-25 10:35:32 -0700

    test: address Andy's feedback

--- test/small/component/buffer.py
+++ test/small/component/buffer.py
@@ -26,21 +26,17 @@ def test_feed_online_mode(dummy_app_state: AppState):
         n_most_recent=2,
     )
 
-    buffer = MixedHistoryBuffer(buffer_cfg, dummy_app_state)
-    online_transitions = make_test_transitions(0, 5, 1)
-    idxs = buffer.feed(online_transitions, DataMode.ONLINE)
-    assert len(idxs) == 5
-    assert buffer._most_recent_online_idxs == deque([4, 3], maxlen=2)
+    for _ in range(100):
+        buffer = MixedHistoryBuffer(buffer_cfg, dummy_app_state)
+        online_transitions = make_test_transitions(0, 5, 1)
+        idxs = buffer.feed(online_transitions, DataMode.ONLINE)
+        assert len(idxs) == 5
 
-    # now feed some transitions with a different mode
-    offline_transitions = make_test_transitions(5, 5, 1)
-    idxs = buffer.feed(offline_transitions, DataMode.OFFLINE)
+        # now feed some transitions with a different mode
+        offline_transitions = make_test_transitions(5, 5, 1)
+        idxs = buffer.feed(offline_transitions, DataMode.OFFLINE)
 
-    # most recent online idxs should not change
-    assert buffer._most_recent_online_idxs == deque([4, 3], maxlen=2)
+        samples = buffer.sample()
 
-
-    samples = buffer.sample()
-
-    for batch in samples:
-        assert (batch.prior.state[:2, 0] == torch.Tensor([4, 3])).all()
+        for batch in samples:
+            assert (batch.prior.state[:2, 0] == torch.Tensor([4, 3])).all()
