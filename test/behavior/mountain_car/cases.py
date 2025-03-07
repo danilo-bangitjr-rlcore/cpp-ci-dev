fddbcb64eee3ffc8a31dc13f052239ad8d123ffd
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-03-07 09:02:25 -0700

    fix: change action tag for bsuite in mountain car

--- test/behavior/mountain_car/cases.py
+++ test/behavior/mountain_car/cases.py
@@ -14,5 +14,5 @@ class MountainCar(BSuiteTestCase):
     config = 'test/behavior/mountain_car/goal.yaml'
 
     # should learn to consistently bias towards slightly right-leaning actions
-    lower_bounds = { 'reward': -0.43, 'action-0': 0.38 }
+    lower_bounds = { 'reward': -0.43, 'ACTION-action-0': 0.38 }
     upper_warns = { 'avg_critic_loss': 0.112, 'actor_loss': -0.9 }
