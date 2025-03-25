1d39e4d3fcc8f4666dd2e273fe08d08ba8cc0463
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-03-25 14:07:15 -0600

    rm registry

--- coreenv/registry.py
+++
@@ -1,9 +0,0 @@
-def register_custom_envs():
-    import corerl.environment.delayed_saturation  # noqa: F401
-    import corerl.environment.distraction_world  # noqa: F401
-    import corerl.environment.multi_action_saturation  # noqa: F401
-    import corerl.environment.pvs  # noqa: F401
-    import corerl.environment.saturation  # noqa: F401
-    import corerl.environment.saturation_goals  # noqa: F401
-    import corerl.environment.three_tanks  # noqa: F401
-    import corerl.environment.windy_room  # noqa: F401
