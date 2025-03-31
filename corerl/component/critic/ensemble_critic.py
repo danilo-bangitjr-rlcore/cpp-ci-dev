2bf761c70f88e6eb0c1690a168d6be0eee1e1af5
Andy Patterson <andy@rlcore.ai>
2025-03-30 21:36:18 -0600

    refactor(PROD-137): extract critic logic into critic module

--- corerl/component/critic/ensemble_critic.py
+++ corerl/component/critic/ensemble_critic.py
@@ -12,7 +12,10 @@ from corerl.component.network.networks import EnsembleNetwork, EnsembleNetworkCo
 from corerl.component.optimizers.factory import OptimizerConfig, init_optimizer
 from corerl.component.optimizers.torch_opts import LSOConfig
 from corerl.configs.config import config
+from corerl.data_pipeline.datatypes import TransitionBatch
+from corerl.messages.events import EventType
 from corerl.state import AppState
+from corerl.utils.device import device
 
 logger = logging.getLogger(__name__)
 
@@ -38,7 +41,13 @@ class BaseCritic(ABC):
         self.app_state = app_state
 
     @abstractmethod
-    def update(self, loss: torch.Tensor, closure: Callable[[], float]) -> None:
+    def update(
+        self,
+        batches: list[TransitionBatch],
+        next_actions: list[torch.Tensor],
+        eval_batches: list[TransitionBatch],
+        eval_actions: list[torch.Tensor],
+    ) -> float:
         raise NotImplementedError
 
     @abstractmethod
@@ -87,12 +96,10 @@ class EnsembleCritic(BaseCritic):
         self.target_sync_counter = 0
         self.optimizer_name = cfg.critic_optimizer.name
 
-
-    def _update_target(self) -> None:
-        self.target_sync_counter += 1
-        self.polyak_avg_target()
-
-    def update(
+    # -------------
+    # -- Updates --
+    # -------------
+    def _update(
         self,
         loss: torch.Tensor,
         closure: Callable[[], float],
@@ -109,7 +116,28 @@ class EnsembleCritic(BaseCritic):
             self.optimizer.step(closure=lambda: 0.)
         else:
             self.optimizer.step(closure=closure)
-        self._update_target()
+
+        self.target_sync_counter += 1
+        self.polyak_avg_target()
+
+
+    def update(
+        self,
+        batches: list[TransitionBatch],
+        next_actions: list[torch.Tensor],
+        eval_batches: list[TransitionBatch],
+        eval_actions: list[torch.Tensor],
+    ):
+        self._app_state.event_bus.emit_event(EventType.agent_update_critic)
+        q_loss = self.compute_loss(
+            batches,
+            next_actions,
+            log_metrics=True,
+        )
+
+        self._update(q_loss, closure=lambda: self.compute_loss(eval_batches, eval_actions).item())
+        return q_loss.item()
+
 
     def polyak_avg_target(self) -> None:
         if self.polyak == 0:
@@ -124,6 +152,99 @@ class EnsembleCritic(BaseCritic):
                 p_targ.data.mul_(self.polyak)
                 p_targ.data.add_((1 - self.polyak) * p.data)
 
+    # ------------
+    # -- Losses --
+    # ------------
+    def compute_loss(
+        self,
+        ensemble_batch: list[TransitionBatch],
+        next_actions: list[torch.Tensor],
+        log_metrics: bool=False,
+    ) -> torch.Tensor:
+        # First, translate ensemble batches in to list for each property
+        ensemble_len = len(ensemble_batch)
+        state_batches = []
+        action_batches = []
+        reward_batches = []
+        next_state_batches = []
+        next_action_batches = []
+        gamma_batches = []
+        for i, batch in enumerate(ensemble_batch):
+            state_batch = batch.prior.state
+            direct_action_batch = batch.post.action
+            reward_batch = batch.n_step_reward
+            next_state_batch = batch.post.state
+            gamma_batch = batch.n_step_gamma
+            dp_mask = batch.post.dp
+
+            # For the 'Anytime' paradigm, only states at decision points can sample next_actions
+            # If a state isn't at a decision point, its next_action is set to the current action
+            with torch.no_grad():
+                next_action_batches.append((dp_mask * next_actions[i]) + ((1.0 - dp_mask) * direct_action_batch))
+
+            state_batches.append(state_batch)
+            action_batches.append(direct_action_batch)
+            reward_batches.append(reward_batch)
+            next_state_batches.append(next_state_batch)
+            gamma_batches.append(gamma_batch)
+
+        loss = self._sarsa_loss(
+            state_batches,
+            action_batches,
+            next_state_batches,
+            next_action_batches,
+            reward_batches,
+            gamma_batches,
+        )
+
+        if log_metrics:
+            self._app_state.metrics.write(
+                agent_step=self._app_state.agent_step,
+                metric="avg_critic_loss",
+                value=loss.item() / ensemble_len,
+            )
+
+        return loss
+
+    def _sarsa_loss(
+        self,
+        states: list[torch.Tensor],
+        actions: list[torch.Tensor],
+        next_states: list[torch.Tensor],
+        next_actions: list[torch.Tensor],
+        rewards: list[torch.Tensor],
+        gammas: list[torch.Tensor],
+    ):
+        values = self.get_values(states, actions, with_grad=True)
+        target_values = self.get_target_values(next_states, next_actions)
+
+        loss = torch.tensor(0.0, device=device.device)
+        for i in range(len(rewards)):
+            q = values.ensemble_values[i]
+            qp = target_values.ensemble_values[i]
+            target = rewards[i] + gammas[i] * qp
+            loss_i = torch.nn.functional.mse_loss(target, q)
+            loss += loss_i
+
+            self._app_state.metrics.write(
+                agent_step=self._app_state.agent_step,
+                metric=f"critic_loss_{i}",
+                value=loss_i.item(),
+            )
+
+        if values.ensemble_variance is not None:
+            mean_variance = torch.mean(values.ensemble_variance)
+            self._app_state.metrics.write(
+                agent_step=self._app_state.agent_step,
+                metric="critic_ensemble_variance",
+                value=mean_variance.item(),
+            )
+
+        return loss
+
+    # ---------------
+    # -- Lifecycle --
+    # ---------------
     def save(self, path: Path) -> None:
         self.model.save(path / "critic_net")
         self.target.save(path / "critic_target")
