9105968bb7a9ed4bb5a7e0316d5891d0b7882aee
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-04-03 10:25:10 -0600

    fixes to GAC

--- src/agent/gac.py
+++ src/agent/gac.py
@@ -192,11 +192,11 @@ class GreedyAC:
 
     def critic_loss(
             self,
-            rng: chex.PRNGKey,
             critic_params: chex.ArrayTree,
             target_params: chex.ArrayTree,
             actor_params: chex.ArrayTree,
             transition: VectorizedTransition,
+            rng: chex.PRNGKey,
     ):
         state = transition.state
         action = transition.action
@@ -204,7 +204,7 @@ class GreedyAC:
         next_state = transition.next_state
         gamma = transition.gamma
 
-        next_action = self.get_actions(actor_params, rng, next_state)
+        next_action = self._get_actions(actor_params, rng, next_state)
         target_value = self.critic.apply(params=target_params, x=next_state, a=next_action)
         target = reward + gamma * target_value.q
         value = self.critic.apply(params=critic_params, x=state, a=action)
@@ -214,41 +214,41 @@ class GreedyAC:
 
     def _batch_critic_loss(
             self,
-            rng: chex.PRNGKey,
             critic_params: chex.ArrayTree,
             target_params: chex.ArrayTree,
             actor_params: chex.ArrayTree,
             transitions: VectorizedTransition,
+            rng: chex.PRNGKey,
     ):
         rngs = jax.random.split(rng, self.critic_buffer.batch_size)
-        vmapped = jax.vmap(self.critic_loss, in_axes=(0, None, None, None, 0))
+        vmapped = jax.vmap(self.critic_loss, in_axes=(None, None, None, 0, 0))
         losses = vmapped(
-            rngs,
             critic_params,
             target_params,
             actor_params,
             transitions,
+            rngs,
         )
 
         return jnp.mean(losses)
 
     def _member_critic_update(
             self,
-            rng: chex.PRNGKey,
             critic_state: CriticState,
             actor_state: PolicyState,
             transitions: VectorizedTransition,
+            rng: chex.PRNGKey,
     ) -> CriticState:
         """
         Updates a single member of the ensemble.
         """
         grad_fn = jax.grad(self._batch_critic_loss)
         grads = grad_fn(
-            rng,
             critic_state.params,
             critic_state.target_params,
             actor_state.params,
             transitions,
+            rng,
         )
         updates, new_opt_state = self.critic_opt.update(grads, critic_state.opt_state)
         new_params = optax.apply_updates(critic_state.params, updates)
@@ -267,21 +267,21 @@ class GreedyAC:
     @partial(jax.jit, static_argnums=(0,))
     def _ensemble_critic_update(
         self,
-        rng: chex.PRNGKey,
         critic_state: CriticState,
         actor_state: PolicyState,
         transitions: VectorizedTransition,
+        rng: chex.PRNGKey,
     ) -> CriticState:
         """
         Updates each member of the ensemble.
         """
         rngs = jax.random.split(rng, self.ensemble)
-        vmapped = jax.vmap(self._member_critic_update, in_axes=(0, 0, None, 0))
+        vmapped = jax.vmap(self._member_critic_update, in_axes=(0, None, 0, 0))
         new_critic_state =  vmapped(
-            rngs,
             critic_state,
             actor_state,
             transitions,
+            rngs,
         )
         return new_critic_state
 
@@ -290,14 +290,14 @@ class GreedyAC:
             return
         transitions = self.critic_buffer.sample()
 
-        self.rng, update_key = jax.random.split(self.rng, 2)
+        self.rng, update_rng = jax.random.split(self.rng, 2)
         new_critic_state = self._ensemble_critic_update(
-            update_key,
             self.agent_state.critic,
             self.agent_state.actor,
             transitions,
+            update_rng,
         )
-        self.agent_state = self.agent_state._replace(critic_state=new_critic_state)
+        self.agent_state = self.agent_state._replace(critic=new_critic_state)
 
 
     def get_action_samples(self, state: jax.Array):
@@ -399,4 +399,4 @@ class GreedyAC:
 
     def update(self):
         self.critic_update()
-        self.policy_update()
+        # self.policy_update()
