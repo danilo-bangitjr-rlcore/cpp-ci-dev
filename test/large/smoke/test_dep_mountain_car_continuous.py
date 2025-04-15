389d876a43ccf2b8adea9acbf034080cd1aa624c
Andy Patterson <andy@rlcore.ai>
2025-04-15 10:05:21 -0600

    test: temporarily disable e2e mountain continuous test

--- test/large/smoke/test_dep_mountain_car_continuous.py
+++ test/large/smoke/test_dep_mountain_car_continuous.py
@@ -154,9 +154,10 @@ def check_sim_farama_environment_ready(run_docker_compose: None, request: Fixtur
                 data_reader.close()
 
 
-@pytest.mark.skipif(
-    should_skip_test(), reason="Docker compose ps saw core-rl services, or failed to run, do not run opc tsdb test"
-)
+# @pytest.mark.skipif(
+#     should_skip_test(), reason="Docker compose ps saw core-rl services, or failed to run, do not run opc tsdb test"
+# )
+@pytest.mark.skip
 @pytest.mark.timeout(500)
 def test_dep_mountain_car_continuous(
     check_sim_farama_environment_ready: None,
