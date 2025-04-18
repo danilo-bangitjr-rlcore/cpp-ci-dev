72871d7ca9319598ead26096f38b8ba100b99d14
Andy Patterson <andy@rlcore.ai>
2025-04-17 19:18:10 -0600

    refactor: pull scheduler timing logic into Clock utility

--- corerl/messages/scheduler.py
+++ corerl/messages/scheduler.py
@@ -2,7 +2,6 @@ import logging
 import threading
 import time
 from datetime import UTC, datetime, timedelta
-from typing import NamedTuple
 
 import zmq
 
@@ -11,9 +10,6 @@ from corerl.messages.events import Event, EventTopic, EventType
 
 logger = logging.getLogger(__name__)
 
-class EventPeriodTime(NamedTuple):
-    period: timedelta
-    next_event_ts: datetime
 
 def scheduler_task(pub_socket: zmq.Socket, cfg: AsyncEnvConfig, stop_event: threading.Event):
     """
@@ -21,46 +17,68 @@ def scheduler_task(pub_socket: zmq.Socket, cfg: AsyncEnvConfig, stop_event: thre
     Responsible for emitting the step events based on configured observation windows.
     """
 
-    topic = EventTopic.corerl_scheduler
+    clocks = [
+        Clock(EventType.step_get_obs, cfg.obs_period),
+        Clock(EventType.step_agent_update, cfg.update_period),
+        Clock(EventType.step_emit_action, cfg.action_period, offset=timedelta(seconds=1)),
+        Clock(EventType.agent_step, cfg.obs_period),
+    ]
 
-    thread_start_time = datetime.now(UTC)
-    next_event_dict = {
-        EventType.step_get_obs: EventPeriodTime(cfg.obs_period, thread_start_time),
-        EventType.step_agent_update: EventPeriodTime(cfg.update_period, thread_start_time),
-        EventType.step_emit_action: EventPeriodTime(cfg.action_period, thread_start_time),
-        EventType.agent_step: EventPeriodTime(cfg.obs_period, thread_start_time), # this should happen after emit action
-    }
     if cfg.setpoint_ping_period is not None:
-        next_event_dict[EventType.ping_setpoints] = EventPeriodTime(
-            cfg.setpoint_ping_period, thread_start_time + cfg.setpoint_ping_period
-        )
-
-
-    def _write_to_zmq(event: Event):
-        logger.debug(f"Scheduling Event: {event}")
-        message_data = event.model_dump_json()
-        payload = f"{topic} {message_data}"
-        pub_socket.send_string(payload)
+        clocks += [
+            Clock(EventType.ping_setpoints, cfg.setpoint_ping_period, offset=cfg.setpoint_ping_period),
+        ]
 
     while not stop_event.is_set():
         try:
-            thread_cur_time = datetime.now(UTC)
+            now = datetime.now(UTC)
+            for clock in clocks:
+                clock.maybe_emit(pub_socket, now)
 
-            for event_type, ept in next_event_dict.items():
-                if thread_cur_time >= ept.next_event_ts:
-                    _write_to_zmq(Event(type=event_type))
-                    next_event_dict[event_type] = EventPeriodTime(
-                        period = ept.period,
-                        next_event_ts= ept.next_event_ts + ept.period
-                    )
+            shortest_duration = min(clock.get_next_ts() for clock in clocks) - datetime.now(UTC)
+            shortest_duration = max(shortest_duration.total_seconds(), 0)
+            time.sleep(shortest_duration)
 
-            time.sleep(0.5)
+        except zmq.ZMQError as e:
+            if e.errno == zmq.ETERM:
+                # exit, break from loop
+                break
+            raise
+
+
+class Clock:
+    def __init__(self, event_type: EventType, period: timedelta, offset: timedelta = timedelta(seconds=0)):
+        self._event_type = event_type
+        self._period = period
+
+        self._next_ts = datetime.now(UTC) + offset
+
+    def emit(self, socket: zmq.Socket, now: datetime):
+        event = Event(type=self._event_type)
+        logger.debug(f"Scheduling Event: {event}")
+        message_data = event.model_dump_json()
+        payload = f"{EventTopic.corerl_scheduler} {message_data}"
+
+        try:
+            socket.send_string(payload)
         except zmq.ZMQError as e:
             if isinstance(e, zmq.error.Again):
                 # temporarily unavailable, retry
-                pass
-            elif e.errno == zmq.ETERM:
-                # exit, break from loop
-                break
+                return
             else:
                 raise
+
+        self.reset(now)
+
+    def should_emit(self, now: datetime):
+        return now > self._next_ts
+
+    def maybe_emit(self, socket: zmq.Socket, now: datetime):
+        if self.should_emit(now):
+            self.emit(socket, now)
+
+    def get_next_ts(self):
+        return self._next_ts
+
+    def reset(self, now: datetime):
+        self._next_ts = now + self._period
