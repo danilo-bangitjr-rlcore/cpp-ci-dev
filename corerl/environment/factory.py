bcb9097b2c7a1505c00468b436e741ecc3711a79
Alexander Wong <alex@rlcore.ai>
2025-05-01 15:52:21 -0600

    fix(PROD-296): set `coreenv` as optional dependency, remove from docker image

--- corerl/environment/factory.py
+++ corerl/environment/factory.py
@@ -1,23 +1,36 @@
 import logging
+from typing import Any
 
 import gymnasium as gym
-from coreenv.factory import init_env
 
 from corerl.environment.async_env.async_env import GymEnvConfig
 
+try:
+    from coreenv.factory import init_env
+except ImportError:
+
+    def init_env(name: str, overrides: Any = None) -> Any:
+        """
+        Placeholder function for initializing a custom environment.
+        This should be replaced with the actual implementation.
+        """
+        raise NotImplementedError("Custom environment initialization not implemented.")
+
+
 log = logging.getLogger(__name__)
+
 
 def init_environment(cfg: GymEnvConfig) -> gym.Env:
     args = cfg.args
     kwargs = cfg.kwargs
 
     match cfg.init_type:
-        case 'gym.make':
+        case "gym.make":
             if cfg.env_config is not None:
                 kwargs = dict(kwargs)
-                kwargs['cfg'] = cfg.env_config
+                kwargs["cfg"] = cfg.env_config
             return gym.make(cfg.gym_name, *args, **kwargs)
-        case 'custom':
+        case "custom":
             return init_env(cfg.gym_name, overrides=cfg.env_config)
         case _:
             raise NotImplementedError
