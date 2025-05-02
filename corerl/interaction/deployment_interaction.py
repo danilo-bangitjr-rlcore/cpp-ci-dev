7fadccab9f71693dae47a21b2f217a08e48d6133
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-05-02 16:32:56 -0600

    refactor: prune checkpoints

--- corerl/interaction/deployment_interaction.py
+++ corerl/interaction/deployment_interaction.py
@@ -5,6 +5,7 @@ import shutil
 import threading
 from collections.abc import Callable
 from datetime import UTC, datetime, timedelta
+from pathlib import Path
 from typing import Any, Generator
 
 import numpy as np
@@ -428,34 +429,10 @@ class DeploymentInteraction:
 
         # keep all checkpoints more recent than the cliff
         cliff = now - self._checkpoint_cliff
-
-        # iterate through the checkpoints to determine which to delete
-        to_delete = []
-        for i, chk in enumerate(chkpoints):
-            # keep latest and first checkpoint
-            if i in (0, len(chkpoints) - 1):
-                continue
-
-            # keep all checkpoints more recent than the cliff
-            if times[i] > cliff:
-                continue
-
-            periods_since_cliff_chk = periods_since(times[i], cliff, self._checkpoint_freq)
-            periods_since_clif_prev_chk = periods_since(times[i-1], cliff, self._checkpoint_freq)
-            periods_since_cliff_next_chk = periods_since(times[i+1], cliff, self._checkpoint_freq)
-
-            # having checkpoints at powers of two is our goal. Get the next and previous powers of two in periods
-            next_power_2 = next_power_of_2(periods_since_cliff_chk)
-            prev_power_2 = prev_power_of_2(periods_since_cliff_chk)
-
-            # we will delete a checkoint if there is an older checkpoint closer to the next power of two
-            # and there is a younger checkpoint closer to the previous power of two
-            if periods_since_clif_prev_chk <= next_power_2 and periods_since_cliff_next_chk >= prev_power_2:
-                to_delete.append(chk)
+        to_delete = prune_checkpoints(chkpoints, times, cliff, self._checkpoint_freq)
 
         for chk in to_delete:
             shutil.rmtree(chk)
-
 
     def restore_checkpoint(self):
         if not self._cfg.restore_checkpoint:
@@ -483,3 +460,34 @@ def prev_power_of_2(x: int):
 
 def periods_since(start: datetime, end: datetime, period: timedelta):
     return math.floor((end - start) / period)
+
+def prune_checkpoints(
+        chkpoints: list[Path],
+        times: list[datetime],
+        cliff: datetime,
+        checkpoint_freq: timedelta
+    ) -> list[Path]:
+
+    to_delete = []
+    for i, chk in enumerate(chkpoints):
+        # keep latest and first checkpoint
+        if i in (0, len(chkpoints) - 1):
+            continue
+
+        # keep all checkpoints more recent than the cliff
+        if times[i] > cliff:
+            continue
+
+        periods_since_cliff_chk = periods_since(times[i], cliff, checkpoint_freq)
+        periods_since_clif_prev_chk = periods_since(times[i-1], cliff, checkpoint_freq)
+        periods_since_cliff_next_chk = periods_since(times[i+1], cliff, checkpoint_freq)
+
+        # having checkpoints at powers of two is our goal. Get the next and previous powers of two in periods
+        next_power_2 = next_power_of_2(periods_since_cliff_chk)
+        prev_power_2 = prev_power_of_2(periods_since_cliff_chk)
+
+        # we will delete a checkoint if there is an older checkpoint closer to the next power of two
+        # and there is a younger checkpoint closer to the previous power of two
+        if periods_since_clif_prev_chk <= next_power_2 and periods_since_cliff_next_chk >= prev_power_2:
+            to_delete.append(chk)
+    return to_delete
