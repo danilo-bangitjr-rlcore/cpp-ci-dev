62a6bd61a64b63e6fae849227f844a3f55fd216f
Andy Patterson <andy@rlcore.ai>
2025-05-02 21:20:53 -0600

    feat: add simple exponential moving average utility

--- corerl/utils/math.py
+++ corerl/utils/math.py
@@ -1,10 +1,13 @@
+from typing import Any
+
 import numba
 import numpy as np
 
+ArrayOrFloat = np.ndarray | float | np.floating
 
 @numba.njit
 def put_in_range(
-    x: np.ndarray | float,
+    x: ArrayOrFloat,
     old_range: tuple[float, float] | np.ndarray,
     new_range: tuple[float, float] | np.ndarray,
 ):
@@ -15,3 +18,22 @@ def put_in_range(
     old_d = (old_range[1] - old_range[0])
     new_d = (new_range[1] - new_range[0])
     return (((x - old_range[0]) * new_d) / old_d) + new_range[0]
+
+
+@numba.njit
+def exp_moving_avg[T: ArrayOrFloat](
+    decay: float,
+    mean: T | None,
+    x: ArrayOrFloat,
+) -> T:
+    """
+    Computes an exponential moving average:
+        mean = decay * mean + (1 - decay) * x
+    """
+    if mean is None:
+        # using type erasure to guarantee return type
+        out: Any = x
+        return out
+
+    out: Any = decay * mean + (1 - decay) * x
+    return out
