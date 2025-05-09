66128eb3df2ca023a718ab29fe3eef66a7366c15
Kerrick Johnstonbaugh <kerrick@rlcore.ai>
2025-05-09 15:33:43 -0600

    refactor: njit stuck detector for loop

--- corerl/data_pipeline/oddity_filters/stuck_detector.py
+++ corerl/data_pipeline/oddity_filters/stuck_detector.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from typing import Literal
 
 import numpy as np
+from numba import njit
 
 from corerl.configs.config import config
 from corerl.data_pipeline.datatypes import MissingType, PipelineFrame
@@ -38,32 +39,39 @@ class StuckDetector(BaseOddityFilter):
         self.step_tol = cfg.step_tol
 
     def __call__(self, pf: PipelineFrame, tag: str, ts: object | None, update_stats: bool = True):
-
         tag_ts = ts if ts is not None else StuckDetectorTemporalState()
         assert isinstance(tag_ts, StuckDetectorTemporalState)
         tag_data = pf.data[tag].to_numpy()
 
-        oddity_mask = np.full(tag_data.shape, False)
-        i = 0
-        while i < len(tag_data):
-            x_i = tag_data[i]
-            if not np.isnan(x_i):
-                val_changed = np.isnan(tag_ts.prev_val) or abs(tag_ts.prev_val - x_i) > self.eps
-                tag_ts.prev_val = x_i
-                if val_changed:
-                    tag_ts.stuck_steps = 0
-                else:
-                    tag_ts.stuck_steps += 1
-
-            if tag_ts.stuck_steps >= self.step_tol:
-                oddity_mask[i] = True
-            i += 1
+        oddity_mask, prev_val, stuck_steps = _get_stuck_mask(
+            tag_data, tag_ts.prev_val, tag_ts.stuck_steps, self.eps, self.step_tol
+        )
+        tag_ts.prev_val = prev_val
+        tag_ts.stuck_steps = stuck_steps
 
         # update missing info and set nans
         update_missing_info(pf.missing_info, name=tag, missing_mask=oddity_mask, new_val=MissingType.OUTLIER)
         tag_data[oddity_mask] = np.nan
 
         return pf, tag_ts
+
+
+@njit
+def _get_stuck_mask(tag_data: np.ndarray, prev_val: float, stuck_steps: int, eps: float, step_tol: int):
+    oddity_mask = np.full(tag_data.shape, False)
+    for i, x_i in enumerate(tag_data):
+        if not np.isnan(x_i):
+            val_changed = np.isnan(prev_val) or abs(prev_val - x_i) > eps
+            prev_val = x_i
+            if val_changed:
+                stuck_steps = 0
+            else:
+                stuck_steps += 1
+
+        if stuck_steps >= step_tol:
+            oddity_mask[i] = True
+
+    return oddity_mask, prev_val, stuck_steps
 
 
 outlier_group.dispatcher(StuckDetector)
