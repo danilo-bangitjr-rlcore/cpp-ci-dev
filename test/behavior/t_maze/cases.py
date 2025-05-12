92941c3e262e988cc35e1a98fc9c43b97e86e6d4
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-05-12 11:11:21 -0600

    fix(PROD-419): stupid mistakes on t maze bsuites

--- test/behavior/t_maze/cases.py
+++ test/behavior/t_maze/cases.py
@@ -4,14 +4,14 @@ from test.behavior.bsuite import BSuiteTestCase
 class ShortTMazeTest(BSuiteTestCase):
     name = 'short_t_maze'
     config = 'test/behavior/t_maze/short.yaml'
-    lower_bounds = { 'reward': -0.1}
+    lower_bounds = {'reward': -0.1}
 
 class MediumTMazeTest(BSuiteTestCase):
-    name = 'short_t_maze'
+    name = 'medium_t_maze'
     config = 'test/behavior/t_maze/medium.yaml'
-    lower_bounds = { 'reward': -0.1}
+    lower_bounds = {'reward': -0.1}
 
 class LongTMazeTest(BSuiteTestCase):
-    name = 'short_t_maze'
+    name = 'long_t_maze'
     config = 'test/behavior/t_maze/long.yaml'
-    lower_bounds = { 'reward': -0.1}
+    lower_bounds = {'reward': -0.1}
