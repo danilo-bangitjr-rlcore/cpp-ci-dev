0af75573c44ce3d49ef378d4f3a26d4bcaa2f337
Kerrick Johnstonbaugh <kerrick@rlcore.ai>
2025-05-21 10:14:13 -0600

    test (PROD-420): add btest to ensure actor variance becomes sufficiently small

--- test/behavior/mountain_car/cases.py
+++ test/behavior/mountain_car/cases.py
@@ -16,3 +16,10 @@ class MountainCar(BSuiteTestCase):
     # should learn to consistently bias towards slightly right-leaning actions
     lower_bounds = { 'reward': -0.43, 'ACTION-action-0': 0.38 }
     upper_warns = { 'avg_critic_loss': 0.112, 'actor_loss': -0.9 }
+
+class LowVarianceActions(BSuiteTestCase):
+    name = 'low variance actions'
+    config = 'test/behavior/mountain_car/stand_still.yaml'
+
+    upper_bounds = { 'actor_var': 0.01 }
+    upper_warns = { 'avg_critic_loss': 0.003, 'actor_loss': -0.57 }
