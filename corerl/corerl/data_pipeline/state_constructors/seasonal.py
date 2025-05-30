dd51b063a74d02abb164519b9bcc1ae7f8d87b70
Alex Trudeau <alexandre.trudeau@rlcore.ai>
2025-05-30 16:30:24 -0600

    chore: modify state constructor and seasonal state feature computer to be compatible with seasonal TagTypes

--- corerl/corerl/data_pipeline/state_constructors/seasonal.py
+++ corerl/corerl/data_pipeline/state_constructors/seasonal.py
@@ -1,15 +1,36 @@
 import numpy as np
 import pandas as pd
 
-from corerl.configs.config import config
+from typing import TYPE_CHECKING
+
+from corerl.configs.config import MISSING, computed, config
 from corerl.data_pipeline.datatypes import PipelineFrame
+from corerl.data_pipeline.tag_config import TagType
+
+if TYPE_CHECKING:
+    from corerl.config import MainConfig
 
 
 @config()
 class SeasonalConfig:
-    time_of_day_enabled: bool = False
-    day_of_week_enabled: bool = False
-    time_of_year_enabled: bool = False
+    time_of_day_enabled: bool = MISSING
+    day_of_week_enabled: bool = MISSING
+    time_of_year_enabled: bool = MISSING
+
+    @computed('time_of_day_enabled')
+    @classmethod
+    def _time_of_day_enabled(cls, cfg: 'MainConfig'):
+        return any(tag_cfg.type == TagType.time_of_day for tag_cfg in cfg.pipeline.tags)
+
+    @computed('day_of_week_enabled')
+    @classmethod
+    def _day_of_week_enabled(cls, cfg: 'MainConfig'):
+        return any(tag_cfg.type == TagType.day_of_week for tag_cfg in cfg.pipeline.tags)
+
+    @computed('time_of_year_enabled')
+    @classmethod
+    def _time_of_year_enabled(cls, cfg: 'MainConfig'):
+        return any(tag_cfg.type == TagType.day_of_year for tag_cfg in cfg.pipeline.tags)
 
 def add_seasonal_features(cfg: SeasonalConfig, pf: PipelineFrame) -> PipelineFrame:
     if not (cfg.time_of_day_enabled or cfg.day_of_week_enabled or cfg.time_of_year_enabled):
@@ -20,23 +41,26 @@ def add_seasonal_features(cfg: SeasonalConfig, pf: PipelineFrame) -> PipelineFra
     num_entries = len(timestamps)
 
     if cfg.time_of_day_enabled:
-        day_seconds = (timestamps.hour * 3600) + (timestamps.minute * 60) + timestamps.second
-        pf.data["time_of_day_sin"] = np.sin(2.0 * np.pi * day_seconds / 86400.0)
-        pf.data["time_of_day_cos"] = np.cos(2.0 * np.pi * day_seconds / 86400.0)
+        pf.data["time_of_day_sin"] = np.sin(2.0 * np.pi * pf.data["time_of_day"] / 86400.0)
+        pf.data["time_of_day_cos"] = np.cos(2.0 * np.pi * pf.data["time_of_day"] / 86400.0)
+
+        pf.data = pf.data.drop("time_of_day", axis=1)
 
     if cfg.day_of_week_enabled:
-        day_of_week = timestamps.weekday
         weekday_cols = np.zeros((num_entries, 7))
-        weekday_cols[range(num_entries), day_of_week] = 1.0
+        weekday_cols[range(num_entries), pf.data["day_of_week"]] = 1.0
         col_names = [f"day_of_week_{i}" for i in range(7)]
         pf.data[col_names] = weekday_cols
 
+        pf.data = pf.data.drop("day_of_week", axis=1)
+
     if cfg.time_of_year_enabled:
-        year_days = timestamps.dayofyear.to_numpy()
         days_in_year = 366.0 * np.ones(num_entries)
         not_leap_year = ~ timestamps.is_leap_year
         days_in_year -= not_leap_year
-        pf.data["time_of_year_sin"] = np.sin(2.0 * np.pi * year_days / days_in_year)
-        pf.data["time_of_year_cos"] = np.cos(2.0 * np.pi * year_days / days_in_year)
+        pf.data["time_of_year_sin"] = np.sin(2.0 * np.pi * pf.data["day_of_year"] / days_in_year)
+        pf.data["time_of_year_cos"] = np.cos(2.0 * np.pi * pf.data["day_of_year"] / days_in_year)
+
+        pf.data = pf.data.drop("day_of_year", axis=1)
 
     return pf
