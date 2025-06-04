e63fb00afbd893ef8f874d5dc169c144489fec52
Andy Patterson <andy@rlcore.ai>
2025-06-04 12:41:22 -0600

    feat(PROD-492): implement jax storage backend for replay

--- libs/lib_agent/lib_agent/buffer/storage.py
+++ libs/lib_agent/lib_agent/buffer/storage.py
@@ -1,35 +1,106 @@
-from collections.abc import Iterable
+from typing import Any, NamedTuple
+
+import jax
+import jax.numpy as jnp
+import lib_utils.jax as jax_u
 
 
-class ReplayStorage[T]:
+class BufferState(NamedTuple):
+    pos: int
+    size: jax.Array
+    capacity: int
+    data: tuple[jax.Array, ...]
+
+
+class ReplayStorage[T: NamedTuple]:
     def __init__(self, capacity: int):
-        self.size = 0
-
         self._capacity = capacity
-        self._pos = 0
-        self._buffer: list[T | None] = [None] * capacity
+
+        self._state: BufferState | None = None
+        # type checker won't be able to infer this type correctly
+        self._tuple_builder: Any | None = None
 
 
     def add(self, item: T):
-        idx = self._pos
-        self._buffer[idx] = item
-        self._pos = (self._pos + 1) % self._capacity
+        if self._state is None:
+            self._state = self._init(item)
+            self._tuple_builder = type(item)
 
-        self.size = min(self.size + 1, self._capacity)
-
+        idx, self._state = self._add(self._state, item)
         return idx
 
+    @jax_u.method_jit
+    def _add(self, state: BufferState, item: T):
+        idx = state.pos
 
-    def get_batch(self, idxs: Iterable[int]) -> list[T]:
-        out: list[T] = []
-        for idx in idxs:
-            item = self._buffer[idx]
-            assert item is not None
-            out.append(item)
+        new_data = tuple(
+            buffer.at[idx].set(element)
+            for buffer, element in zip(state.data, item, strict=True)
+        )
 
-        return out
+        pos = (state.pos + 1) % state.capacity
+
+        return idx, BufferState(
+            pos=pos,
+            size=jnp.minimum(state.size + 1, state.capacity),
+            capacity=state.capacity,
+            data=new_data,
+        )
+
+
+    def _init(self, item: T):
+        elements = tuple(
+            element if isinstance(element, jax.Array) else jnp.array(element)
+            for element in item
+        )
+
+        buffers = tuple(
+            jnp.empty((self._capacity, *element.shape), element.dtype)
+            for element in elements
+        )
+
+        return BufferState(
+            pos=0,
+            size=jnp.zeros(1, dtype=jnp.int32),
+            capacity=self._capacity,
+            data=buffers,
+        )
+
+    def get_ensemble_batch(self, idxs: list[jax.Array]) -> T:
+        assert self._state is not None
+        assert self._tuple_builder is not None
+        raw_buffers = self._get_ensemble_batch(self._state, idxs)
+        return self._tuple_builder(*raw_buffers)
+
+
+    @jax_u.method_jit
+    def _get_ensemble_batch(self, state: BufferState, idxs: list[jax.Array]):
+        return tuple(
+            jnp.stack([buffer[sub_idxs] for sub_idxs in idxs], axis=0)
+            for buffer in state.data
+        )
+
+
+    def get_batch(self, idxs: jax.Array) -> T:
+        assert self._state is not None
+        assert self._tuple_builder is not None
+        raw_buffers = self._get_batch(self._state, idxs)
+        return self._tuple_builder(*raw_buffers)
+
+
+    @jax_u.method_jit
+    def _get_batch(self, state: BufferState, idxs: jax.Array):
+        return tuple(buffer[idxs] for buffer in state.data)
 
 
     def last_idx(self):
-        assert self.size > 0
-        return (self._pos - 1) % self._capacity
+        assert self._state is not None
+        assert self._state.size.item() > 0
+        return (self._state.pos - 1) % self._capacity
+
+
+    def size(self):
+        if self._state is None:
+            return 0
+
+        return int(self._state.size.item())
