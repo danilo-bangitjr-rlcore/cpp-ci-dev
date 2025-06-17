d73033a873d552bccd1419aaf7befd4a34cd91f4
Andy Patterson <andy@rlcore.ai>
2025-06-17 13:33:22 -0600

    fix(corerl): ensure datapipeline resets temporal state if overlapping timestamps occur

--- corerl/tests/small/data_pipeline/test_pipeline.py
+++ corerl/tests/small/data_pipeline/test_pipeline.py
@@ -4,8 +4,11 @@ from typing import Any
 
 import numpy as np
 import pandas as pd
+from lib_config.errors import ConfigValidationErrors
+from lib_config.loader import direct_load_config
 from test.infrastructure.utils.pandas import dfs_close
 
+from corerl.config import MainConfig
 from corerl.data_pipeline.all_the_time import AllTheTimeTCConfig
 from corerl.data_pipeline.constructors.sc import SCConfig
 from corerl.data_pipeline.datatypes import DataMode, StageCode
@@ -220,3 +223,61 @@ def test_sub_pipeline1(dummy_app_state: AppState):
 
     assert dfs_close(got.df, expected_df)
     assert got.transitions is None
+
+
+def test_pipeline_overlapping_time(dummy_app_state: AppState):
+    cfg = direct_load_config(MainConfig, config_name='tests/small/data_pipeline/end_to_end/test_pipeline1.yaml')
+    assert not isinstance(cfg, ConfigValidationErrors)
+    pipeline = Pipeline(dummy_app_state, cfg.pipeline)
+
+    start = datetime.datetime.now(datetime.UTC)
+    Δ = datetime.timedelta(minutes=5)
+
+    dates = [start + i * Δ for i in range(7)]
+    idx = pd.DatetimeIndex(dates)
+
+    cols: Any = ['tag-1', 'tag-2', 'reward', 'action-1']
+    df = pd.DataFrame(
+        data=[
+            [np.nan, 0,        0,    0],
+            [0,      2,        3,    1],
+            [1,      4,        0,    0],
+            [2,      6,        0,    1],
+            [np.nan, np.nan,   0,    0],
+            [4,      10,       1,    1],
+            [5,      12,       0,    0],
+        ],
+        columns=cols,
+        index=idx,
+    )
+
+    pipeline(df)
+
+    # NOTE: overlap time by two time steps
+    dates = [start + (i - 2) * Δ for i in range(7)]
+    idx = pd.DatetimeIndex(dates)
+
+    first_value = 3
+
+    prior_df = pd.DataFrame(
+        data=[
+            [3,  first_value,  0,    0],
+            [0,      2,        3,    1],
+            [1,      4,        0,    0],
+            [2,      6,        0,    1],
+            [np.nan, np.nan,   0,    0],
+            [4,      10,       1,    1],
+            [5,      np.nan,   np.nan, np.nan],
+        ],
+        columns=cols,
+        index=idx,
+    )
+
+    out = pipeline(prior_df)
+
+    # this can only be true if the temporal state is being reset
+    # between invocations
+    assert np.isclose(
+        out.states['tag-2_norm_trace-0.1'].iloc[0],
+        0.1 * first_value,
+    )
