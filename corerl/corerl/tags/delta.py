7c5d72cc3c90ed43ea225aacadb876f78d806097
Alex Trudeau <alexandre.trudeau@rlcore.ai>
2025-07-08 12:53:04 -0600

    fix(PROD-681): make DeltaTagConfigs OPCTags

--- corerl/corerl/tags/delta.py
+++ corerl/corerl/tags/delta.py
@@ -4,11 +4,14 @@ from lib_config.config import MISSING, config
 from lib_defs.config_defs.tag_config import TagType
 
 from corerl.tags.components.bounds import SafetyZonedTag
+from corerl.tags.components.opc import OPCTag
 
 
 @config()
 class DeltaTagConfig(
     SafetyZonedTag,
+    OPCTag
 ):
     name: str = MISSING
     type: Literal[TagType.delta] = TagType.delta
+    is_computed: bool = False
