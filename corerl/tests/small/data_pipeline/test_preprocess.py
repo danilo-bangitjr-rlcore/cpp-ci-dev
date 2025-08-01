2631b4027bb52ca181546c8566f357d0372bf3d5
Alex Trudeau <alexandre.trudeau@rlcore.ai>
2025-08-01 11:20:14 -0600

    test: added test with tags sharing same prefix

--- corerl/tests/small/data_pipeline/test_preprocess.py
+++ corerl/tests/small/data_pipeline/test_preprocess.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from test.infrastructure.utils.pandas import dfs_close
 
 from corerl.data_pipeline.constructors.preprocess import Preprocessor
 from corerl.data_pipeline.datatypes import DataMode, PipelineFrame
@@ -25,3 +26,28 @@ def test_only_normalize_preprocess():
     expected = np.array([0.1, 0.5])
 
     assert np.allclose(out, expected)
+
+def test_tag_names_same_prefix_preprocess():
+    tag_cfg1 = BasicTagConfig(
+        name="tag_1",
+        operating_range=(0, 10),
+        preprocess=[
+            NormalizerConfig(min=0, max=10),
+        ],
+    )
+    tag_cfg2 = BasicTagConfig(
+        name="tag_1_sp",
+        operating_range=(0, 10),
+        preprocess=[],
+    )
+    preprocessor = Preprocessor([tag_cfg1, tag_cfg2])
+
+    df = pd.DataFrame({"tag_1": [1, 5], "tag_2": [1, 5]})
+    pf = PipelineFrame(df, DataMode.ONLINE)
+
+    pf = preprocessor(pf)
+    out = pf.data
+
+    expected = pd.DataFrame({"tag_1": [0.1, 0.5], "tag_2": [1, 5]})
+
+    assert dfs_close(out, expected)
