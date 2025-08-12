53336a825c327da317e4f2f1b29a3bc757e58762
Alex Trudeau <alexandre.trudeau@rlcore.ai>
2025-08-12 09:32:59 -0600

    style: SetpointTagConfig assertion in test

--- corerl/tests/small/tags/components/test_nominal_setpoint.py
+++ corerl/tests/small/tags/components/test_nominal_setpoint.py
@@ -11,6 +11,7 @@ from corerl.eval.evals import EvalsTable
 from corerl.eval.metrics import MetricsTable
 from corerl.messages.event_bus import DummyEventBus
 from corerl.state import AppState
+from corerl.tags.setpoint import SetpointTagConfig
 
 
 def test_nominal_setpoint_norm():
@@ -20,6 +21,7 @@ def test_nominal_setpoint_norm():
     )
     cfg = direct_load_config(MainConfig, config_name=config_path)
     assert not isinstance(cfg, ConfigValidationErrors)
+    assert isinstance(cfg.pipeline.tags[0], SetpointTagConfig)
     assert cfg.pipeline.tags[0].nominal_setpoint == 0.4
 
     event_bus = DummyEventBus()
