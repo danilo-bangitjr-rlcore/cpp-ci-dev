b9a93be221f3ed3a4fadc1a67c4692591ab95857
Alex Trudeau <alexandre.trudeau@rlcore.ai>
2025-08-15 09:09:59 -0600

    test: update tests to reflect refactor

--- corerl/tests/small/environment/async_env/test_deployment_async_env.py
+++ corerl/tests/small/environment/async_env/test_deployment_async_env.py
@@ -1,14 +1,14 @@
+import os
 from unittest.mock import MagicMock, patch
 
 import pandas as pd
 import pytest
 from coreio.utils.io_events import IOEvent, IOEventType, OPCUANodeWriteValue
+from lib_config.loader import direct_load_config
 from pandas import DataFrame
 from test.infrastructure.utils.pandas import dfs_close
 
-from corerl.data_pipeline.db.data_reader import TagDBConfig
-from corerl.data_pipeline.transforms import NukeConfig
-from corerl.environment.async_env.async_env import AsyncEnvConfig
+from corerl.config import MainConfig
 from corerl.environment.async_env.deployment_async_env import DeploymentAsyncEnv, clip_action, sanitize_actions
 from corerl.tags.setpoint import SetpointTagConfig
 
@@ -25,31 +25,29 @@ def mock_socket(monkeypatch: pytest.MonkeyPatch):
     return socket
 
 @pytest.fixture
-def minimal_cfg():
-    return AsyncEnvConfig(
-        coreio_origin="tcp://localhost:5555",
-        db=TagDBConfig(),
-        obs_period=pd.Timedelta("1min"),
+def single_tag_minimal_cfg():
+    config_path = os.path.join(
+        os.path.dirname(__file__),
+        'test_deployment_async_env_single_tag.yaml',
     )
 
+    return direct_load_config(MainConfig, config_name=config_path)
+
 @pytest.fixture
-def minimal_tag_cfgs():
-    return [
-        SetpointTagConfig(
-            name="action-1",
-            operating_range=(0.0, 10.0),
-            node_identifier="ns=2;i=1",
-            connection_id="conn-1",
-        ),
-    ]
+def multi_tag_minimal_cfg():
+    config_path = os.path.join(
+        os.path.dirname(__file__),
+        'test_deployment_async_env_multi_tag.yaml',
+    )
+
+    return direct_load_config(MainConfig, config_name=config_path)
 
 @pytest.fixture
 def deployment_env(
-    minimal_cfg: AsyncEnvConfig,
-    minimal_tag_cfgs: list[SetpointTagConfig],
+    single_tag_minimal_cfg: MainConfig,
 ):
     with patch("corerl.environment.async_env.deployment_async_env.DataReader", autospec=True):
-        return DeploymentAsyncEnv(minimal_cfg, minimal_tag_cfgs)
+        return DeploymentAsyncEnv(single_tag_minimal_cfg.env, single_tag_minimal_cfg.pipeline.tags)
 
 
 def test_emit_action_emits_correct_event(mock_socket: MagicMock, deployment_env: DeploymentAsyncEnv):
@@ -81,28 +79,10 @@ def test_emit_action_emits_correct_event(mock_socket: MagicMock, deployment_env:
 # ---------------------------
 # -- Multiple action tests --
 # ---------------------------
-
-def make_multi_tag_cfgs():
-    return [
-        SetpointTagConfig(
-            name="action-1",
-            operating_range=(0.0, 10.0),
-            node_identifier="ns=2;i=1",
-            connection_id="conn-1",
-        ),
-        SetpointTagConfig(
-            name="action-2",
-            operating_range=(5.0, 15.0),
-            node_identifier="ns=2;i=2",
-            connection_id="conn-2",
-        ),
-    ]
-
 @pytest.fixture
-def multi_deployment_env(minimal_cfg: AsyncEnvConfig):
-    tag_cfgs = make_multi_tag_cfgs()
+def multi_deployment_env(multi_tag_minimal_cfg: MainConfig):
     with patch("corerl.environment.async_env.deployment_async_env.DataReader", autospec=True):
-        return DeploymentAsyncEnv(minimal_cfg, tag_cfgs)
+        return DeploymentAsyncEnv(multi_tag_minimal_cfg.env, multi_tag_minimal_cfg.pipeline.tags)
 
 
 def test_emit_action_multiple_actions(mock_socket: MagicMock, multi_deployment_env: DeploymentAsyncEnv):
@@ -137,7 +117,7 @@ def test_emit_action_clips_out_of_range(mock_socket: MagicMock, multi_deployment
     actual_event = IOEvent.model_validate_json(json_str)
 
     assert topic == "coreio"
-    assert actual_event.data["conn-1"][0].value == pytest.approx(0.01, abs=1e-6)
+    assert actual_event.data["conn-1"][0].value == pytest.approx(0.008, abs=1e-6)
     assert actual_event.data["conn-2"][0].value == pytest.approx(14.99, abs=1e-6)
 
 def test_emit_action_unknown_action_raises(deployment_env: DeploymentAsyncEnv):
@@ -153,80 +133,70 @@ def test_emit_action_unknown_action_raises(deployment_env: DeploymentAsyncEnv):
 # -- Utility tests --
 # -------------------
 
-def test_action_clipping():
+def test_action_clipping(multi_tag_minimal_cfg: MainConfig):
     """
     Clips actions to operating or red zone bounds as expected.
     """
+    tag_cfg_1 = multi_tag_minimal_cfg.pipeline.tags[0]
+    tag_cfg_2 = multi_tag_minimal_cfg.pipeline.tags[1]
+
+    assert isinstance(tag_cfg_1, SetpointTagConfig)
+    assert isinstance(tag_cfg_2, SetpointTagConfig)
+
     tag_cfgs = {
-        "action-1": SetpointTagConfig(
-            name="action-1",
-            operating_range=(5.0, 15.0),
-            red_bounds=(None, 12),
-            preprocess=[],
-            state_constructor=[NukeConfig()],
-        ),
-        "action-2": SetpointTagConfig(
-            name="action-2",
-            operating_range=(0.0, 60.0),
-            preprocess=[],
-            state_constructor=[NukeConfig()],
-        ),
+        "action-1": tag_cfg_1,
+        "action-2": tag_cfg_2,
     }
 
-    actions = DataFrame({"action-1": [2.5], "action-2": [30]})
-    expected = DataFrame({"action-1": [5], "action-2": [30]})
+    actions = DataFrame({"action-1": [-2.5], "action-2": [30]})
+    expected = DataFrame({"action-1": [0], "action-2": [15]})
     clip_action(actions, tag_cfgs, rtol=0)
     assert dfs_close(actions, expected)
 
     actions = DataFrame({"action-1": [30], "action-2": [30]})
-    expected = DataFrame({"action-1": [12], "action-2": [30]}) # clip to red zone
+    expected = DataFrame({"action-1": [8], "action-2": [15]}) # clip to red zone
     clip_action(actions, tag_cfgs, rtol=0)
     assert dfs_close(actions, expected)
 
-    actions = DataFrame({"action-1": [10], "action-2": [-3]})
-    expected = DataFrame({"action-1": [10], "action-2": [0]})
+    actions = DataFrame({"action-1": [5], "action-2": [-3]})
+    expected = DataFrame({"action-1": [5], "action-2": [5]})
     clip_action(actions, tag_cfgs, rtol=0)
     assert dfs_close(actions, expected)
 
-    actions = DataFrame({"action-1": [10], "action-2": [90]})
-    expected = DataFrame({"action-1": [10], "action-2": [60]})
+    actions = DataFrame({"action-1": [5], "action-2": [90]})
+    expected = DataFrame({"action-1": [5], "action-2": [15]})
     clip_action(actions, tag_cfgs, rtol=0)
     assert dfs_close(actions, expected)
 
-    actions = DataFrame({"action-1": [10], "action-2": [-3]})
-    expected = DataFrame({"action-1": [10], "action-2": [0]})
+    actions = DataFrame({"action-1": [5], "action-2": [-3]})
+    expected = DataFrame({"action-1": [5], "action-2": [5]})
     clip_action(actions, tag_cfgs, rtol=0)
     assert dfs_close(actions, expected)
 
     actions = DataFrame({"action-1": [30], "action-2": [90]})
-    expected = DataFrame({"action-1": [12], "action-2": [60]})
+    expected = DataFrame({"action-1": [8], "action-2": [15]})
     clip_action(actions, tag_cfgs, rtol=0)
     assert dfs_close(actions, expected)
 
-    actions = DataFrame({"action-1": [3], "action-2": [-13]})
-    expected = DataFrame({"action-1": [5], "action-2": [0]})
+    actions = DataFrame({"action-1": [-3], "action-2": [-13]})
+    expected = DataFrame({"action-1": [0], "action-2": [5]})
     clip_action(actions, tag_cfgs, rtol=0)
     assert dfs_close(actions, expected)
 
-def test_action_sanitizer():
+def test_action_sanitizer(multi_tag_minimal_cfg: MainConfig):
+    tag_cfg_1 = multi_tag_minimal_cfg.pipeline.tags[0]
+    tag_cfg_2 = multi_tag_minimal_cfg.pipeline.tags[1]
+
+    assert isinstance(tag_cfg_1, SetpointTagConfig)
+    assert isinstance(tag_cfg_2, SetpointTagConfig)
+
     tag_cfgs = {
-        "action-1": SetpointTagConfig(
-            name="action-1",
-            operating_range=(5.0, 15.0),
-            red_bounds=(None, 12),
-            preprocess=[],
-            state_constructor=[NukeConfig()],
-        ),
-        "action-2": SetpointTagConfig(
-            name="action-2",
-            operating_range=(0.0, 60.0),
-            preprocess=[],
-            state_constructor=[NukeConfig()],
-        ),
+        "action-1": tag_cfg_1,
+        "action-2": tag_cfg_2,
     }
 
     actions = DataFrame({"action-1": [30], "action-2": [30]})
-    expected = DataFrame({"action-1": [12], "action-2": [30]}) # clip to red zone
+    expected = DataFrame({"action-1": [8], "action-2": [15]}) # clip to red zone
     sanitize_actions(actions, tag_cfgs, rtol=0)
     assert dfs_close(actions, expected)
 
