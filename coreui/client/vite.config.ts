8b58e1df7e234bc04cb110c48050f6b644812f87
javier-rlcore <javier@rlcore.ai>
2025-08-26 14:07:46 -0600

    chore: add prettier and tailwindcss

--- coreui/client/vite.config.ts
+++ coreui/client/vite.config.ts
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tanstackRouter({
+  plugins: [
+    tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
-    }), react()],
-   server: {
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  server: {
     proxy: {
       '/api': 'http://localhost:8000',
     },
   },
   base: '/app/',
-})
+});
