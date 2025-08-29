a9515cd04f232cfc144376d640289051e4da3253
Alex Trudeau <alexandre.trudeau@rlcore.ai>
2025-08-29 09:47:59 -0600

    feat(PROD-725): config validation check that ensures Goals/JointGoals are consistent with red zones

--- corerl/corerl/tags/validate_tag_configs.py
+++ corerl/corerl/tags/validate_tag_configs.py
@@ -1,4 +1,5 @@
 import warnings
+from collections.abc import Callable
 from functools import partial
 from itertools import product
 
@@ -7,7 +8,7 @@ from lib_utils.maybe import Maybe
 
 from corerl.config import MainConfig
 from corerl.environment.reward.config import Goal, JointGoal
-from corerl.tags.components.bounds import BoundInfo, SafetyZonedTag, get_widest_static_bounds
+from corerl.tags.components.bounds import BoundInfo, BoundsInfo, Direction, SafetyZonedTag, get_widest_static_bounds
 from corerl.tags.tag_config import TagConfig
 
 
@@ -261,9 +262,115 @@ def assert_consistent_non_redundant_goals(cfg: MainConfig):
 
     return tag_ranges
 
+
+def assert_consistent_goals_and_red_zones(cfg: MainConfig, goal_ranges: dict[str, list[float | None]]):
+    """
+    Ensure Goals do not violate red zones
+    """
+    def _get_goal_range_that_violates_red_zone(
+        tag_cfg: SafetyZonedTag,
+        red_dir: Direction,
+        red_bound: float,
+        goal_ind: int,
+        goal_bound: float,
+    ) -> tuple[float | None, float | None]:
+        if (red_dir == Direction.Lower and goal_ind == 0) or (red_dir == Direction.Upper and goal_ind == 1):
+            min_val = min(goal_bound, red_bound)
+            max_val = max(goal_bound, red_bound)
+        else:
+            assert tag_cfg.operating_range
+            op_ind = (goal_ind + 1) % 2
+            if op_ind == 0:
+                min_val = tag_cfg.operating_range[op_ind]
+                max_val = goal_bound
+            else:
+                min_val = goal_bound
+                max_val = tag_cfg.operating_range[op_ind]
+
+        return min_val, max_val
+
+    def _assert_goal_bound_within_red_bounds(
+        tag_cfg: SafetyZonedTag,
+        lens: Callable[[BoundsInfo], BoundInfo | None],
+        comparison: Callable[[float, float], bool],
+        ind: int,
+    ):
+        assert tag_cfg.red_bounds_info
+        red_bound_info = lens(tag_cfg.red_bounds_info)
+        goal_bound = goal_ranges[tag_cfg.name][ind]
+        if red_bound_info and isinstance(goal_bound, float):
+            # Can directly compare red zone boundary and goal boundary if they are both floats
+            if isinstance(red_bound_info.bound_elem, float):
+                min_val, max_val = _get_goal_range_that_violates_red_zone(
+                    tag_cfg=tag_cfg,
+                    red_dir=red_bound_info.direction,
+                    red_bound=red_bound_info.bound_elem,
+                    goal_ind=ind,
+                    goal_bound=goal_bound,
+                )
+                assert comparison(goal_bound, red_bound_info.bound_elem), (
+                    f"The Goals for {tag_cfg.name} are satisfied at values between [{min_val}, {max_val}] "
+                    f"but this violates the red zone boundary ({red_bound_info.bound_elem})."
+                )
+            elif isinstance(red_bound_info.bound_elem, str):
+                assert red_bound_info.bound_tags
+                assert red_bound_info.bound_func
+                permutations = get_tag_value_permutations(red_bound_info.bound_tags, cfg.pipeline.tags)
+                for permutation in permutations:
+                    red_bound_val = red_bound_info.bound_func(*permutation)
+                    min_val, max_val = _get_goal_range_that_violates_red_zone(
+                        tag_cfg=tag_cfg,
+                        red_dir=red_bound_info.direction,
+                        red_bound=red_bound_val,
+                        goal_ind=ind,
+                        goal_bound=goal_bound,
+                    )
+                    assert comparison(goal_bound, red_bound_val), (
+                        f"The Goals for {tag_cfg.name} are satisfied at values between [{min_val}, {max_val}] "
+                        f"but this violates the red zone boundary ({red_bound_info.bound_elem}) "
+                        f"when {red_bound_info.bound_tags} have the following values: {permutation}. "
+                    )
+
+    for tag_name in goal_ranges:
+        goal_tag_cfg = (
+            Maybe.find(lambda tag_cfg, tag_name=tag_name: tag_cfg.name == tag_name, cfg.pipeline.tags)
+            .is_instance(SafetyZonedTag)
+            .expect(f"Was unable to find tag config for tag: {tag_name}")
+        )
+        if goal_tag_cfg.red_bounds_info is not None:
+            # Compare red_lo and goal_lo
+            _assert_goal_bound_within_red_bounds(
+                tag_cfg=goal_tag_cfg,
+                lens=(lambda bounds_info: bounds_info.lower),
+                comparison=(lambda goal_bound, red_bound: goal_bound > red_bound),
+                ind=0,
+            )
+            # Compare red_hi and goal_hi
+            _assert_goal_bound_within_red_bounds(
+                tag_cfg=goal_tag_cfg,
+                lens=(lambda bounds_info: bounds_info.upper),
+                comparison=(lambda goal_bound, red_bound: goal_bound < red_bound),
+                ind=1,
+            )
+            # Compare red_lo and goal_hi
+            _assert_goal_bound_within_red_bounds(
+                tag_cfg=goal_tag_cfg,
+                lens=(lambda bounds_info: bounds_info.lower),
+                comparison=(lambda goal_bound, red_bound: goal_bound > red_bound),
+                ind=1,
+            )
+            # Compare red_hi and goal_lo
+            _assert_goal_bound_within_red_bounds(
+                tag_cfg=goal_tag_cfg,
+                lens=(lambda bounds_info: bounds_info.upper),
+                comparison=(lambda goal_bound, red_bound: goal_bound < red_bound),
+                ind=0,
+            )
+
 def validate_tag_configs(cfg: MainConfig):
     tag_cfgs = cfg.pipeline.tags
-    assert_consistent_non_redundant_goals(cfg)
+    goal_ranges = assert_consistent_non_redundant_goals(cfg)
+    assert_consistent_goals_and_red_zones(cfg, goal_ranges)
 
     def check_bounds(tag_cfg: SafetyZonedTag):
         operating_vs_expected_range_checks(tag_cfg, tag_cfgs)
