a8c65d98697afd3ac3f1889fa4adaf240f017195
James Short <james.short@rlcore.ai>
2025-09-09 13:44:17 -0700

    tag card component

--- coreui/client/src/components/navigation/Tags.tsx
+++ coreui/client/src/components/navigation/Tags.tsx
@@ -4,10 +4,10 @@ type Tag = {
 
 export function TagCard({key, tag}: { key: number, tag: Tag}) {
     return (
-        <div key={key} className=''>
+        <div key={key} className='border-2 border-black border-solid rounded-sm shadow-md m-2 p-2'>
             <label>
-                <span className='block text-sm font-bold'>Tag Name</span>
-                <input type='text' value={tag.name} className='border border-black border-solid rounded-sm text-base'/>
+                <span className='block text-xs font-bold'>Tag Name</span>
+                <input type='text' value={tag.name} className='border-2 border-black border-solid rounded-sm text-base pl-1'/>
             </label>
         </div>
     );
