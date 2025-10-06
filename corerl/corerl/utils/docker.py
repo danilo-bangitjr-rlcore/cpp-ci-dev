3816316b06f1e187772e82dc656c2596ddfa5be2
Andy Patterson <andy@rlcore.ai>
2025-10-05 21:53:45 -0600

    refactor(corerl): remove empty utility files docker.py and types.py

--- corerl/corerl/utils/docker.py
+++ corerl/corerl/utils/docker.py
@@ -1,7 +1,1 @@
-from docker import DockerClient
 
-
-def container_exists(client: DockerClient, name: str) -> bool:
-    containers = client.containers.list(all=True)
-    container_names = [c.name for c in containers]
-    return name in container_names
