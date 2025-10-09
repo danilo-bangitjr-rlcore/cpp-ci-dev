f1bd0b9637b95a2ed03184579c3da7bf6265cd39
Andy Patterson <andy@rlcore.ai>
2025-10-09 13:53:29 -0600

    refactor(corerl): infrastructure configs extracted to configs/infra.py

--- corerl/corerl/config.py
+++ corerl/corerl/config.py
@@ -9,6 +9,8 @@ from lib_defs.config_defs.tag_config import TagType
 from pydantic import Field
 
 from corerl.agent.greedy_ac import GreedyACConfig
+from corerl.configs.infra import FeatureFlags, InfraConfig
+from corerl.configs.messages.event_bus import EventBusConfig
 from corerl.data_pipeline.pipeline_config import PipelineConfig
 from corerl.environment.async_env.async_env import AsyncEnvConfig
 from corerl.eval.config import EvalConfig
@@ -17,79 +19,6 @@ from corerl.eval.metrics.base import MetricsDBConfig
 from corerl.interaction.factory import InteractionConfig
 from corerl.messages.factory import EventBusConfig
 
-
-@config()
-class DBConfig:
-    """
-    Kind: optional external
-
-    Default configurations for our timeseries
-    database. Should generally be owned by RLCore
-    during setup, however in more extreme circumstances
-    this may be owned by the user on an internal network.
-    """
-    drivername: str = 'postgresql+psycopg2'
-    username: str = 'postgres'
-    password: str = 'password'
-    ip: str = 'localhost'
-    port: int = 5432
-    db_name: str = 'postgres'
-    schema: str = 'public'
-
-
-@config()
-class InfraConfig:
-    """
-    Kind: optional external
-
-    Infrastructure configuration for wiring the agent
-    into the external system.
-    """
-    db: DBConfig = Field(default_factory=DBConfig)
-
-
-@config()
-class FeatureFlags:
-    """
-    Kind: internal
-
-    Flags to enable/disable new features in the agent.
-    These should default to False and the date that they
-    are added should be recorded.
-
-    Feature flags should generally be for internal use only.
-    Their impact on the customer cannot be easily guaranteed
-    or communicated.
-
-    See documentation:
-    https://docs.google.com/document/d/1Inm7dMHIRvIGvM7KByrRhxHsV7uCIZSNsddPTrqUcOU/edit?tab=t.4238yb3saoju
-    """
-    # 2025-02-01
-    ensemble: int = 2
-
-    # 2025-04-29
-    recency_bias_buffer: bool = False
-
-    # 2025-05-14
-    regenerative_optimism: bool = False
-
-    # 2025-05-26
-    normalize_return: bool = False
-
-    # 2025-06-11
-    nominal_setpoint_bias: bool = True
-
-    # 2025-06-27
-    higher_critic_lr: bool = True
-
-    # 2025-06-27
-    mu_sigma_multipliers: bool = False
-
-    # 2025-07-24
-    wide_metrics: bool = False
-
-    # 2025-10-02
-    all_layer_norm: bool = False
 
 @config()
 class MainConfig:
