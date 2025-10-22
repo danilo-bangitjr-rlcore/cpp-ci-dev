722e8136a48cef7a8a5a4fa9669a6959f73c1957
Andy Patterson <andy@rlcore.ai>
2025-10-21 22:00:08 -0600

    feat(coreio): add global event bus with dummy events to coreio

---
+++ coreio/coreio/event_bus/client.py
@@ -0,0 +1,20 @@
+from lib_defs.type_defs.base_events import Event, EventTopic, EventType
+from lib_utils.messages.event_bus_client import EventBusClient as _EventBusClient
+
+
+class IOEventBusClient(_EventBusClient[Event, EventType, EventTopic]):
+    def __init__(
+        self,
+        host: str = "localhost",
+        pub_port: int = 5559,
+        sub_port: int = 5560,
+    ):
+        super().__init__(
+            event_class=Event,
+            host=host,
+            pub_port=pub_port,
+            sub_port=sub_port,
+        )
+
+
+EventBusClient = IOEventBusClient
