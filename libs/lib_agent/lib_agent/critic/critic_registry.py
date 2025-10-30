d9a768d94249521eca3725f458e46476cd659aec
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-10-30 14:27:04 -0600

    chore(PROD-1192): add adv to registry

--- libs/lib_agent/lib_agent/critic/critic_registry.py
+++ libs/lib_agent/lib_agent/critic/critic_registry.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from lib_agent.critic.adv_critic import AdvConfig, AdvCritic
 from lib_agent.critic.qrc_critic import QRCConfig, QRCCritic
 
 
@@ -11,4 +12,6 @@ def get_critic(
     name = cfg['name']
     if name == 'QRC':
         return QRCCritic(QRCConfig(**cfg), state_dim, action_dim)
+    if name == 'Adv':
+        return AdvCritic(AdvConfig(**cfg), state_dim, action_dim)
     raise NotImplementedError()
