6904d9e73af31af3163c2d6353fa28d914f7bf7f
revan-macqueen-rlcore <revan.macqueen@rlcore.ai>
2025-10-30 10:17:06 -0600

    feat(research):  different ensemble aggregations

--- research/src/agent/gac.py
+++ research/src/agent/gac.py
@@ -4,6 +4,7 @@ from typing import Any, NamedTuple
 import chex
 import jax
 import jax.numpy as jnp
+import lib_utils.jax as jax_u
 import numpy as np
 from lib_agent.actor.actor_registry import get_actor
 from lib_agent.actor.percentile_actor import PAState
@@ -39,6 +40,8 @@ class GreedyACConfig:
     loss_threshold: float = 1e-4
     bootstrap_action_samples: int = 10
     even_better_q: bool = False
+    ensemble_aggregation: str = 'mean'
+    ensemble_percentile: float = .5
 
 
 class GreedyAC:
@@ -197,4 +200,13 @@ class GreedyAC:
         qs = out.q
         if self._cfg.even_better_q:
             qs = out.q + out.h  # EvenBetterQ correction
-        return qs.mean(axis=0).squeeze(-1)
+        aggregated_values = self._aggregate_ensemble_values(qs)
+        return aggregated_values.squeeze(-1)
+
+    @jax_u.method_jit
+    def _aggregate_ensemble_values(self, ensemble_values: jax.Array) -> jax.Array:
+        if self._cfg.ensemble_aggregation == "mean":
+            return ensemble_values.mean(axis=0)
+        if self._cfg.ensemble_aggregation == "percentile":
+            return jnp.percentile(ensemble_values, self._cfg.ensemble_percentile * 100, axis=0)
+        raise ValueError(f"Unknown ensemble aggregation method: {self._cfg.ensemble_aggregation}")
