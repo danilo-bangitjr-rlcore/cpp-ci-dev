207f203e1f6e7f90042e58c18318a46a90d50014
Andy Patterson <andy@rlcore.ai>
2025-11-02 14:08:33 -0700

    feat(coreio): enable global event bus by default

--- coreio/coreio/configs/messages/event_bus_client.py
+++ coreio/coreio/configs/messages/event_bus_client.py
@@ -3,7 +3,7 @@ from lib_config.config import config
 
 @config()
 class EventBusClientConfig:
-    enabled: bool = False
+    enabled: bool = True
     host: str = "localhost"
     pub_port: int = 5570
     sub_port: int = 5571
