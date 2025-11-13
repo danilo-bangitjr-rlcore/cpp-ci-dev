de292e0e4bc9723195a3317431583818a0678d9d
danilo-bangitjr-rlcore <danilo.bangit.jr@rlcore.ai>
2025-11-13 15:30:52 -0700

    fix: clang-format errors

--- coreio-cpp/tests/hello_test.cpp
+++ coreio-cpp/tests/hello_test.cpp
@@ -1,11 +1,11 @@
-#include <gtest/gtest.h>
 #include "../include/hello.h"
+#include <gtest/gtest.h>
 
 // Define a test case: Suite Name "HelloTest", Test Name "BasicAssertions"
 TEST(HelloTest, BasicAssertions) {
-    std::string expected = "Hello, World!";
-    std::string actual = hello_world_message();
+  std::string expected = "Hello, World!";
+  std::string actual = hello_world_message();
 
-    // Google Test assertion for equality
-    EXPECT_EQ(expected, actual);
+  // Google Test assertion for equality
+  EXPECT_EQ(expected, actual);
 }
