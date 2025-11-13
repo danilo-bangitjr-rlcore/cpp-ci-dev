fe7780bbeb6f6f4301b39a8f75955ecfb038c3cd
danilo-bangitjr-rlcore <danilo.bangit.jr@rlcore.ai>
2025-11-13 15:20:03 -0700

    chore: add cpp style check and static analysis

--- coreio-cpp/coreio/hello.cpp
+++ coreio-cpp/coreio/hello.cpp
@@ -1,5 +1,3 @@
 #include "../include/hello.h"
 
-std::string hello_world_message() {
-    return "Hello, World!";
-}
+std::string hello_world_message() { return "Hello, World!"; }
--- coreio-cpp/coreio/main.cpp
+++ coreio-cpp/coreio/main.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
 #include "hello.h"
+#include <iostream>
 
 int main() {
-    std::cout << hello_world_message() << std::endl;
-    return 0;
+  std::cout << hello_world_message() << std::endl;
+  return 0;
 }
